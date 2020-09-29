8355ebb740648168c79419924586d41122c0665d
Belen Curcio <curciobelen@gmail.com>
2020-09-29 19:39:16 -0300

    Changes to Button

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -17,10 +17,14 @@ const ProductView: FunctionComponent<Props> = ({ productData, className }) => {
   const rootClassName = cn(s.root, className);
   return (
     <div className={rootClassName}>
-      <div className="absolute h-12">
-        <h1>T-Shirt</h1>
+      <div className="absolute">
+        <h1 className="px-8 py-2 bg-violet text-white font-bold text-3xl">
+          T-Shirt
+        </h1>
+        <div className="px-6 py-2 pb-4 bg-violet text-white font-semibold inline-block">
+          $50
+        </div>
       </div>
-      <div className="absolute h-12">T-Shirt</div>
       <div className="flex-1 h-full p-24">
         <div className="bg-violet h-full"></div>
       </div>
@@ -57,7 +61,7 @@ const ProductView: FunctionComponent<Props> = ({ productData, className }) => {
           <p>{productData.description}</p>
         </section>
         <section className="pb-4">
-          <Button className="">Add to Cart</Button>
+          <Button className={s.button}>Add to Cart</Button>
         </section>
       </div>
     </div>
