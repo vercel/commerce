fe7051c1b4d425e7bf7b29b1fabf007da1f0e89b
Belen Curcio <curciobelen@gmail.com>
2020-10-02 11:58:13 -0300

    Sidebar changes

--- components/ui/Sidebar/Sidebar.tsx
+++ components/ui/Sidebar/Sidebar.tsx
@@ -11,7 +11,7 @@ const Sidebar: FC<Props> = ({ className, children }) => {
   const rootClassName = cn(s.root, className)
   return (
     <div className={rootClassName}>
-      <div className="fixed inset-0 overflow-hidden shadow-sm bg-black bg-opacity-25">
+      <div className="fixed inset-0 overflow-hidden shadow-sm bg-black bg-opacity-25 z-50">
         <div className="absolute inset-0 overflow-hidden">
           <section className="absolute inset-y-0 right-0 pl-10 max-w-full flex sm:pl-16 ">
             <div className="w-screen max-w-2xl">
