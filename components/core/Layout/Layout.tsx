6587ab10ae429842c8daf460512a88055efc0f17
Luis Alvarez <luis@vercel.com>
2020-10-01 21:18:01 -0500

    Moved UIProvider to the layout

--- components/core/Layout/Layout.tsx
+++ components/core/Layout/Layout.tsx
@@ -1,17 +1,17 @@
 import cn from 'classnames'
-import React, { FunctionComponent } from 'react'
+import { FunctionComponent } from 'react'
 import s from './Layout.module.css'
 import { Navbar, Featurebar } from '@components/core'
 import { Container, Sidebar } from '@components/ui'
 import { CartSidebarView } from '@components/cart'
-import { useUI } from '@components/ui/context'
+import { UIProvider, useUI } from '@components/ui/context'
 
 interface Props {
   className?: string
   children?: any
 }
 
-const Layout: FunctionComponent<Props> = ({ className, children }) => {
+const CoreLayout: FunctionComponent<Props> = ({ className, children }) => {
   const rootClassName = cn(s.root, className)
   const { displaySidebar } = useUI()
   return (
@@ -32,5 +32,11 @@ const Layout: FunctionComponent<Props> = ({ className, children }) => {
     </div>
   )
 }
+
+const Layout: FunctionComponent<Props> = (props) => (
+  <UIProvider>
+    <CoreLayout {...props} />
+  </UIProvider>
+)
 
 export default Layout
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,6 @@
+import type { AppProps } from 'next/app'
 import '@assets/global.css'
-import React from 'react'
-import { UIProvider } from '@components/ui/context'
 
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <UIProvider>
-        <Component {...pageProps} />
-      </UIProvider>
-    </>
-  )
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
