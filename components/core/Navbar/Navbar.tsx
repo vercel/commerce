a25da7d6cf496a617a4db16ce8d7155038ebbeac
Belen Curcio <curciobelen@gmail.com>
2020-10-05 14:28:59 -0300

    Transitions

--- components/core/Navbar/Navbar.tsx
+++ components/core/Navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import s from './Navbar.module.css'
 import { Logo, Container } from '@components/ui'
 import { Searchbar } from '@components/core'
 import { UserNav } from '@components/core'
+import Link from 'next/link'
 interface Props {
   className?: string
 }
@@ -12,12 +13,16 @@ const Navbar: FC<Props> = ({ className }) => {
   const rootClassName = cn(s.root, className)
   return (
     <Container className={rootClassName}>
-      <Logo />
+      <Link href="/">
+        <h1 className="cursor-pointer">
+          <Logo />
+        </h1>
+      </Link>
       <div className="flex flex-row h-full content-center flex-1 ml-10">
-        <nav className="hidden flex-row items-center px-3 lg:flex">
-          <a className="pr-4">All</a>
-          <a className="pr-4">Clothes</a>
-          <a>Accesories</a>
+        <nav className="hidden flex-row items-center px-6 lg:flex space-x-4">
+          <a className={s.link}>All</a>
+          <a className={s.link}>Clothes</a>
+          <a className={s.link}>Accesories</a>
         </nav>
         <Searchbar />
       </div>
--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -2,7 +2,8 @@ import cn from 'classnames'
 import s from './ProductCard.module.css'
 import { FC } from 'react'
 import { Heart } from '@components/icon'
-import { url } from 'inspector'
+import Link from 'next/link'
+
 interface Props {
   className?: string
   children?: any
@@ -13,34 +14,39 @@ interface ProductData {
   name: string
   images: any
   prices: any
+  path: string
 }
 
 const ProductCard: FC<Props> = ({ className, node: productData }) => {
   const rootClassName = cn(s.root, className)
 
   return (
-    <div
-      className={rootClassName}
-      style={{
-        backgroundImage: `url('${productData.images.edges[0].node.urlSmall}')`,
-      }}
-    >
-      <div className="flex flex-row justify-between box-border w-full z-10 relative">
-        <div className="flex flex-col flex-1 overflow-hidden">
-          <div className="flex-1">
-            <h1 className={s.productTitle}>{productData.name}</h1>
-          </div>
-          <div className="flex-0">
-            <div className={s.productPrice}>
-              ${productData.prices.price.value}
+    <Link href={`product${productData.path}`}>
+      <div className={rootClassName}>
+        <div className="absolute z-10 inset-0 flex items-center justify-center">
+          <img
+            className="w-full object-cover"
+            src={productData.images.edges[0].node.urlSmall}
+          />
+        </div>
+        <div className={s.squareBg} />
+        <div className="flex flex-row justify-between box-border w-full z-10 relative">
+          <div className="flex flex-col flex-1 overflow-hidden">
+            <div className="flex-1">
+              <h1 className={s.productTitle}>{productData.name}</h1>
+            </div>
+            <div className="flex-0">
+              <div className={s.productPrice}>
+                ${productData.prices.price.value}
+              </div>
             </div>
           </div>
-        </div>
-        <div className={s.wishlistButton}>
-          <Heart />
+          <div className={s.wishlistButton}>
+            <Heart />
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
