24f6ff792af4741e3dd843cec7788636ffcf9a68
Belen Curcio <curciobelen@gmail.com>
2020-10-05 19:16:49 -0300

    Is this the problem

--- components/core/Navbar/Navbar.tsx
+++ components/core/Navbar/Navbar.tsx
@@ -12,22 +12,26 @@ interface Props {
 const Navbar: FC<Props> = ({ className }) => {
   const rootClassName = cn(s.root, className)
   return (
-    <Container className={rootClassName}>
-      <Link href="/">
-        <span className="inline-block cursor-pointer">
-          <Logo />
-        </span>
-      </Link>
-      <div className="flex flex-row h-full content-center flex-1 ml-10">
-        <nav className="hidden flex-row items-center px-6 lg:flex space-x-4">
-          <a className={s.link}>All</a>
-          <a className={s.link}>Clothes</a>
-          <a className={s.link}>Accesories</a>
-        </nav>
-        <Searchbar />
-      </div>
-      <UserNav />
-    </Container>
+    <div className={rootClassName}>
+      <Container>
+        <div className={s.container}>
+          <Link href="/">
+            <span className="flex-0 cursor-pointer">
+              <Logo />
+            </span>
+          </Link>
+          <div className="flex flex-row h-full content-center flex-1 ml-10">
+            <nav className="hidden flex-row items-center px-6 lg:flex space-x-4">
+              <a className={s.link}>All</a>
+              <a className={s.link}>Clothes</a>
+              <a className={s.link}>Accesories</a>
+            </nav>
+            <Searchbar />
+          </div>
+          <UserNav />
+        </div>
+      </Container>
+    </div>
   )
 }
 
