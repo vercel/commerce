4d2d3dc6d1fe42774eeb37ca046e7fc7ec4a1b2f
Belen Curcio <curciobelen@gmail.com>
2020-10-14 17:22:37 -0300

    Loading State for Button

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,5 +1,5 @@
 export { default as Hero } from './Hero'
-export { default as Logo } from './LoadingDots'
+export { default as Logo } from './Logo'
 export { default as Grid } from './Grid'
 export { default as Button } from './Button'
 export { default as Sidebar } from './Sidebar'
