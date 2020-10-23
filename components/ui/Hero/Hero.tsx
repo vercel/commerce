cb4492e33fd7daf1f0aa307ff9e0eb7eb061ce6d
Belen Curcio <curciobelen@gmail.com>
2020-10-23 13:17:52 -0300

    Changes to the Hero Component

--- components/ui/Hero/Hero.tsx
+++ components/ui/Hero/Hero.tsx
@@ -18,11 +18,11 @@ const Hero: FC<Props> = ({ headline, description, className }) => {
           <h2 className="text-4xl leading-10 font-extrabold text-white sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl">
             {headline}
           </h2>
-          <div>
+          <div className="flex flex-col justify-between">
             <p className="mt-5 text-xl leading-7 text-accent-2">
               {description}
             </p>
-            <a className="block text-white py-3 font-bold hover:underline flex flex-row cursor-pointer">
+            <a className="block text-white pt-3 font-bold hover:underline flex flex-row cursor-pointer">
               <span>Read it here</span>
               <RightArrow width="20" heigh="20" className="ml-1" />
             </a>
