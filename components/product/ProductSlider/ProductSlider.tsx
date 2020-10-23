e4f8a74f63f3be3b71a2697e4dce49afc0eabdda
Franco Arza <franco@basement.studio>
2020-10-23 12:15:12 -0300

    use bg-image

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,6 +1,6 @@
 import { useKeenSlider } from 'keen-slider/react'
+import Image from 'next/image'
 import React, { Children, FC, isValidElement, useState } from 'react'
-import { HiChevronLeft, HiChevronRight } from 'react-icons/hi'
 import cn from 'classnames'
 
 import s from './ProductSlider.module.css'
@@ -20,12 +20,11 @@ const ProductSlider: FC = ({ children }) => {
 
   return (
     <div className={s.root}>
-      <button className={cn(s.leftControl, s.control)} onClick={slider?.prev}>
-        <HiChevronLeft />
-      </button>
-      <button className={cn(s.rightControl, s.control)} onClick={slider?.next}>
-        <HiChevronRight />
-      </button>
+      <button className={cn(s.leftControl, s.control)} onClick={slider?.prev} />
+      <button
+        className={cn(s.rightControl, s.control)}
+        onClick={slider?.next}
+      />
       <div
         ref={ref}
         className="keen-slider h-full transition-opacity duration-150"
