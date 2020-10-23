ef2d647a99947c25a5f5592601e33a8e592ccd76
Belen Curcio <curciobelen@gmail.com>
2020-10-23 13:25:33 -0300

    Changes to the Hero Component

--- components/ui/Hero/Hero.tsx
+++ components/ui/Hero/Hero.tsx
@@ -1,6 +1,4 @@
-import cn from 'classnames'
 import React, { FC } from 'react'
-import s from './Hero.module.css'
 import { Container } from '@components/ui'
 import { RightArrow } from '@components/icon'
 interface Props {
@@ -9,27 +7,22 @@ interface Props {
   description: string
 }
 
-const Hero: FC<Props> = ({ headline, description, className }) => {
-  const rootClassName = cn('bg-black py-40 min-h-72', className)
+const Hero: FC<Props> = ({ headline, description }) => {
   return (
-    <div className={rootClassName}>
-      <Container>
-        <div className="mx-auto grid grid-cols-2">
-          <h2 className="text-4xl leading-10 font-extrabold text-white sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl">
-            {headline}
-          </h2>
-          <div className="flex flex-col justify-between">
-            <p className="mt-5 text-xl leading-7 text-accent-2">
-              {description}
-            </p>
-            <a className="block text-white pt-3 font-bold hover:underline flex flex-row cursor-pointer">
-              <span>Read it here</span>
-              <RightArrow width="20" heigh="20" className="ml-1" />
-            </a>
-          </div>
+    <Container>
+      <div className="mx-auto grid grid-cols-2 bg-black py-32">
+        <h2 className="text-4xl leading-10 font-extrabold text-white sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl">
+          {headline}
+        </h2>
+        <div className="flex flex-col justify-between">
+          <p className="mt-5 text-xl leading-7 text-accent-2">{description}</p>
+          <a className="block text-white pt-3 font-bold hover:underline flex flex-row cursor-pointer">
+            <span>Read it here</span>
+            <RightArrow width="20" heigh="20" className="ml-1" />
+          </a>
         </div>
-      </Container>
-    </div>
+      </div>
+    </Container>
   )
 }
 
