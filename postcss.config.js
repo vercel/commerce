222c96472bc854849c1ba843a5d1869985a63a6e
Belen Curcio <curciobelen@gmail.com>
2020-10-26 10:26:04 -0300

    Trying new purge

--- postcss.config.js
+++ postcss.config.js
@@ -15,5 +15,23 @@ module.exports = {
         },
       },
     ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: ['./pages/**/*.{tsx}', './components/**/*.{tsx}'],
+
+        // This is the function used to extract class names from your templates
+        defaultExtractor: (content) => {
+          // Capture as liberally as possible, including things like `h-(screen-1.5)`
+          const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+          // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+          const innerMatches =
+            content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+
+          return broadMatches.concat(innerMatches)
+        },
+      },
+    ],
   ],
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,6 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: {
-    content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
-    ],
-  },
   theme: {
     extend: {
       maxWidth: {
