074e9c0028250a56b99d77cd145cd18217583812
Belen Curcio <curciobelen@gmail.com>
2020-10-26 14:59:02 -0300

    Added empty states

--- components/cart/CartSidebarView/CartSidebarView.tsx
+++ components/cart/CartSidebarView/CartSidebarView.tsx
@@ -57,13 +57,13 @@ const CartSidebarView: FC = () => {
 
       {isEmpty ? (
         <div className="flex-1 px-4 flex flex-col justify-center items-center ">
-          <span className="border border-dashed border-white rounded-full flex items-center justify-center w-16 h-16 bg-black p-12 rounded-lg text-white">
+          <span className="border border-dashed border-secondary rounded-full flex items-center justify-center w-16 h-16 p-12 bg-primary text-primary">
             <Bag className="absolute" />
           </span>
           <h2 className="pt-6 text-2xl font-bold tracking-wide text-center">
             Your cart is empty
           </h2>
-          <p className="text-accents-3 px-10 text-center pt-2">
+          <p className="text-accents-6 px-10 text-center pt-2">
             Biscuit oat cake wafer icing ice cream tiramisu pudding cupcake.
           </p>
         </div>
