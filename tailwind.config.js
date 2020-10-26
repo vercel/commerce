876cadf8b8641f2b0c97a7eeb670ff51498c8779
Belen Curcio <curciobelen@gmail.com>
2020-10-26 10:31:44 -0300

    Trying new purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
+  purge: {
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
+    ],
   },
   theme: {
     extend: {
@@ -47,9 +50,5 @@ module.exports = {
       },
     },
   },
-  variants: {},
   plugins: [require('@tailwindcss/ui')],
-  experimental: {
-    applyComplexClasses: true,
-  },
 }
