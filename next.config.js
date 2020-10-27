0e298e8e57bda88c195c0ae3d9434fb03315a127
Luis Alvarez <luis@vercel.com>
2020-10-27 03:49:24 -0500

    re-enable i18n

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ module.exports = {
     sizes: [320, 480, 820, 1200, 1600],
     domains: ['cdn11.bigcommerce.com'],
   },
+  experimental: {
+    i18n: {
+      locales: ['en-US', 'es'],
+      defaultLocale: 'en-US',
+    },
+  },
   rewrites() {
     return [
       {
