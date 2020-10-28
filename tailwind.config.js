a0fee94223bb0ff35abb7a0c47a49fd1d7349c96
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-10-28 11:47:45 -0300

    Change extra-loose size to accomodate system font

--- tailwind.config.js
+++ tailwind.config.js
@@ -47,7 +47,7 @@ module.exports = {
           'rgba(0, 0, 0, 0.02) 0px 30px 30px, rgba(0, 0, 0, 0.03) 0px 0px 8px, rgba(0, 0, 0, 0.05) 0px 1px 0px',
       },
       lineHeight: {
-        'extra-loose': '2.3'
+        'extra-loose': '2.2'
       }
     },
   },
