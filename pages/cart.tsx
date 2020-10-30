b0137736d1543c1d8ac694bbf2ed97e7fc1093b7
Martin Bavio <mbavio@gmail.com>
2020-10-30 03:46:16 -0300

    Make the cart page ready for skeleton

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -21,7 +21,9 @@ export async function getStaticProps({
 }
 
 export default function Cart() {
-  const { data, isEmpty } = useCart()
+  const { data, error, isEmpty } = useCart()
+  const isLoading = data === undefined
+
   const { price: subTotal } = usePrice(
     data && {
       amount: data.base_amount,
@@ -37,112 +39,78 @@ export default function Cart() {
 
   const items = data?.line_items.physical_items ?? []
 
-  const error = null
-  const success = null
-
-  // Loading state
-  if (!data) {
-    return <div className="px-4 pt-2 sm:px-6 flex-1">Loading...</div>
-  }
-
   return (
-    <div className="grid lg:grid-cols-12">
-      <div className="lg:col-span-8">
-        {isEmpty ? (
-          <div className="flex-1 px-12 py-24 flex flex-col justify-center items-center ">
-            <span className="border border-dashed border-secondary rounded-full flex items-center justify-center w-16 h-16 bg-primary p-12 rounded-lg text-primary">
-              <Bag className="absolute" />
-            </span>
-            <h2 className="pt-6 text-2xl font-bold tracking-wide text-center">
-              Your cart is empty
-            </h2>
-            <p className="text-accents-6 px-10 text-center pt-2">
-              Biscuit oat cake wafer icing ice cream tiramisu pudding cupcake.
-            </p>
-          </div>
-        ) : error ? (
-          <div className="flex-1 px-4 flex flex-col justify-center items-center">
-            <span className="border border-white rounded-full flex items-center justify-center w-16 h-16">
-              <Cross width={24} height={24} />
-            </span>
-            <h2 className="pt-6 text-xl font-light text-center">
-              We couldn’t process the purchase. Please check your card
-              information and try again.
-            </h2>
-          </div>
-        ) : success ? (
-          <div className="flex-1 px-4 flex flex-col justify-center items-center">
-            <span className="border border-white rounded-full flex items-center justify-center w-16 h-16">
-              <Check />
-            </span>
-            <h2 className="pt-6 text-xl font-light text-center">
-              Thank you for your order.
-            </h2>
-          </div>
-        ) : (
-          <div className="px-4 sm:px-6 flex-1">
-            <Text variant="pageHeading">My Cart</Text>
-            <Text variant="sectionHeading">Review your Order</Text>
-            <ul className="py-6 space-y-6 sm:py-0 sm:space-y-0 sm:divide-y sm:divide-accents-2 border-b border-accents-2">
-              {items.map((item) => (
-                <CartItem
-                  key={item.id}
-                  item={item}
-                  currencyCode={data?.currency.code!}
-                />
-              ))}
-            </ul>
-            <div className="my-6">
-              <Text>
-                Before you leave, take a look at these items. We picked them
-                just for you
-              </Text>
-              <div className="flex py-6 space-x-6">
-                {[1, 2, 3, 4, 5, 6].map((x) => (
-                  <div className="border border-accents-3 w-full h-24 bg-accents-2 bg-opacity-50 transform cursor-pointer hover:scale-110 duration-75" />
+    <div className="px-4 pt-2 sm:px-6 flex-1">
+      <Text variant="pageHeading">My Cart</Text>
+      {error && <div className="mt-2">Failed to load</div>}
+      {isLoading && <div>Loading...</div>}
+      {isEmpty ? (
+        <div className="flex-1 px-12 py-24 flex flex-col justify-center items-center ">
+          <span className="border border-dashed border-secondary rounded-full flex items-center justify-center w-16 h-16 bg-primary p-12 rounded-lg text-primary">
+            <Bag className="absolute" />
+          </span>
+          <h2 className="pt-6 text-2xl font-bold tracking-wide text-center">
+            Your cart is empty
+          </h2>
+          <p className="text-accents-6 px-10 text-center pt-2">
+            Biscuit oat cake wafer icing ice cream tiramisu pudding cupcake.
+          </p>
+        </div>
+      ) : (
+        <div className="grid lg:grid-cols-12">
+          <div className="lg:col-span-8">
+            <div className="flex-1">
+              <Text variant="sectionHeading">Review your Order</Text>
+              <ul className="py-6 space-y-6 sm:py-0 sm:space-y-0 sm:divide-y sm:divide-accents-2 border-b border-accents-2">
+                {items.map((item) => (
+                  <CartItem
+                    key={item.id}
+                    item={item}
+                    currencyCode={data?.currency.code!}
+                  />
                 ))}
+              </ul>
+            </div>
+          </div>
+          <div className="lg:col-span-4">
+            <div className="flex-shrink-0 pt-10 sm:pl-16">
+              <div className="border-t border-accents-2">
+                <ul className="pt-5 pb-7">
+                  <li className="flex justify-between">
+                    <span>Subtotal</span>
+                    <span>{subTotal}</span>
+                  </li>
+                  <li className="flex justify-between mt-3">
+                    <span>Taxes</span>
+                    <span>Calculated at checkout</span>
+                  </li>
+                  <li className="flex justify-between mt-3">
+                    <span>Estimated Shipping</span>
+                    <span className="font-bold tracking-wide">FREE</span>
+                  </li>
+                </ul>
+                <div className="flex justify-between border-t border-accents-2 font-bold pt-6 mb-10">
+                  <span>Total</span>
+                  <span>{total}</span>
+                </div>
+              </div>
+              <div className="flex flex-row justify-end">
+                <div className="w-full lg:w-72">
+                  {isEmpty ? (
+                    <Button href="/" Component="a" width="100%">
+                      Continue Shopping
+                    </Button>
+                  ) : (
+                    <Button href="/checkout" Component="a" width="100%">
+                      Proceed to Checkout
+                    </Button>
+                  )}
+                </div>
               </div>
             </div>
           </div>
-        )}
-      </div>
-      <div className="lg:col-span-4">
-        <div className="flex-shrink-0 px-4 py-24 sm:px-6">
-          <div className="border-t border-accents-2">
-            <ul className="py-3">
-              <li className="flex justify-between py-1">
-                <span>Subtotal</span>
-                <span>{subTotal}</span>
-              </li>
-              <li className="flex justify-between py-1">
-                <span>Taxes</span>
-                <span>Calculated at checkout</span>
-              </li>
-              <li className="flex justify-between py-1">
-                <span>Estimated Shipping</span>
-                <span className="font-bold tracking-wide">FREE</span>
-              </li>
-            </ul>
-            <div className="flex justify-between border-t border-accents-2 py-3 font-bold mb-10">
-              <span>Total</span>
-              <span>{total}</span>
-            </div>
-          </div>
-          <div className="flex flex-row justify-end">
-            <div className="w-full lg:w-72">
-              {isEmpty ? (
-                <Button href="/" Component="a" width="100%">
-                  Continue Shopping
-                </Button>
-              ) : (
-                <Button href="/checkout" Component="a" width="100%">
-                  Proceed to Checkout
-                </Button>
-              )}
-            </div>
-          </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
