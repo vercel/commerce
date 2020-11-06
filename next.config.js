c2a115ca573d2c542737872979aa294aa4ab730e
Belen Curcio <bc@Belens-MacBook-Pro.local>
2020-11-06 20:09:33 -0300

    Adding Analyzer, Adding more Purge CSS Options

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const bundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: !!process.env.BUNDLE_ANALYZE
+})
+
+module.exports = module.exports = bundleAnalyzer({
   images: {
     domains: ['cdn11.bigcommerce.com'],
   },
@@ -50,4 +54,4 @@ module.exports = {
       },
     ]
   },
-}
+});
