b5fd6bd38e2f6be76410631448894bcb194ac672
Belen Curcio <curciobelen@gmail.com>
2020-11-26 13:13:30 -0300

    Removing HeadlessUI and React-Aria

--- pages/wishlist.tsx
+++ pages/wishlist.tsx
@@ -4,9 +4,9 @@ import getAllPages from '@bigcommerce/storefront-data-hooks/api/operations/get-a
 import useWishlist from '@bigcommerce/storefront-data-hooks/wishlist/use-wishlist'
 import { Layout } from '@components/common'
 import { Heart } from '@components/icons'
-import { Container, Text } from '@components/ui'
+import { Text } from '@components/ui'
 import { WishlistCard } from '@components/wishlist'
-import { Transition } from '@headlessui/react'
+import { defatultPageProps } from '@lib/defaults'
 
 export async function getStaticProps({
   preview,
@@ -15,7 +15,7 @@ export async function getStaticProps({
   const config = getConfig({ locale })
   const { pages } = await getAllPages({ config, preview })
   return {
-    props: { pages },
+    props: { ...defatultPageProps, pages },
   }
 }
 
@@ -23,54 +23,27 @@ export default function Wishlist() {
   const { data, isEmpty } = useWishlist({ includeProducts: true })
 
   return (
-    <Container>
-      <div className="mt-3 mb-20">
-        <Text variant="pageHeading">My Wishlist</Text>
-        <div className="group flex flex-col">
-          {isEmpty ? (
-            <Transition show>
-              <Transition.Child
-                enter="transition-opacity ease-linear duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="transition-opacity ease-linear duration-300"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <div className="flex-1 px-12 py-24 flex flex-col justify-center items-center ">
-                  <span className="border border-dashed border-secondary rounded-full flex items-center justify-center w-16 h-16 bg-primary p-12 rounded-lg text-primary">
-                    <Heart className="absolute" />
-                  </span>
-                  <h2 className="pt-6 text-2xl font-bold tracking-wide text-center">
-                    Your wishlist is empty
-                  </h2>
-                  <p className="text-accents-6 px-10 text-center pt-2">
-                    Biscuit oat cake wafer icing ice cream tiramisu pudding
-                    cupcake.
-                  </p>
-                </div>
-              </Transition.Child>
-            </Transition>
-          ) : (
-            <Transition show>
-              {data &&
-                data.items?.map((item) => (
-                  <Transition.Child
-                    enter="transition-opacity ease-linear duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="transition-opacity ease-linear duration-300"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <WishlistCard key={item.id} item={item} />
-                  </Transition.Child>
-                ))}
-            </Transition>
-          )}
-        </div>
+    <div className="mt-3 mb-20 px-3">
+      <Text variant="pageHeading">My Wishlist</Text>
+      <div className="group flex flex-col">
+        {isEmpty ? (
+          <div className="flex-1 px-12 py-24 flex flex-col justify-center items-center ">
+            <span className="border border-dashed border-secondary rounded-full flex items-center justify-center w-16 h-16 bg-primary p-12 rounded-lg text-primary">
+              <Heart className="absolute" />
+            </span>
+            <h2 className="pt-6 text-2xl font-bold tracking-wide text-center">
+              Your wishlist is empty
+            </h2>
+            <p className="text-accents-6 px-10 text-center pt-2">
+              Biscuit oat cake wafer icing ice cream tiramisu pudding cupcake.
+            </p>
+          </div>
+        ) : (
+          data &&
+          data.items?.map((item) => <WishlistCard key={item.id} item={item} />)
+        )}
       </div>
-    </Container>
+    </div>
   )
 }
 
