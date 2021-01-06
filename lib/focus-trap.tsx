87be4c8c5cf6bbb2d8a547f0e8c4e1b6fae9abac
Belen Curcio <curciobelen@gmail.com>
2021-01-06 11:58:42 -0300

    Adding delay for dynamic

--- lib/focus-trap.tsx
+++ lib/focus-trap.tsx
@@ -29,21 +29,22 @@ export default function FocusTrap({ children, focusFirst = false }: Props) {
 
   const selectFirstFocusableEl = () => {
     // Try to find focusable elements, if match then focus.
+    // Up to 4 seconds of load time threshold
     let match = false
-    let end = 20 // Try to find match at least n times.
+    let end = 22 // Try to find match at least n times.
     let i = 0
     const timer = setInterval(
       () => {
+        console.log('-----------', i)
         if (!match !== i > end) {
           match = !!tabbable(root.current).length
           if (root.current) {
             tabbable(root.current)[0].focus()
           }
+          i = i + 1
         } else {
           clearInterval(timer)
         }
-        i = i++
-        console.log('-----------', i)
       },
       i < 2 ? 0 : 200
     )
