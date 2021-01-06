1664a2377ab8b5f98370b3c81ef9291b87486f71
Belen Curcio <curciobelen@gmail.com>
2021-01-06 11:12:41 -0300

    Retry to focus

--- lib/focus-trap.tsx
+++ lib/focus-trap.tsx
@@ -3,9 +3,10 @@ import { tabbable } from 'tabbable'
 
 interface Props {
   children: React.ReactNode | any
+  focusFirst: boolean
 }
 
-export default function FocusTrap({ children }: Props) {
+export default function FocusTrap({ children, focusFirst = false }: Props) {
   const root: RefObject<any> = React.useRef()
   const anchor: RefObject<any> = React.useRef(document.activeElement)
 
@@ -17,9 +18,12 @@ export default function FocusTrap({ children }: Props) {
   }
 
   const trapFocus = () => {
+    // Focus the container element
     if (root.current) {
       root.current.focus()
-      selectFirstFocusableEl()
+      if (focusFirst) {
+        selectFirstFocusableEl()
+      }
     }
   }
 
