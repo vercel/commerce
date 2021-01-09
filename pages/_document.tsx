3f6c8db36d58c6ff68e5dda39607673fa382e328
Belen Curcio <curciobelen@gmail.com>
2021-01-09 12:26:37 -0300

    changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,4 @@
-import Document, {
-  DocumentContext,
-  Head,
-  Html,
-  Main,
-  NextScript,
-} from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   render() {
