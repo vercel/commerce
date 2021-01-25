8d48bc98aca04f45194340eaf09df55063fd491a
Michael Bromley <michael@michaelbromley.co.uk>
2021-01-25 15:25:41 +0100

    Implement useSearch

--- framework/vendure/product/use-search.tsx
+++ framework/vendure/product/use-search.tsx
@@ -2,11 +2,33 @@ import type { HookFetcher } from '@commerce/utils/types'
 import type { SwrOptions } from '@commerce/utils/use-data'
 import useCommerceSearch from '@commerce/products/use-search'
 import type { SearchProductsData } from '../api/catalog/products'
+import useResponse from '@commerce/utils/use-response'
 
-const defaultOpts = {
-  url: '/api/bigcommerce/catalog/products',
-  method: 'GET',
-}
+export const searchQuery = /* GraphQL */ `
+  query search($input: SearchInput!) {
+    search(input: $input) {
+      items {
+        productId
+        currencyCode
+        productName
+        description
+        priceWithTax {
+          ...on SinglePrice {
+            value
+          }
+          ...on PriceRange {
+            min max
+          }
+        }
+        productAsset {
+          preview
+        }
+        slug
+      }
+      totalItems
+    }
+  }
+`
 
 export type SearchProductsInput = {
   search?: string
@@ -20,40 +42,57 @@ export const fetcher: HookFetcher<SearchProductsData, SearchProductsInput> = (
   { search, categoryId, brandId, sort },
   fetch
 ) => {
-  // Use a dummy base as we only care about the relative path
-  const url = new URL(options?.url ?? defaultOpts.url, 'http://a')
-
-  if (search) url.searchParams.set('search', search)
-  if (Number.isInteger(categoryId))
-    url.searchParams.set('category', String(categoryId))
-  if (Number.isInteger(categoryId))
-    url.searchParams.set('brand', String(brandId))
-  if (sort) url.searchParams.set('sort', sort)
-
   return fetch({
-    url: url.pathname + url.search,
-    method: options?.method ?? defaultOpts.method,
+    query: searchQuery,
+    variables: {
+      input: {
+        term: search,
+        collectionId: categoryId,
+        groupByProduct: true
+      }
+    }
   })
 }
 
 export function extendHook(
   customFetcher: typeof fetcher,
-  swrOptions?: SwrOptions<SearchProductsData, SearchProductsInput>
+  swrOptions?: SwrOptions<any, SearchProductsInput>
 ) {
   const useSearch = (input: SearchProductsInput = {}) => {
     const response = useCommerceSearch(
-      defaultOpts,
+      {},
       [
         ['search', input.search],
         ['categoryId', input.categoryId],
         ['brandId', input.brandId],
-        ['sort', input.sort],
+        ['sort', input.sort]
       ],
       customFetcher,
       { revalidateOnFocus: false, ...swrOptions }
     )
 
-    return response
+    return useResponse(response, {
+      normalizer: data => {
+        return {
+          found: data?.search.totalItems > 0,
+          products: data?.search.items.map((item: any) => ({
+            id: item.productId,
+            name: item.productName,
+            description: item.description,
+            slug: item.slug,
+            path: item.slug,
+            images: [{ url: item.productAsset?.preview }],
+            variants: [],
+            price: {
+              value: (item.priceWithTax.min / 100),
+              currencyCode: item.currencyCode
+            },
+            options: [],
+            sku: item.sku
+          })) ?? [],
+        }
+      }
+    })
   }
 
   useSearch.extend = extendHook
