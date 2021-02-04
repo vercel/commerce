029a8372a23989d4f639eac4b56a70a2aebad02a
cond0r <pinte_catalin@yahoo.com>
2021-02-04 17:22:01 +0200

    Update get-checkout-id.ts

--- framework/shopify/utils/get-checkout-id.ts
+++ framework/shopify/utils/get-checkout-id.ts
@@ -1,8 +1,8 @@
 import Cookies from 'js-cookie'
-import { SHOPIFY_CHECKOUT_COOKIE } from '..'
+import { SHOPIFY_CHECKOUT_ID_COOKIE } from '../const'
 
 const getCheckoutId = (id?: string) => {
-  const checkoutID = id ?? Cookies.get(SHOPIFY_CHECKOUT_COOKIE)
+  const checkoutID = id ?? Cookies.get(SHOPIFY_CHECKOUT_ID_COOKIE)
   console.log(checkoutID)
   return checkoutID
 }
