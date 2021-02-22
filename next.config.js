43df2ba0f22204d0dbe28001d40cf66aaa95cded
cond0r <pinte_catalin@yahoo.com>
2021-02-22 16:29:38 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -34,4 +34,11 @@ module.exports = {
       },
     ]
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
