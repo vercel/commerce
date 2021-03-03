639863bb354fc6a90fae5b52ab0facecede974b7
cond0r <pinte_catalin@yahoo.com>
2021-03-03 20:36:36 +0200

    Update helpers.ts

--- components/product/helpers.ts
+++ components/product/helpers.ts
@@ -8,18 +8,18 @@ export type SelectedOptions = {
 export function getVariant(product: Product, opts: SelectedOptions) {
   const variant = product.variants.find((variant) => {
     return Object.entries(opts).every(([key, value]) =>
-      variant.options.find((option) => {
-        if (
-          option.__typename === 'MultipleChoiceOption' &&
-          option.displayName.toLowerCase() === key.toLowerCase()
-        ) {
-          return option.values.find((v) => v.label.toLowerCase() === value)
-        } else if (!value) {
-          return !variant.options.find(
-            (v) => v.displayName.toLowerCase() === key
+      value
+        ? variant.options.find((option) => {
+            if (
+              option.__typename === 'MultipleChoiceOption' &&
+              option.displayName.toLowerCase() === key.toLowerCase()
+            ) {
+              return option.values.find((v) => v.label.toLowerCase() === value)
+            }
+          })
+        : !variant.options.find(
+            (v) => v.displayName.toLowerCase() === key.toLowerCase()
           )
-        }
-      })
     )
   })
   return variant
