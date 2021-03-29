3d7a45550d7b228f5367b205c0357c70b8a30fa1
Nicolas Figueroa <figueroanicolas.a@gmail.com>
2021-03-29 17:47:27 -0300

    fix any for correct type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,15 @@ import 'keen-slider/keen-slider.min.css'
 
 import { FC, useEffect } from 'react'
 import type { AppProps } from 'next/app'
-import { Head } from '@components/common'
+import { Head, Layout } from '@components/common'
 import { ManagedUIContext } from '@components/ui/context'
 
-const Noop: FC = ({ children }) => <>{children}</>
+type LayoutT = FC & { Layout: typeof Layout }
 
+const Noop: FC = ({ children }) => <>{children}</>
+cl
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const Layout = (Component as any).Layout || Noop
+  const Layout = (Component as LayoutT).Layout || Noop
 
   useEffect(() => {
     document.body.classList?.remove('loading')
