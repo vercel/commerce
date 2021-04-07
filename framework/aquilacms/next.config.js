e1f2e16b3ddc2382f074be2854f9ad4fcafbbb4a
Gérard Le Cloerec <gerard.le.cloerec@gmail.com>
2021-04-07 16:12:57 +0200

    chore: add providers.aquila-cms.com to image domain

--- framework/aquilacms/next.config.js
+++ framework/aquilacms/next.config.js
@@ -3,6 +3,6 @@ const commerce = require('./commerce.config.json')
 module.exports = {
   commerce,
   images: {
-    domains: ['recdem.hosting-ns.com', '192.168.1.138'],
+    domains: ['recdem.hosting-ns.com', 'providers.aquila-cms.com'],
   },
 }
