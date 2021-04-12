aa507a7dbb91fda7ceeaa38e9b90d52b5317e7e3
Gérard Le Cloerec <gerard.le.cloerec@gmail.com>
2021-04-12 12:27:33 +0200

    chore: add domain provider-vercel.aquila-cms.com

--- framework/aquilacms/next.config.js
+++ framework/aquilacms/next.config.js
@@ -3,6 +3,11 @@ const commerce = require('./commerce.config.json')
 module.exports = {
   commerce,
   images: {
-    domains: ['recdem.hosting-ns.com', 'providers.aquila-cms.com'],
+    domains: [
+      'recdem.hosting-ns.com',
+      'providers.aquila-cms.com',
+      'provider-vercel.aquila-cms.com',
+      'localhost',
+    ],
   },
 }
