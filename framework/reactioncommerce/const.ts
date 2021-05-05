d5804d60a151f074292387a19720ad888315d1cd
Loan Laux <loan@outgrow.io>
2021-05-05 17:52:11 +0400

    update wording for Reaction API URL const/env var
    Signed-off-by: Loan Laux <loan@outgrow.io>

--- framework/reactioncommerce/const.ts
+++ framework/reactioncommerce/const.ts
@@ -7,10 +7,8 @@ export const REACTION_CART_ID_COOKIE = 'reaction_cartId'
 
 export const REACTION_EMPTY_DUMMY_CART_ID = 'DUMMY_EMPTY_CART_ID'
 
-export const STORE_DOMAIN = process.env.NEXT_PUBLIC_REACTION_STORE_DOMAIN
-
 export const REACTION_COOKIE_EXPIRE = 30
 
-export const API_URL = `http://${process.env.REACTION_STORE_DOMAIN}/graphql`
+export const API_URL = `http://${process.env.REACTION_API_DOMAIN}/graphql`
 
 export const SHOP_ID = process.env.REACTION_SHOP_ID ?? ''
