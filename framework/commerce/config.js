4f3674aafa2fa83495d4f17b30207fecd6b0e1cc
B <curciobelen@gmail.com>
2021-05-26 17:18:50 -0300

    New provider and tiny fixes. (#326)
    * changes
    
    * Adding new provider
    
    * Adding new provider
    
    * Adding new provider
    
    * Adding new provider

--- framework/commerce/config.js
+++ framework/commerce/config.js
@@ -7,7 +7,7 @@ const fs = require('fs')
 const merge = require('deepmerge')
 const prettier = require('prettier')
 
-const PROVIDERS = ['bigcommerce', 'shopify']
+const PROVIDERS = ['bigcommerce', 'shopify', 'swell']
 
 function getProviderName() {
   return (
@@ -16,6 +16,8 @@ function getProviderName() {
       ? 'bigcommerce'
       : process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN
       ? 'shopify'
+      : process.env.NEXT_PUBLIC_SWELL_STORE_ID
+      ? 'swell'
       : null)
   )
 }
