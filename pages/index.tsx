d71646a60d8a1b40c8bae76ff1570ead8df9d1fc
John Vandivier <vandivier_john@yahoo.com>
2021-05-31 22:31:16 -0400

    feat: concise card code (#348)

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,15 +52,7 @@ export default function Home({
       </Grid>
       <Marquee variant="secondary">
         {products.slice(0, 3).map((product, i) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            variant="slim"
-            imgProps={{
-              width: 320,
-              height: 320,
-            }}
-          />
+          <ProductCard key={product.id} product={product} variant="slim" />
         ))}
       </Marquee>
       <Hero
@@ -87,15 +79,7 @@ export default function Home({
       </Grid>
       <Marquee>
         {products.slice(0, 3).map((product, i) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            variant="slim"
-            imgProps={{
-              width: 320,
-              height: 320,
-            }}
-          />
+          <ProductCard key={product.id} product={product} variant="slim" />
         ))}
       </Marquee>
       {/* <HomeAllProductsGrid
