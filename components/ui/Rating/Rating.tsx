60c9fbca00e92baec0e5abb8867771b9f24d92cf
Bel Curcio <curciobel@gmail.com>
2021-06-04 14:56:39 -0300

    Rating Component

--- components/ui/Rating/Rating.tsx
+++ components/ui/Rating/Rating.tsx
@@ -12,6 +12,7 @@ const Quantity: FC<RatingProps> = ({ value = 5 }) => {
     <div className="flex flex-row py-6 text-accent-9">
       {rangeMap(5, (i) => (
         <span
+          key={`star_${i}`}
           className={cn('inline-block ml-1 ', {
             'text-accent-5': i >= Math.floor(value),
           })}
