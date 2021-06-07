633fd0c98d1a639511d49eef132ebe0b1c659454
Bel Curcio <curciobel@gmail.com>
2021-06-07 14:31:42 -0300

    Scroll for Mobile on IOs devises

--- components/ui/Sidebar/Sidebar.tsx
+++ components/ui/Sidebar/Sidebar.tsx
@@ -28,12 +28,14 @@ const Sidebar: FC<SidebarProps> = ({ children, onClose }) => {
   }, [])
 
   return (
-    <div className={cn(s.root)} ref={ref}>
+    <div className={cn(s.root)}>
       <div className="absolute inset-0 overflow-hidden">
         <div className={s.backdrop} onClick={onClose} />
         <section className="absolute inset-y-0 right-0 max-w-full flex outline-none pl-10">
           <div className="h-full w-full md:w-screen md:max-w-md">
-            <div className={s.sidebar}>{children}</div>
+            <div className={s.sidebar} ref={ref}>
+              {children}
+            </div>
           </div>
         </section>
       </div>
