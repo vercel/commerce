6656fe4878f4f23bcd0baea6368bc4c3f3a3929d
Bel Curcio <curciobel@gmail.com>
2021-06-08 13:06:23 -0300

    Search issue ui

--- pages/search.tsx
+++ pages/search.tsx
@@ -353,7 +353,9 @@ export default function Search({
           ) : (
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
               {rangeMap(12, (i) => (
-                <Skeleton key={i} />
+                <Skeleton key={i}>
+                  <div className="w-60 h-60" />
+                </Skeleton>
               ))}
             </div>
           )}
