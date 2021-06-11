c74c4b0e07dee18f1e00c6ea9c6b0d541ec9a74f
Bel Curcio <curciobel@gmail.com>
2021-06-11 13:57:59 -0300

    More updates

--- framework/local/api/operations/get-all-pages.ts
+++ framework/local/api/operations/get-all-pages.ts
@@ -1,8 +1,16 @@
 export type Page = any
 export type GetAllPagesResult = { pages: Page[] }
+import type { LocalConfig } from '../index'
 
 export default function getAllPagesOperation() {
-  function getAllPages(): Promise<GetAllPagesResult> {
+  function getAllPages({
+    config,
+    preview,
+  }: {
+    url?: string
+    config?: Partial<LocalConfig>
+    preview?: boolean
+  }): Promise<GetAllPagesResult> {
     return Promise.resolve({
       pages: [],
     })
--- framework/local/api/operations/get-site-info.ts
+++ framework/local/api/operations/get-site-info.ts
@@ -1,5 +1,6 @@
 import { OperationContext } from '@commerce/api/operations'
 import { Category } from '@commerce/types/site'
+import { LocalConfig } from '../index'
 
 export type GetSiteInfoResult<
   T extends { categories: any[]; brands: any[] } = {
@@ -9,7 +10,16 @@ export type GetSiteInfoResult<
 > = T
 
 export default function getSiteInfoOperation({}: OperationContext<any>) {
-  function getSiteInfo(): Promise<GetSiteInfoResult> {
+  function getSiteInfo({
+    query,
+    variables,
+    config: cfg,
+  }: {
+    query?: string
+    variables?: any
+    config?: Partial<LocalConfig>
+    preview?: boolean
+  } = {}): Promise<GetSiteInfoResult> {
     return Promise.resolve({
       categories: [],
       brands: [],
