e112efed8ef0a2a92d0b8d24ba7c8b7c316811d4
Tobias Koppers <tobias.koppers@googlemail.com>
2021-06-14 23:06:54 +0200

    avoid circular dependency in bigcommerce

--- framework/bigcommerce/api/utils/fetch-graphql-api.ts
+++ framework/bigcommerce/api/utils/fetch-graphql-api.ts
@@ -1,38 +1,36 @@
 import { FetcherError } from '@commerce/utils/errors'
 import type { GraphQLFetcher } from '@commerce/api'
-import { provider } from '..'
+import type { BigcommerceConfig } from '../index'
 import fetch from './fetch'
 
-const fetchGraphqlApi: GraphQLFetcher = async (
-  query: string,
-  { variables, preview } = {},
-  fetchOptions
-) => {
-  // log.warn(query)
-  const { config } = provider
-  const res = await fetch(config.commerceUrl + (preview ? '/preview' : ''), {
-    ...fetchOptions,
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${config.apiToken}`,
-      ...fetchOptions?.headers,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      query,
-      variables,
-    }),
-  })
-
-  const json = await res.json()
-  if (json.errors) {
-    throw new FetcherError({
-      errors: json.errors ?? [{ message: 'Failed to fetch Bigcommerce API' }],
-      status: res.status,
+const fetchGraphqlApi: (getConfig: () => BigcommerceConfig) => GraphQLFetcher =
+  (getConfig) =>
+  async (query: string, { variables, preview } = {}, fetchOptions) => {
+    // log.warn(query)
+    const config = getConfig()
+    const res = await fetch(config.commerceUrl + (preview ? '/preview' : ''), {
+      ...fetchOptions,
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${config.apiToken}`,
+        ...fetchOptions?.headers,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
     })
-  }
 
-  return { data: json.data, res }
-}
+    const json = await res.json()
+    if (json.errors) {
+      throw new FetcherError({
+        errors: json.errors ?? [{ message: 'Failed to fetch Bigcommerce API' }],
+        status: res.status,
+      })
+    }
+
+    return { data: json.data, res }
+  }
 
 export default fetchGraphqlApi
