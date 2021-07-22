e8bd59f2a8fcf6c1a805b711ba9185ca96c684b9
Bayram Muhiyev <bay-co@mail.ru>
2021-07-22 22:29:04 +0500

    Added s3 url to images config

--- framework/vendure/next.config.js
+++ framework/vendure/next.config.js
@@ -3,6 +3,6 @@ const commerce = require('./commerce.config.json')
 module.exports = {
   commerce,
   images: {
-    domains: ['localhost', 'vendure-admin-dap.herokuapp.com'],
+    domains: ['localhost', 'vendure-admin-dap.herokuapp.com', 'dap-storage.s3.eu-west-1.amazonaws.com'],
   },
 }
