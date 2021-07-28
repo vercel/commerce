7510cb03a86319c9509b7fda406cfff5fc04c63d
GunaTrika <guna.r@trikatechnologies.com>
2021-07-28 15:53:54 +0530

    Enabled get all products with sdk. but needs normalizing Added vs code debug script

--- framework/elasticpath/api/operations/get-all-products.ts
+++ framework/elasticpath/api/operations/get-all-products.ts
@@ -2,7 +2,12 @@ import { Product } from '@commerce/types/product'
 import { GetAllProductsOperation } from '@commerce/types/product'
 import type { OperationContext } from '@commerce/api/operations'
 import type { ElasticpathConfig, Provider } from '../index'
+import { gateway as MoltinGateway } from '@moltin/sdk'
 import data from '../../data.json'
+
+const Moltin = MoltinGateway({
+  client_id: process.env.NEXT_PUBLIC_ELASTICPATH_CLIENTID
+})
 
 export default function getAllProductsOperation({
   commerce,
@@ -17,6 +22,9 @@ export default function getAllProductsOperation({
     config?: Partial<ElasticpathConfig>
     preview?: boolean
   } = {}): Promise<{ products: Product[] | any[] }> {
+    //elastic path get all products
+    let products = await Moltin.Products.Limit(200).All();
+    console.log("All products", products);
     return {
       products: data.products,
     }
