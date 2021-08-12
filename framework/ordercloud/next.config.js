4151865fa269f0b327c4c4a182804b3409fdf561
goncy <gonzalo.pozzo4@gmail.com>
2021-08-12 10:45:26 -0300

    Update images origins

--- framework/ordercloud/next.config.js
+++ framework/ordercloud/next.config.js
@@ -3,15 +3,6 @@ const commerce = require('./commerce.config.json')
 module.exports = {
   commerce,
   images: {
-    domains: [
-      'localhost',
-      // TODO: Remove this
-      'images.bloomingdalesassets.com',
-      'pbs.twimg.com',
-      'images.asos-media.com',
-      'di2ponv0v5otw.cloudfront.net',
-      'cdn.shopify.com',
-      'encrypted-tbn0.gstatic.com',
-    ],
+    domains: ['localhost', 'ocdevops.blob.core.windows.net'],
   },
 }
