eed70795ae1eef66b501cde0bdcde9c2e4f5567c
lytrankieio123 <lytran@kie.io>
2021-08-20 18:58:23 +0700

    styles: button common

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 
 import { ButtonCommon, Layout } from 'src/components/common'
+import { ButonType, ButtonSize } from 'src/utils/constanst.utils'
 export default function Home() {
   return (
     <>
       <div>This is home page</div>
       <ButtonCommon>Button default</ButtonCommon>
+      <ButtonCommon type={ButonType.light}>Button light</ButtonCommon>
+      <ButtonCommon size={ButtonSize.large}>Button default large</ButtonCommon>
       <p>Go to <code>pages/index.tsx</code> to get your hand dirty!</p>
       <p>Go to <code>src/components</code> to make your awesome component!</p>
       <p>Go to <code>src/styles</code> to find global styles!</p>
