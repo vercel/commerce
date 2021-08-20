d8ec1fa7174462638da408d982858da89940cd2b
lytrankieio123 <lytran@kie.io>
2021-08-20 09:36:44 +0700

    :wrench: config: tailwind purge path
    :%s

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     content: [
       './pages/**/*.{js,ts,jsx,tsx}',
       './components/**/*.{js,ts,jsx,tsx}',
+      './src/components/**/*.{js,ts,jsx,tsx}',
     ],
     options: {
       safelist: {
