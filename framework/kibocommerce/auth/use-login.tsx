4cdce5ed392689edb98fc9341036e7ebbca19de7
SushantJadhav <Sushant.Jadhav@kibocommerce.com>
2021-08-25 10:28:44 +0530

    Initial Commit

--- framework/kibocommerce/auth/use-login.tsx
+++ framework/kibocommerce/auth/use-login.tsx
@@ -1,16 +1,42 @@
 import { MutationHook } from '@commerce/utils/types'
 import useLogin, { UseLogin } from '@commerce/auth/use-login'
 
+import { useCallback } from 'react'
+import { CommerceError } from '@commerce/utils/errors'
+import type { LoginHook } from '../types/login'
+import useCustomer from '../customer/use-customer'
+
 export default useLogin as UseLogin<typeof handler>
 
-export const handler: MutationHook<any> = {
+export const handler: MutationHook<LoginHook> = {
   fetchOptions: {
-    query: '',
+    url: '/api/login',
+    method: 'POST',
+    query: ''
   },
-  async fetcher() {
-    return null
+  async fetcher({ input: { email, password }, options, fetch }) {
+    if (!(email && password)) {
+      throw new CommerceError({
+        message:
+          'An email and password are required to login',
+      })
+    }
+
+    return fetch({
+      ...options,
+      body: { email, password },
+    })
   },
-  useHook: () => () => {
-    return async function () {}
+  useHook: ({ fetch }) => () => {
+    const { revalidate } = useCustomer()
+
+    return useCallback(
+      async function login(input) {
+        const data = await fetch({ input })
+        // await revalidate()
+        return data
+      },
+      [fetch, revalidate]
+    )
   },
 }
