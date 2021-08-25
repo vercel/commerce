d345020e6e45248fba85a52b4f7ca95109ef771a
lytrankieio123 <lytran@kie.io>
2021-08-25 18:54:48 +0700

    :art: styles: home banner (in progress)
    :%s

--- src/components/modules/home/HomeBanner/HomeBanner.tsx
+++ src/components/modules/home/HomeBanner/HomeBanner.tsx
@@ -1,14 +1,26 @@
-import { FC } from 'react'
-import s from './HomeBanner.module.css'
+import React from 'react'
+import { Banner } from 'src/components/common'
+import s from './HomeBanner.module.scss'
 
 interface Props {
     className?: string
     children?: any
 }
 
-const HomeBanner: FC<Props> = ({ }) => {
+const HomeBanner = ({ }: Props) => {
     return (
-        <div className={s.homeBanner}>This is HomeBanner</div>
+        <div className={s.homeBanner}>
+            <section className={s.left}>
+                <div className={s.text}>
+                    Freshness<br/>guaranteed
+                </div>
+            </section >
+            <Banner
+                title="Save 15% on your first order"
+                subtitle="Last call! Shop deep deals on 100+ bulk picks while you can."
+                imgLink="https://user-images.githubusercontent.com/76729908/130574371-3b75fa72-9552-4605-aba9-a4b31cd9dce7.png"
+            />
+        </div >
     )
 }
 
