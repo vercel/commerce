047da49e76bc1e5c06a5a2bcaaa77090011b30fd
Luis Alvarez <luis@vercel.com>
2021-08-27 13:07:40 -0500

    Updated saleor setup

--- framework/saleor/index.tsx
+++ framework/saleor/index.tsx
@@ -1,32 +1,9 @@
-import * as React from 'react'
-import { ReactNode } from 'react'
-
-import { CommerceConfig, CommerceProvider as CoreCommerceProvider, useCommerce as useCoreCommerce } from '@commerce'
-
+import { getCommerceProvider, useCommerce as useCoreCommerce } from '@commerce'
 import { saleorProvider, SaleorProvider } from './provider'
-import * as Const from './const'
 
 export { saleorProvider }
 export type { SaleorProvider }
 
-export const saleorConfig: CommerceConfig = {
-  locale: 'en-us',
-  cartCookie: Const.CHECKOUT_ID_COOKIE,
-}
+export const CommerceProvider = getCommerceProvider(saleorProvider)
 
-export type SaleorConfig = Partial<CommerceConfig>
-
-export type SaleorProps = {
-  children?: ReactNode
-  locale: string
-} & SaleorConfig
-
-export function CommerceProvider({ children, ...config }: SaleorProps) {
-  return (
-    <CoreCommerceProvider provider={saleorProvider} config={{ ...saleorConfig, ...config }}>
-      {children}
-    </CoreCommerceProvider>
-  )
-}
-
-export const useCommerce = () => useCoreCommerce()
+export const useCommerce = () => useCoreCommerce<SaleorProvider>()
