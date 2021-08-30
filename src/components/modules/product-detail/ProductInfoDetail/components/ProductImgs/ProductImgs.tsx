a7d0eb051c3e040c12e7708874d8531f97b70e91
lytrankieio123 <lytran@kie.io>
2021-08-30 18:32:45 +0700

    :art: styles: product detail imgs
    :%s

--- src/components/modules/product-detail/ProductInfoDetail/components/ProductImgs/ProductImgs.tsx
+++ src/components/modules/product-detail/ProductInfoDetail/components/ProductImgs/ProductImgs.tsx
@@ -1,22 +1,38 @@
 import React from 'react'
-import { CarouselCommon } from 'src/components/common'
+import { CarouselCommon, ImgWithLink } from 'src/components/common'
+import { ImgWithLinkProps } from 'src/components/common/ImgWithLink/ImgWithLink'
 import s from './ProductImgs.module.scss'
-
-interface ImgProps {
-    src: string, alt?: string
-}
 
 interface Props {
     className?: string
     children?: any,
-    // data: ImgProps[]
+}
+
+const DATA = [
+    {
+        src: 'https://user-images.githubusercontent.com/76729908/130574371-3b75fa72-9552-4605-aba9-a4b31cd9dce7.png',
+        alt: 'Broccoli',
+    },
+    {
+        src: 'https://user-images.githubusercontent.com/76729908/130574371-3b75fa72-9552-4605-aba9-a4b31cd9dce7.png',
+        alt: 'Broccoli',
+    }
+]
+
+const option = {
+    slidesPerView: 1,
 }
 
 const ProductImgs = ({ }: Props) => {
     return (
         <section className={s.productImgs}>
-            {/* <CarouselCommon<ImgProps>  /> */}
-            <img className={s.img} src="https://user-images.githubusercontent.com/76729908/130574371-3b75fa72-9552-4605-aba9-a4b31cd9dce7.png" alt="" />
+            <CarouselCommon<ImgWithLinkProps>
+                data={DATA}
+                itemKey="product-detail-img"
+                Component={ImgWithLink}
+                option={option}
+                isDot={true}
+            />
         </section >
     )
 }
