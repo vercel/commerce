187739c9515b23d0b7cbc5091668562cf9aea0df
Bolaji Ayodeji <sirbeejay1@gmail.com>
2021-09-02 12:37:43 +0100

    fix: fix issue with /cart page

--- framework/commercelayer/cart/use-cart.tsx
+++ framework/commercelayer/cart/use-cart.tsx
@@ -53,7 +53,7 @@ export const handler: SWRHook<any> = {
           Object.create(response, {
             isEmpty: {
               get() {
-                return response.data.lineItems.length === 0
+                return response.data?.lineItems.length === 0
               },
               enumerable: true,
             },
