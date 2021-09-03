abaf7220dca20a67fa587933b06e2e15a6447d03
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2021-09-03 16:36:22 +0200

    merge main (#1)
    * Update README.md to include information on how to add a custom provider (#250)
    
    * Update README.md
    
    * Updated commerce readme
    
    Co-authored-by: Luis Alvarez D <luis@vercel.com>
    
    * Remove unused provider config (#459)
    
    * Updated core types for commerce provider
    
    * Updated bigcommerce provider
    
    * Added util method for provider creation
    
    * Removed unrequired code from saleor
    
    * Updated shopify provider
    
    * Updated providers and local setup
    
    * Updated saleor setup
    
    * Updated swell
    
    * Updated vendure
    
    * Updated swell-js usage
    
    * Removed unrequired import from saleor
    
    Co-authored-by: Isiko Saidiali <54436062+SaidiAli@users.noreply.github.com>
    Co-authored-by: Luis Alvarez D <luis@vercel.com>

--- framework/commerce/index.tsx
+++ framework/commerce/index.tsx
@@ -47,49 +47,58 @@ export type Provider = CommerceConfig & {
   }
 }
 
-export type CommerceProps<P extends Provider> = {
-  children?: ReactNode
-  provider: P
-  config: CommerceConfig
-}
-
-export type CommerceConfig = Omit<
-  CommerceContextValue<any>,
-  'providerRef' | 'fetcherRef'
->
-
-export type CommerceContextValue<P extends Provider> = {
-  providerRef: MutableRefObject<P>
-  fetcherRef: MutableRefObject<Fetcher>
+export type CommerceConfig = {
   locale: string
   cartCookie: string
 }
 
-export function CommerceProvider<P extends Provider>({
+export type CommerceContextValue<P extends Provider> = {
+  providerRef: MutableRefObject<P>
+  fetcherRef: MutableRefObject<Fetcher>
+} & CommerceConfig
+
+export type CommerceProps<P extends Provider> = {
+  children?: ReactNode
+  provider: P
+}
+
+/**
+ * These are the properties every provider should allow when implementing
+ * the core commerce provider
+ */
+export type CommerceProviderProps = {
+  children?: ReactNode
+} & Partial<CommerceConfig>
+
+export function CoreCommerceProvider<P extends Provider>({
   provider,
   children,
-  config,
 }: CommerceProps<P>) {
-  if (!config) {
-    throw new Error('CommerceProvider requires a valid config object')
-  }
-
   const providerRef = useRef(provider)
   // TODO: Remove the fetcherRef
   const fetcherRef = useRef(provider.fetcher)
-  // Because the config is an object, if the parent re-renders this provider
-  // will re-render every consumer unless we memoize the config
+  // If the parent re-renders this provider will re-render every
+  // consumer unless we memoize the config
+  const { locale, cartCookie } = providerRef.current
   const cfg = useMemo(
-    () => ({
-      providerRef,
-      fetcherRef,
-      locale: config.locale,
-      cartCookie: config.cartCookie,
-    }),
-    [config.locale, config.cartCookie]
+    () => ({ providerRef, fetcherRef, locale, cartCookie }),
+    [locale, cartCookie]
   )
 
   return <Commerce.Provider value={cfg}>{children}</Commerce.Provider>
+}
+
+export function getCommerceProvider<P extends Provider>(provider: P) {
+  return function CommerceProvider({
+    children,
+    ...props
+  }: CommerceProviderProps) {
+    return (
+      <CoreCommerceProvider provider={{ ...provider, ...props }}>
+        {children}
+      </CoreCommerceProvider>
+    )
+  }
 }
 
 export function useCommerce<P extends Provider>() {
--- framework/vendure/index.tsx
+++ framework/vendure/index.tsx
@@ -1,33 +1,9 @@
-import * as React from 'react'
-import { ReactNode } from 'react'
-import {
-  CommerceConfig,
-  CommerceProvider as CoreCommerceProvider,
-  useCommerce as useCoreCommerce,
-} from '@commerce'
-import { vendureProvider } from './provider'
+import { getCommerceProvider, useCommerce as useCoreCommerce } from '@commerce'
+import { vendureProvider, VendureProvider } from './provider'
 
-export const vendureConfig: CommerceConfig = {
-  locale: 'en-us',
-  cartCookie: 'session',
-}
+export { vendureProvider }
+export type { VendureProvider }
 
-export type VendureConfig = Partial<CommerceConfig>
-
-export type VendureProps = {
-  children?: ReactNode
-  locale: string
-} & VendureConfig
-
-export function CommerceProvider({ children, ...config }: VendureProps) {
-  return (
-    <CoreCommerceProvider
-      provider={vendureProvider}
-      config={{ ...vendureConfig, ...config }}
-    >
-      {children}
-    </CoreCommerceProvider>
-  )
-}
+export const CommerceProvider = getCommerceProvider(vendureProvider)
 
 export const useCommerce = () => useCoreCommerce()
