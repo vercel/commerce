7b5e7af8f9f48b179a4734e4bbec8cf3e80027cf
lytrankieio123 <tranly237@gmail.com>
2021-09-06 12:55:49 +0700

    :art: styles: page product detail
    :%s

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -1,10 +1,15 @@
 
-import { Layout } from 'src/components/common'
-import { ProductInfoDetail } from 'src/components/modules/product-detail'
+import { Layout, RecipeDetail } from 'src/components/common'
+import { ProductInfoDetail, RecommendedRecipes, ReleventProducts, ViewedProducts } from 'src/components/modules/product-detail'
+import { INGREDIENT_DATA_TEST, RECIPE_DATA_TEST } from 'src/utils/demo-data'
 
 export default function Slug() {
   return <>
-    <ProductInfoDetail/>
+    <ProductInfoDetail />
+    <RecipeDetail ingredients={INGREDIENT_DATA_TEST} />
+    <RecommendedRecipes data={RECIPE_DATA_TEST} />
+    <ReleventProducts />
+    <ViewedProducts />
   </>
 }
 
