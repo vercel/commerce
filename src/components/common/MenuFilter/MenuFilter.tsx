5261cb04bc522805babb4e12784e42a24a9de59e
quangnhankie <nhantran@kie.io>
2021-09-07 11:31:58 +0700

    fix: fix ClassNames menuFilter

--- src/components/common/MenuFilter/MenuFilter.tsx
+++ src/components/common/MenuFilter/MenuFilter.tsx
@@ -1,43 +1,30 @@
 import classNames from 'classnames'
-import { useEffect, useState } from 'react';
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import s from './MenuFilter.module.scss'
+
+
 interface Props {
     children?: any,
-    heading?:string,
-    categories:{name:string,link:string}[],
-    type:string,
-    onChangeValue?: (value: Object) => void
+    heading:string,
+    categories:{name:string,link:string}[]
 }
 
-const MenuFilter = ({heading,categories,type,onChangeValue}:Props)=> {
-    const [active, setActive] = useState<string>('');
+const MenuFilter = ({heading,categories}:Props)=> {
+    const router = useRouter()
 
-    function handleClick(link:string){
-        setActive(link);
-
-        if(active === link){
-            setActive('');
-        }
-    }
-
-    useEffect(()=>{
-      
-        let href = active?.split("=");
-        const linkValue = href[1];
-
-        onChangeValue && onChangeValue({[type]:linkValue});
-    },[active]) 
-  
     return (
         <section className={s.menuFilterWrapper}>
             <h2 className={s.menuFilterHeading}>{heading}</h2>
             <ul className={s.menuFilterList}>
                 {
                     categories.map(item => <li key={item.name}>
-                        <div onClick={()=> handleClick(item.link)} className={classNames({ [s.active]: item.link === active? true: false })}>
-                            {item.name}
-                        </div>
+                        <Link href={item.link}>
+                            <a className={classNames({ [s.active]: router.asPath === item.link})}>
+                                {item.name}
+                            </a>
+                        </Link>
                     </li>)
                 }
             </ul>
