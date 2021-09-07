90f45b433432319ae86794a6e402f12126b88c9d
quangnhankie <nhantran@kie.io>
2021-09-07 11:52:52 +0700

    fix: revert file error

--- src/utils/language.utils.ts
+++ src/utils/language.utils.ts
@@ -2,12 +2,9 @@ export const LANGUAGE = {
     BUTTON_LABEL: {
         BUY_NOW: 'Buy now',
         SHOP_NOW: 'Shop now',
-<<<<<<< HEAD
-        CONFIRM:'Confirm'
-=======
+        CONFIRM:'Confirm',
         ADD_TO_CARD: 'Add to Cart',
         PREORDER: 'Pre-Order Now',
->>>>>>> a9f9f06eb9dee2a1ddefe907ff804237a78c5210
     },
     PLACE_HOLDER: {
         SEARCH: 'Search',
