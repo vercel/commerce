51c8642d246ca533eb276a76aa546fb694712dc5
unknown <shioryuu0302@gmail.com>
2021-09-08 20:08:26 +0700

    styles: add padding DeliveryAndPolicyContent

--- src/components/modules/delivery-policy/index.tsx
+++ src/components/modules/delivery-policy/index.tsx
@@ -1,3 +1,2 @@
 export { default as DeliveryAndPolicyContent } from './DeliveryAndPolicyContent/DeliveryAndPolicyContent'
 export { default as DeliveryAndPolicyBreadCrumb } from './DeliveryAndPolicyBreadCrumb/DeliveryAndPolicyBreadCrumb'
-export { default as DeliveryAndPolicyPage } from './DeliveryAndPolicyPage/DeliveryAndPolicyPage'
