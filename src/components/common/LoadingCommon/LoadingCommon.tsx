d6686b8e2dfd4fe59e864814f8635a95bba7b759
sonnguyenkieio <sonnguyen@kie.io>
2021-09-09 08:25:28 +0700

    :sparkles: feat: Loading Common

--- src/components/common/LoadingCommon/LoadingCommon.tsx
+++ src/components/common/LoadingCommon/LoadingCommon.tsx
@@ -3,17 +3,16 @@ import React from "react";
 import s from './LoadingCommon.module.scss'
 
 interface LoadingCommonProps {
-    visible: boolean
     size?: "small" | "default" | "large",
     children? : React.ReactNode;
 }
 
-const LoadingCommon = ({ visible, size="default", children }: LoadingCommonProps) => {
+const LoadingCommon = ({ size="default", children }: LoadingCommonProps) => {
     
     return (
         <>
         {
-            visible && <div className={classNames(s.loadingCommon, {
+            <div className={classNames(s.loadingCommon, {
                 [s[size]]: size
             })}>
                 {children}
