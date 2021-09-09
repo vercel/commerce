985f8861c802fd0a9f79fc48a780ace19706d977
lytrankieio123 <lytran@kie.io>
2021-09-09 14:33:52 +0700

    :bug: bug: fix header animation
    :%s

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import React, { memo, useEffect, useState } from 'react'
+import React, { memo, useEffect, useMemo, useRef, useState } from 'react'
 import { useModalCommon } from 'src/components/hooks'
 import { isMobile } from 'src/utils/funtion.utils'
 import ModalAuthenticate from '../ModalAuthenticate/ModalAuthenticate'
@@ -12,40 +12,54 @@ import s from './Header.module.scss'
 
 
 const Header = memo(() => {
+    const headeFullRef = useRef<HTMLDivElement>(null)
     const [isFullHeader, setIsFullHeader] = useState<boolean>(true)
     const { visible: visibleModalAuthen, closeModal: closeModalAuthen, openModal: openModalAuthen } = useModalCommon({ initialValue: false })
     const { visible: visibleModalInfo, closeModal: closeModalInfo, openModal: openModalInfo } = useModalCommon({ initialValue: false })
 
+    const headerHeight = useMemo(() => {
+        return headeFullRef.current?.offsetHeight
+    }, [headeFullRef])
+
     useEffect(() => {
+        const handleScroll = () => {
+            if (!isMobile()) {
+                if (!headerHeight || window.scrollY > headerHeight) {
+                    setIsFullHeader(false)
+                } else {
+                    setIsFullHeader(true)
+                }
+            }
+        }
         window.addEventListener('scroll', handleScroll)
         return () => {
             window.removeEventListener('scroll', handleScroll)
         }
-    }, [])
+    }, [headerHeight])
 
-    const handleScroll = () => {
-        if (!isMobile()) {
-            if (window.scrollY === 0) {
-                setIsFullHeader(true)
-            } else {
-                setIsFullHeader(false)
-            }
-        }
-    }
     return (
         <>
-            <header className={classNames({ [s.header]: true, [s.full]: isFullHeader })}>
-                <HeaderHighLight isShow={isFullHeader} />
+            <header ref={headeFullRef} className={classNames({ [s.header]: true, [s.full]: isFullHeader })}>
+                <HeaderHighLight />
                 <div className={s.menu}>
                     <HeaderMenu isFull={isFullHeader}
                         openModalAuthen={openModalAuthen}
                         openModalInfo={openModalInfo} />
-                    <HeaderSubMenu isShow={isFullHeader} />
+                    <HeaderSubMenu/>
                 </div>
             </header>
+            <div className={classNames({
+                headerSticky: true,
+                show: !isFullHeader
+            })}>
+                <HeaderMenu isFull={isFullHeader}
+                    openModalAuthen={openModalAuthen}
+                    openModalInfo={openModalInfo} />
+            </div>
+
             <HeaderSubMenuMobile />
             <ModalAuthenticate visible={visibleModalAuthen} closeModal={closeModalAuthen} />
-            <ModalCreateUserInfo demoVisible={visibleModalInfo} demoCloseModal={closeModalInfo}/>
+            <ModalCreateUserInfo demoVisible={visibleModalInfo} demoCloseModal={closeModalInfo} />
         </>
     )
 })
