50725d982f616cd8f42dd7a34585b9993d8cf12d
lytrankieio123 <lytran@kie.io>
2021-09-13 10:30:09 +0700

    styles: banner common

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -1,7 +1,7 @@
 
-import { Layout, RecipeDetail } from 'src/components/common'
+import { Layout, RecipeDetail, RelevantBlogPosts } from 'src/components/common'
 import { ProductInfoDetail, RecommendedRecipes, ReleventProducts, ViewedProducts } from 'src/components/modules/product-detail'
-import { INGREDIENT_DATA_TEST, RECIPE_DATA_TEST } from 'src/utils/demo-data'
+import { BLOGS_DATA_TEST, INGREDIENT_DATA_TEST, RECIPE_DATA_TEST } from 'src/utils/demo-data'
 
 export default function Slug() {
   return <>
@@ -10,6 +10,7 @@ export default function Slug() {
     <RecommendedRecipes data={RECIPE_DATA_TEST} />
     <ReleventProducts />
     <ViewedProducts />
+    <RelevantBlogPosts data={BLOGS_DATA_TEST} title="You will like also" bgcolor="cream"/>
   </>
 }
 
