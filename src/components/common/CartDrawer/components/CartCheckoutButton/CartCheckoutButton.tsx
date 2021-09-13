9336462af17bee9f9e8b480d3952fbad17dae465
lytrankieio123 <lytran@kie.io>
2021-09-13 16:13:07 +0700

    :art: styles: cart drawer responsive
    :%s

--- src/components/common/CartDrawer/components/CartCheckoutButton/CartCheckoutButton.tsx
+++ src/components/common/CartDrawer/components/CartCheckoutButton/CartCheckoutButton.tsx
@@ -1,12 +1,16 @@
 import React, { memo } from 'react';
 import { ButtonCommon } from 'src/components/common';
 import s from './CartCheckoutButton.module.scss';
+import Link from 'next/link'
+import { ROUTE } from 'src/utils/constanst.utils';
 
 const CartCheckoutButton = memo(() => {
   return (
-    <div className={s.cartCheckoutButton}>
-      <ButtonCommon size='large'>Check out - Rp 120.500</ButtonCommon>
-    </div>
+    <Link href={ROUTE.CHECKOUT}>
+      <a className={s.cartCheckoutButton}>
+        <ButtonCommon size='large'>Check out - Rp 120.500</ButtonCommon>
+      </a>
+    </Link>
   )
 })
 
