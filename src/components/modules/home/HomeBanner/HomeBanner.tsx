0eddfe7c91638822ca9a2f64568965530089a848
lytrankieio123 <lytran@kie.io>
2021-09-13 10:30:19 +0700

    styles: banner home

--- src/components/modules/home/HomeBanner/HomeBanner.tsx
+++ src/components/modules/home/HomeBanner/HomeBanner.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Banner } from 'src/components/common'
+import { Banner, StaticImage } from 'src/components/common'
 import BannerImgRight from './assets/banner_full.png'
 import HomeBannerImg from './assets/home_banner.png'
 import s from './HomeBanner.module.scss'
 import Image from 'next/image'
+import { ROUTE } from 'src/utils/constanst.utils'
 
 interface Props {
     className?: string
@@ -15,7 +16,7 @@ const HomeBanner = ({ }: Props) => {
         <div className={s.homeBanner}>
             <section className={s.left}>
                 <div className={s.imgWrap}>
-                    <Image src={HomeBannerImg} placeholder='blur' />
+                    <StaticImage src={HomeBannerImg} />
                 </div>
                 <div className={s.text}>
                     Freshness<br />guaranteed
@@ -28,12 +29,14 @@ const HomeBanner = ({ }: Props) => {
                         subtitle: "Last call! Shop deep deals on 100+ bulk picks while you can.",
                         imgLink: BannerImgRight.src,
                         size: "small",
+                        linkButton: ROUTE.PRODUCTS,
                     },
                     {
                         title: "Save 15% on your first order 2",
                         subtitle: "Last call! Shop deep deals on 100+ bulk picks while you can.",
                         imgLink: BannerImgRight.src,
                         size: "small",
+                        linkButton: ROUTE.PRODUCTS,
                     }
                     ]
                 }
