1518629590211d71cb7a5be8cd454b451b2b4201
sonnguyenkieio <sonnguyen@kie.io>
2021-09-14 16:59:16 +0700

    :sparkles: feat: Loading Common

--- src/components/common/LoadingCommon/LoadingCommon.tsx
+++ src/components/common/LoadingCommon/LoadingCommon.tsx
@@ -6,9 +6,8 @@ const LoadingCommon = () => {
     return (
         <div className={s.wrapper}>
             <div className={s.loadingCommon}>
-            
             </div>
-            <p>Loading...</p>
+            <p className={s.text}>Loading...</p>
         </div>
     )
 }
