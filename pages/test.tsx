fa0fa859cbde013742225cd41d466c2ad4c24577
sonnguyenkieio <sonnguyen@kie.io>
2021-09-14 14:45:23 +0700

    update

--- pages/test.tsx
+++ pages/test.tsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import { ButtonCommon, Layout } from 'src/components/common'
 
 import CheckoutSuccess from 'src/components/modules/checkout/CheckoutSuccess/CheckoutSuccess'
+import LoadingCommon from 'src/components/common/LoadingCommon/LoadingCommon'
+import SkeletonParagraph from 'src/components/common/SkeletonCommon/SkeletonParagraph/SkeletonParagraph'
+import SkeletonImage from 'src/components/common/SkeletonCommon/SkeletonImage/SkeletonImage'
 
 export default function Test() {
-  
   return (
     <>
       <CheckoutSuccess />
+      <LoadingCommon />
+      <SkeletonParagraph rows={3} />
+
+      <SkeletonImage align="center" size="large" />
     </>
   )
 }
