0463fb7808a12a619973fff53f2ee000bb8e6932
lytrankieio123 <lytran@kie.io>
2021-09-14 16:37:16 +0700

    :sparkles: feat: add error page
    :%s

---
+++ pages/_error.tsx
@@ -0,0 +1,9 @@
+import { Layout } from 'src/components/common'
+import { ErrorPage } from 'src/components/modules/error'
+
+export default function NotFound() {
+  return (
+       <ErrorPage/>
+  )
+}
+NotFound.Layout = Layout
