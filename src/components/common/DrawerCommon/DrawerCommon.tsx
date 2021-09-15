b9e47ef299e4472a448c83a4e765d3c8a746c129
lytrankieio123 <lytran@kie.io>
2021-09-15 13:53:24 +0700

    :art: styles: add cart drawer layer
    :%s

--- src/components/common/DrawerCommon/DrawerCommon.tsx
+++ src/components/common/DrawerCommon/DrawerCommon.tsx
@@ -11,22 +11,32 @@ interface Props {
 }
 
 const DrawerCommon = ({ title, visible, children, onClose }: Props) => {
+const refInner = useRef<HTMLDivElement>(null)
+
+const handleClickOut = (e: any) => {
+  if (e.target.contains(refInner.current)) {
+    onClose()
+  }
+}
+
   return (
     <div className={classNames({
       [s.drawerCommon]: true,
-      [s.hide]: !visible
+      [s.show]: visible
     })}>
-      <div className={s.inner}>
-        <div className={s.top}>
-          <h4 className={s.heading}>
-            {title}
-          </h4>
-          <div className={s.iconClose} onClick={onClose}>
-            <IconClose />
+      <div className={s.innerWrap} onClick={handleClickOut}>
+        <div className={s.inner} ref={refInner}>
+          <div className={s.top}>
+            <h4 className={s.heading}>
+              {title}
+            </h4>
+            <div className={s.iconClose} onClick={onClose}>
+              <IconClose />
+            </div>
           </div>
-        </div>
-        <div className={s.content}>
-          {children}
+          <div className={s.content}>
+            {children}
+          </div>
         </div>
       </div>
     </div>
