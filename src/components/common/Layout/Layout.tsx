bb5aec7dba497f3302128432328741fae511aba0
sonnguyenkieio <sonnguyen@kie.io>
2021-09-16 15:04:11 +0700

    :art: restyle: Layout for Account Page

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -2,7 +2,7 @@ import { CommerceProvider } from '@framework'
 import { useRouter } from 'next/router'
 import { FC } from 'react'
 import { useModalCommon } from 'src/components/hooks'
-import { BRAND, CATEGORY, FEATURED } from 'src/utils/constanst.utils'
+import { BRAND, CATEGORY, FEATURED, ROUTE } from 'src/utils/constanst.utils'
 import { ScrollToTop } from '..'
 import Footer from '../Footer/Footer'
 import Header from '../Header/Header'
@@ -33,10 +33,11 @@ const Layout: FC<Props> = ({ children }) => {
         <CommerceProvider locale={locale}>
             <div className={s.mainLayout}>
                 <Header toggleFilter={toggleFilter} visibleFilter={visibleFilter} />
-                { router.pathname === "/account" ?
-                    <section className={s.bgForAccount}>{children}</section> :
-                    <main>{children}</main> }
-                {/* <main>{children}</main> */}
+                {   
+                    router.pathname === ROUTE.ACCOUNT ?
+                    <section>{children}</section> :
+                    <main>{children}</main> 
+                }
                 <div className={s.filter}><MenuNavigationProductList categories={CATEGORY} brands={BRAND} featured={FEATURED} visible={visibleFilter} onClose={closeFilter} /> </div>
                 <ScrollToTop visibilityHeight={1500} />
                 <Footer />
