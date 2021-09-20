44b5ec94640ce2b4d097c4bf6ac61d98339a5ed8
goncy <gonzalo.pozzo4@gmail.com>
2021-09-20 14:36:11 -0300

    Add accepted payment

--- framework/ordercloud/api/endpoints/customer/card/add-item.ts
+++ framework/ordercloud/api/endpoints/customer/card/add-item.ts
@@ -1,10 +1,10 @@
 import type { CustomerCardEndpoint } from '.'
 import type { OredercloudCreditCard } from '../../../../types/customer/card'
 
-import Stripe from "stripe"
+import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET as string, {
-  apiVersion: "2020-08-27"
+  apiVersion: '2020-08-27',
 })
 
 const addItem: CustomerCardEndpoint['handlers']['addItem'] = async ({
@@ -29,30 +29,31 @@ const addItem: CustomerCardEndpoint['handlers']['addItem'] = async ({
   }
 
   // Get token
-  const token = await stripe.tokens.create({
-    card: {
-      number: item.cardNumber,
-      exp_month: item.cardExpireDate.split('/')[0],
-      exp_year: item.cardExpireDate.split('/')[1],
-      cvc: item.cardCvc
-    }
-  }).then((res: {id: string}) => res.id)
+  const token = await stripe.tokens
+    .create({
+      card: {
+        number: item.cardNumber,
+        exp_month: item.cardExpireDate.split('/')[0],
+        exp_year: item.cardExpireDate.split('/')[1],
+        cvc: item.cardCvc,
+      },
+    })
+    .then((res: { id: string }) => res.id)
 
   // Register credit card
   const creditCard = await restFetch('POST', `/me/creditcards`, {
-    "Token": token,
-    "CardType": "credit",
-    "PartialAccountNumber": item.cardNumber.slice(-4),
-    "CardholderName": item.cardHolder,
-    "ExpirationDate": item.cardExpireDate
-  }).then(
-    (response: OredercloudCreditCard) => response.ID
-  )
+    Token: token,
+    CardType: 'credit',
+    PartialAccountNumber: item.cardNumber.slice(-4),
+    CardholderName: item.cardHolder,
+    ExpirationDate: item.cardExpireDate,
+  }).then((response: OredercloudCreditCard) => response.ID)
 
   // Assign payment to order
   await restFetch('POST', `/orders/Outgoing/${cartId}/payments`, {
-    "Type": "CreditCard",
-    CreditCardID: creditCard
+    Accepted: true,
+    Type: 'CreditCard',
+    CreditCardID: creditCard,
   })
 
   return res.status(200).json({ data: null, errors: [] })
