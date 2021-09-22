68ba052be3c3d5f61996a91a6b791728c339885a
Quangnhankie <nhantran@kie.io>
2021-09-22 12:14:54 +0700

    bug: fix bug position Homespice

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       <HomeCategories />
       <HomeCollection />
       <HomeVideo />
-      <FeaturedProductsCarousel/>
       <HomeSpice/>
+      <FeaturedProductsCarousel/>
       <HomeCTA />
       <HomeRecipe />  
       <HomeSubscribe />
