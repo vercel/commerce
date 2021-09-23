1861d34d792ef64f9fc27b76f354e4964d4a7086
Luis Alvarez D <luis@vercel.com>
2021-09-22 19:49:01 -0500

    Remove additional title

--- pages/[...pages].tsx
+++ pages/[...pages].tsx
@@ -78,7 +78,6 @@ export default function Pages({
     <h1>Loading...</h1> // TODO (BC) Add Skeleton Views
   ) : (
     <div className="max-w-2xl mx-8 sm:mx-auto py-20">
-      {page?.name && <Text variant="heading">{page.name}</Text>}
       {page?.body && <Text html={page.body} />}
     </div>
   )
