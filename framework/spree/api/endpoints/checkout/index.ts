73c5e9df1003420a13b9ebebd6430e0e6263fc3d
tniezg <contact@tomaszniezgoda.com>
2021-09-24 14:25:02 +0200

    Upgrade checkout behavior in line with core NextJS Commerce changes

--- framework/spree/api/endpoints/checkout/index.ts
+++ framework/spree/api/endpoints/checkout/index.ts
@@ -2,7 +2,7 @@ import { createEndpoint } from '@commerce/api'
 import type { GetAPISchema, CommerceAPI } from '@commerce/api'
 import checkoutEndpoint from '@commerce/api/endpoints/checkout'
 import type { CheckoutSchema } from '@commerce/types/checkout'
-import checkout from './checkout'
+import getCheckout from './get-checkout'
 import type { SpreeApiProvider } from '../..'
 
 export type CheckoutAPI = GetAPISchema<
@@ -12,7 +12,10 @@ export type CheckoutAPI = GetAPISchema<
 
 export type CheckoutEndpoint = CheckoutAPI['endpoint']
 
-export const handlers: CheckoutEndpoint['handlers'] = { checkout }
+export const handlers: CheckoutEndpoint['handlers'] = {
+  getCheckout,
+  submitCheckout: () => {},
+}
 
 const checkoutApi = createEndpoint<CheckoutAPI>({
   handler: checkoutEndpoint,
