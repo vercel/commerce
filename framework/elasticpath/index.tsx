737612656ff09ba1842cd374f4d20903eb1df577
GunaTrika <guna.r@trikatechnologies.com>
2021-10-06 17:28:19 +0530

    Adding checkout files

--- framework/elasticpath/index.tsx
+++ framework/elasticpath/index.tsx
@@ -1,7 +1,6 @@
-import type { ReactNode } from 'react'
 import {
   CommerceConfig,
-  CommerceProvider as CoreCommerceProvider,
+  getCommerceProvider,
   useCommerce as useCoreCommerce,
 } from '@commerce'
 import { elasticpathProvider } from './provider'
@@ -10,27 +9,5 @@ import type { ElasticpathProvider } from './provider'
 export { elasticpathProvider }
 export type { ElasticpathProvider }
 
-export const elasticpathConfig: CommerceConfig = {
-  locale: 'en-us',
-  cartCookie: 'bc_cartId',
-}
-
-export type ElasticpathConfig = Partial<CommerceConfig>
-
-export type ElasticpathProps = {
-  children?: ReactNode
-  locale: string
-} & ElasticpathConfig
-
-export function CommerceProvider({ children, ...config }: ElasticpathProps) {
-  return (
-    <CoreCommerceProvider
-      provider={elasticpathProvider}
-      config={{ ...elasticpathConfig, ...config }}
-    >
-      {children}
-    </CoreCommerceProvider>
-  )
-}
-
+export const CommerceProvider = getCommerceProvider(elasticpathProvider)
 export const useCommerce = () => useCoreCommerce<ElasticpathProvider>()
--- framework/elasticpath/provider.ts
+++ framework/elasticpath/provider.ts
@@ -14,6 +14,10 @@ import { handler as useLogin } from './auth/use-login'
 import { handler as useLogout } from './auth/use-logout'
 import { handler as useSignup } from './auth/use-signup'
 
+import { handler as useCheckout } from './checkout/use-checkout'
+import { handler as useCards } from './customer/card/use-cards'
+import { handler as useAddresses } from './customer/address/use-addresses'
+
 import {fetcher} from './fetcher'
 
 export const elasticpathProvider = {
@@ -26,7 +30,18 @@ export const elasticpathProvider = {
     useAddItem: useWishlistAddItem,
     useRemoveItem: useWishlistRemoveItem,
   },
-  customer: { useCustomer },
+  checkout: {
+    useCheckout,
+  },
+  customer: { 
+    useCustomer,
+    card: {
+      useCards
+    },
+    address: {
+      useAddresses
+    }
+  },
   products: { useSearch },
   auth: { useLogin, useLogout, useSignup },
 }
