8b5637a2471024369c30e76185bd3ccc175cfd16
Tan Le <shioryuu0302@gmail.com>
2021-10-07 10:46:20 +0700

    push code to check

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,17 +1,17 @@
 import commerce from '@lib/api/commerce';
 import { GetStaticPropsContext } from 'next';
 import { Layout } from 'src/components/common';
-
+import { ProductCard } from '@commerce/types/product';
 interface Props {
-  products: any
+  productDetail: ProductCard[],
 }
-export default function Home({ products }: Props) {
+export default function Home({ productDetail }: Props) {
   return (
     <>
       <p>
-        TOTAL: {products?.length}
+        TOTAL: {productDetail}
       </p>
-      {JSON.stringify(products[0])}
+      {/* {JSON.stringify(productDetail)} */}
     </>
   )
 }
@@ -23,10 +23,11 @@ export async function getServerSideProps({
   locales,
 }: GetStaticPropsContext) {
   const config = { locale, locales }
-  const productsPromise = commerce.getAllProducts({
+  
+  const productsPromise = commerce.getProduct({
     // const productsPromise = commerce.getAllFacets({
     variables: {
-      first: 70,
+      slug: "hand-trowel"
       //  filter: {
       //   name: {
       //     contains: 'ca'
@@ -34,16 +35,15 @@ export async function getServerSideProps({
       // }
     },
     config,
-    preview,
+    // preview,
     // Saleor provider only
     ...({ featured: true } as any),
   })
 
-  const { products } = await productsPromise
-
-
+  const { product } = await productsPromise
+  const productDetail = JSON.stringify(product)
   return {
-    props: { products },
+    props: { productDetail },
   }
 }
 
--- src/components/modules/product-detail/ProductInfoDetail/ProductInfoDetail.tsx
+++ src/components/modules/product-detail/ProductInfoDetail/ProductInfoDetail.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import ProductImgs from './components/ProductImgs/ProductImgs'
 import ProductInfo from './components/ProductInfo/ProductInfo'
 import s from './ProductInfoDetail.module.scss'
+import { GetStaticPropsContext, GetStaticPathsContext, InferGetStaticPropsType } from 'next'
+import commerce from '@lib/api/commerce'
+import { PromiseWithKey } from 'src/utils/types.utils'
+import { getAllPromies } from 'src/utils/funtion.utils'
+import { ProductCard } from '@commerce/types/product';
+import { useRouter } from 'next/router'
 
 interface Props {
-    className?: string
-    children?: any
+    productDetail: ProductCard[],
 }
 
-const ProductInfoDetail = ({ }: Props) => {
+const ProductInfoDetail = ({ product }: InferGetStaticPropsType<typeof getStaticProps>) => {
+    console.log(product)
     return (
         <section className={s.productInfoDetail}>
             <ProductImgs/>
@@ -16,5 +22,62 @@ const ProductInfoDetail = ({ }: Props) => {
         </section >
     )
 }
+
+export async function getStaticProps({
+    params,
+    locale,
+    locales,
+    preview,
+  }: GetStaticPropsContext<{ slug: string }>) {
+    const config = { locale, locales }
+    const pagesPromise = commerce.getAllPages({ config, preview })
+    const siteInfoPromise = commerce.getSiteInfo({ config, preview })
+    const productPromise = commerce.getProduct({
+      variables: { slug: params!.slug },
+      config,
+      preview,
+    })
+  
+    // const allProductsPromise = commerce.getAllProducts({
+    //   variables: { first: 4 },
+    //   config,
+    //   preview,
+    // })
+    const { pages } = await pagesPromise
+    const { categories } = await siteInfoPromise
+    const { product } = await productPromise
+    // const { products: relatedProducts } = await allProductsPromise
+  
+    if (!product) {
+      throw new Error(`Product with slug '${params!.slug}' not found`)
+    }
+  
+    return {
+      props: {
+        pages,
+        product,
+        // relatedProducts,
+        categories,
+      },
+      revalidate: 200,
+    }
+    }
+  
+//   export async function getStaticPaths({ locales }: GetStaticPathsContext) {
+//     const { products } = await commerce.getAllProductPaths()
+  
+//     return {
+//       paths: locales
+//         ? locales.reduce<string[]>((arr, locale) => {
+//             // Add a product path for every locale
+//             products.forEach((product: any) => {
+//               arr.push(`/${locale}/product${product.path}`)
+//             })
+//             return arr
+//           }, [])
+//         : products.map((product: any) => `/product${product.path}`),
+//       fallback: 'blocking',
+//     }
+//   }
 
 export default ProductInfoDetail
