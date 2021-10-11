19ee8de4563883ad6183fdf4b99f4cf016a37665
Oliver Heywood <oliver.heywood@sitecore.com>
2021-10-11 10:42:55 -0500

    turns out the if statements here are necesary

--- framework/ordercloud/product/use-search.tsx
+++ framework/ordercloud/product/use-search.tsx
@@ -12,10 +12,11 @@ export const handler: SWRHook<SearchProductsHook> = {
     // Use a dummy base as we only care about the relative path
     const url = new URL(options.url!, 'http://a')
 
-    url.searchParams.set('search', String(search))
-    url.searchParams.set('categoryId', String(categoryId))
-    url.searchParams.set('brandId', String(brandId))
-    url.searchParams.set('sort', String(sort))
+
+    if (search) url.searchParams.set('search', String(search))
+    if (categoryId) url.searchParams.set('categoryId', String(categoryId))
+    if (brandId) url.searchParams.set('brandId', String(brandId))
+    if (sort) url.searchParams.set('sort', String(sort))
 
     return fetch({
       url: url.pathname + url.search,
