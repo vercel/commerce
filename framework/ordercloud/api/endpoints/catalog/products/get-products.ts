3a3eb38eccca077585eb393577da2b9e96cda515
Oliver Heywood <oliver.heywood@sitecore.com>
2021-10-11 10:09:55 -0500

    product list page and search working

---
+++ framework/ordercloud/api/endpoints/catalog/products/get-products.ts
@@ -0,0 +1,49 @@
+import { Product } from '@commerce/types/product'
+import { RawProduct } from '@framework/types/product'
+import { normalize as normalizeProduct } from '@framework/utils/product'
+import { ProductsEndpoint } from '.'
+
+
+const SORT: { [key: string]: string | undefined } = {
+  latest: 'id',
+  trending: 'total_sold',
+  price: 'price',
+}
+
+// Return current cart info
+const getProducts: ProductsEndpoint['handlers']['getProducts'] = async ({
+  req,
+  res,
+  body: { search, categoryId, brandId, sort },
+  config: { restBuyerFetch, cartCookie, tokenCookie },
+}) => {
+  console.log("categoryId", categoryId)
+
+  //Use a dummy base as we only care about the relative path
+  const url = new URL('/me/products', 'http://a')
+
+  if (search) { 
+    url.searchParams.set('search', search) 
+  }
+  if (categoryId && Number.isInteger(Number(categoryId))) {
+    url.searchParams.set('categoryID', String(categoryId)) 
+  }
+
+  // Get token from cookies
+  const token = req.cookies[tokenCookie];
+
+  var rawProducts = await restBuyerFetch(
+    'GET',
+    url.pathname + url.search,
+    null,
+    { token }
+  );
+
+  const products = rawProducts.Items.map(normalizeProduct);
+  const found = rawProducts?.Items?.length > 0;
+
+  res.status(200).json({ data: { products, found } })
+}
+
+export default getProducts
+
---
+++ framework/ordercloud/api/endpoints/catalog/products/index.ts
@@ -0,0 +1,18 @@
+import { createEndpoint, GetAPISchema } from "@commerce/api"
+import { ProductsSchema } from "@commerce/types/product"
+import { OrdercloudAPI } from "@framework/api"
+import getProducts from "./get-products";
+import productsEndpoint from '@commerce/api/endpoints/catalog/products'
+
+export type ProductsAPI = GetAPISchema<OrdercloudAPI, ProductsSchema>
+
+export type ProductsEndpoint = ProductsAPI['endpoint']
+
+export const handlers: ProductsEndpoint['handlers'] = { getProducts }
+
+const productsApi = createEndpoint<ProductsAPI>({
+  handler: productsEndpoint,
+  handlers,
+})
+
+export default productsApi
