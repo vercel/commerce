d1ea2354ee6baa82d0a0a12812476e1ffdcd5fc9
Alessandro Casazza <alessandro@commercelayer.io>
2021-10-12 16:33:28 +0200

    feat: Add search products by name

--- framework/commercelayer/product/use-search.tsx
+++ framework/commercelayer/product/use-search.tsx
@@ -1,17 +1,32 @@
 import { SWRHook } from '@commerce/utils/types'
 import useSearch, { UseSearch } from '@commerce/product/use-search'
+import data from '../data.json'
 export default useSearch as UseSearch<typeof handler>
+
+const productsFinder = (s: string) => {
+  const { products } = data
+  return s
+    ? products.filter(
+        (p) => p.name.toLowerCase().search(s.toLowerCase()) !== -1
+      )
+    : []
+}
 
 export const handler: SWRHook<any> = {
   fetchOptions: {
     query: '',
   },
   async fetcher({ input, options, fetch }) {},
-  useHook: () => () => {
-    return {
-      data: {
-        products: [],
-      },
-    }
-  },
+  useHook:
+    ({ useData }) =>
+    ({ search }) => {
+      const { mutate } = useData()
+      const products = productsFinder(search)
+      mutate()
+      return {
+        data: {
+          products,
+        },
+      }
+    },
 }
