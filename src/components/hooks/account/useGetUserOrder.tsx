bd06e1d8157fec36e54d2a9b5f6ada81790fa7da
Quangnhankie <nhantran@kie.io>
2021-10-15 17:23:42 +0700

    delete console.log

--- src/components/hooks/account/useGetUserOrder.tsx
+++ src/components/hooks/account/useGetUserOrder.tsx
@@ -5,7 +5,7 @@ import useSWR from 'swr'
 
 const useGetUserOrder = () => {
   const { data, ...rest } = useSWR<ActiveCustomerQuery>([getUserOrderQuery], gglFetcher)
-  console.log(data);
+  
   const addingItem = data?.activeCustomer?.orders.items.filter((val:Order) =>val.state == 'AddingItems');
   const arrangingPayment = data?.activeCustomer?.orders.items.filter((val:Order) =>val.state == 'ArrangingPayment');
   const cancelled = data?.activeCustomer?.orders.items.filter((val:Order) =>val.state == "Cancelled");
