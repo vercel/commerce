cfd01a3dc00c23d2507aaa30251bd67371c58518
Chris Vibert <cp.vibert@gmail.com>
2021-10-17 21:08:09 +0700

    commercejs: Remove unused API endpoints

--- framework/commercejs/api/endpoints/customer/address/remove-item.ts
+++
@@ -1,9 +0,0 @@
-import type { CustomerAddressEndpoint } from '.'
-
-const removeItem: CustomerAddressEndpoint['handlers']['removeItem'] = async ({
-  res,
-}) => {
-  return res.status(200).json({ data: null, errors: [] })
-}
-
-export default removeItem
