db66209bbcd234cbd1d29424c0c7efa5cde78a31
Florian-crg <floriancargouet@gmail.com>
2021-10-17 20:42:12 +0200

    fix className

--- components/common/Subscribe/subscribe.jsx
+++ components/common/Subscribe/subscribe.jsx
@@ -28,10 +28,10 @@ const Subscribe = () => {
   }
 
   return (
-    <form onSubmit={handleSubscribe} class="w-full max-w-sm">
-      <div class="flex items-center border-b border-teal-500 py-2">
+    <form onSubmit={handleSubscribe} className="w-full max-w-sm">
+      <div className="flex items-center border-b border-teal-500 py-2">
         <input
-          class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+          className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
           type="email"
           placeholder="janedoe@gmail.com"
           aria-label="Email"
@@ -40,9 +40,6 @@ const Subscribe = () => {
         <Button variant="slim" className="mt-1 h-8 " loading={loading}>
           S'INSCRIRE
         </Button>
-        {/* <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
-      Sign Up
-    </button> */}
       </div>
     </form>
   )
