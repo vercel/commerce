abca50f39e3e523229bd5e3e1e693cd46e4d521b
Chris Vibert <cp.vibert@gmail.com>
2021-10-19 22:15:01 +0700

    commercejs: Refactor product search logic

--- framework/commercejs/product/use-search.tsx
+++ framework/commercejs/product/use-search.tsx
@@ -2,6 +2,7 @@ import { SWRHook } from '@commerce/utils/types'
 import useSearch, { UseSearch } from '@commerce/product/use-search'
 import { SearchProductsBody, SearchProductsHook } from '@commerce/types/product'
 import type { Product as CommercejsProduct } from '@chec/commerce.js/types/product'
+import { getProductSearchVariables } from '../utils/product-search'
 import { normalizeProduct } from '../utils/normalize-product'
 
 export default useSearch as UseSearch<typeof handler>
@@ -12,18 +13,6 @@ export const handler: SWRHook<SearchProductsHook> = {
     method: 'list',
   },
   async fetcher({ input, options, fetch }) {
-    const getSearchVariables = (input: SearchProductsBody) => {
-      const { search, categoryId } = input
-      let variables: { [key: string]: any } = {}
-      if (search) {
-        variables.query = search
-      }
-      if (categoryId) {
-        variables['category_id'] = categoryId
-      }
-      return variables
-    }
-
     const { data, meta } = await fetch<{
       data: CommercejsProduct[]
       meta: {
@@ -34,27 +23,10 @@ export const handler: SWRHook<SearchProductsHook> = {
     }>({
       query: options.query,
       method: options.method,
-      variables: getSearchVariables(input),
+      variables: getProductSearchVariables(input),
     })
 
-    // Manually sort the products because the Commerce.js SDK doesn't support this yet.
-    let sortedProducts = data
-    switch (input.sort) {
-      case 'trending-desc':
-        sortedProducts = data.sort((a, b) => b.updated - a.updated)
-        break
-      case 'latest-desc':
-        sortedProducts = data.sort((a, b) => b.updated - a.updated)
-        break
-      case 'price-asc':
-        sortedProducts = data.sort((a, b) => a.price.raw - b.price.raw)
-        break
-      case 'price-desc':
-        sortedProducts = data.sort((a, b) => b.price.raw - a.price.raw)
-        break
-    }
-
-    const formattedProducts = sortedProducts?.map(normalizeProduct) || []
+    const formattedProducts = data?.map(normalizeProduct) || []
 
     return {
       products: formattedProducts,
