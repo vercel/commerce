2dbc8357086a5a977bf3443bf1bdc92da42d9b1d
Chris Vibert <cp.vibert@gmail.com>
2021-10-23 10:33:36 +0700

    commercejs: Enhance fetcher to allow custom API routes

--- framework/commercejs/fetcher.ts
+++ framework/commercejs/fetcher.ts
@@ -1,7 +1,30 @@
 import { commerce } from './lib/commercejs'
 import { Fetcher } from '@commerce/utils/types'
 
-const sdkFetch: Fetcher = async ({ query, method, variables }) => {
+// Fetches from an API route within /api/endpoints directory
+const customFetcher: Fetcher = async ({ method, url, body }) => {
+  const response = await fetch(url!, {
+    method,
+    body: body ? JSON.stringify(body) : undefined,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => response.data)
+
+  return response
+}
+
+const fetcher: Fetcher = async ({ url, query, method, variables, body }) => {
+  // If a URL is passed, it means that the fetch needs to be passed on to a custom API route.
+  const isCustomFetch = !!url
+  if (isCustomFetch) {
+    const data = await customFetcher({ url, method, body })
+    return data
+  }
+
+  // Fetch using the Commerce.js SDK.
   const variablesArgument = Array.isArray(variables) ? variables : [variables]
   const resource = commerce[query as keyof typeof commerce]
 
@@ -10,4 +33,4 @@ const sdkFetch: Fetcher = async ({ query, method, variables }) => {
   return data
 }
 
-export default sdkFetch
+export default fetcher
