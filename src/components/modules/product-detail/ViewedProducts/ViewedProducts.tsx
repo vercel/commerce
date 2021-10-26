db94462f13c4c3052b149a6809e840b28a3516d6
lytrankieio123 <lytran@kie.io>
2021-10-26 08:57:02 +0700

    :bug: bug: check data undefined
    :%s

--- src/components/modules/product-detail/ViewedProducts/ViewedProducts.tsx
+++ src/components/modules/product-detail/ViewedProducts/ViewedProducts.tsx
@@ -1,15 +1,11 @@
-import { Product } from '@commerce/types/product';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ListProductWithInfo from 'src/components/common/ListProductWithInfo/ListProductWithInfo';
 import { ProductCardProps } from 'src/components/common/ProductCard/ProductCard';
-import { LOCAL_STORAGE_KEY } from 'src/utils/constanst.utils'
-import { normalizeProductCard } from '@framework/utils/normalize';
-import { useLocalStorage } from 'src/components/hooks/useLocalStorage';
 interface Props {
     data: ProductCardProps[]
 }
 const ViewedProducts = ({data}:Props) => {
-    if (data.length===0){
+    if (data && data.length===0){
         return <div></div>
     }
     return (
