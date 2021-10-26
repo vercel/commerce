fbac4b9e54accf492e3f424892ea31786aa1f4ea
QuangNhan <tranquangnhan1606@gmail.com>
2021-10-26 17:29:13 +0700

    :hammer: refactor: move file useAvailableCountries
    :%s

--- src/components/hooks/order/index.ts
+++ src/components/hooks/order/index.ts
@@ -1,7 +1,6 @@
 export { default as useSetCustomerForOrder } from './useSetCustomerForOrder'
 export { default as useSetOrderShippingAddress } from './useSetOrderShippingAddress'
 export { default as useApplyCouponCode } from './useApplyCouponCode'
-export { default as useAvailableCountries } from './useAvailableCountries'
 export { default as useSetOrderShippingMethod } from './useSetOrderShippingMethod'
 export { default as useGetActiveOrderForCheckout } from './useGetActiveOrderForCheckout'
 export { default as useEligibleShippingMethods } from './useEligibleShippingMethods'
