5cd26a07cd70e391ff94002ea85b6207229640b9
Chris Vibert <cp.vibert@gmail.com>
2021-10-26 20:32:05 +0700

    commercejs: Rename public key env variable as commercejs

--- framework/commercejs/lib/commercejs.ts
+++ framework/commercejs/lib/commercejs.ts
@@ -1,10 +1,11 @@
 import Commerce from '@chec/commerce.js'
 
-const checPublicKey = process.env.NEXT_PUBLIC_CHEC_PUBLIC_KEY as string
+const commercejsPublicKey = process.env
+  .NEXT_PUBLIC_COMMERCEJS_PUBLIC_KEY as string
 const devEnvironment = process.env.NODE_ENV === 'development'
 
-if (devEnvironment && !checPublicKey) {
-  throw Error('A Chec public API key must be provided')
+if (devEnvironment && !commercejsPublicKey) {
+  throw Error('A Commerce.js public API key must be provided')
 }
 
-export const commerce = new Commerce(checPublicKey, devEnvironment)
+export const commerce = new Commerce(commercejsPublicKey, devEnvironment)
