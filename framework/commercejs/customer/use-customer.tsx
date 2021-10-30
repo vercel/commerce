9517a3e1624ed6acab37c3f86ffc778868b877dd
Chris Vibert <cp.vibert@gmail.com>
2021-10-30 09:00:06 +0700

    commercejs: Add hook to fetch logged in customer

--- framework/commercejs/customer/use-customer.tsx
+++ framework/commercejs/customer/use-customer.tsx
@@ -1,15 +1,44 @@
+import Cookies from 'js-cookie'
+import { decode } from 'jsonwebtoken'
 import { SWRHook } from '@commerce/utils/types'
 import useCustomer, { UseCustomer } from '@commerce/customer/use-customer'
+import { CUSTOMER_COOKIE, API_URL } from '@framework/constants'
+import type { CustomerHook } from '../types/customer'
 
 export default useCustomer as UseCustomer<typeof handler>
-export const handler: SWRHook<any> = {
+export const handler: SWRHook<CustomerHook> = {
   fetchOptions: {
-    query: '',
+    query: 'customer',
+    method: '_request',
   },
-  async fetcher({ input, options, fetch }) {},
-  useHook: () => () => {
-    return async function addItem() {
-      return {}
+  async fetcher({ options, fetch }) {
+    const token = Cookies.get(CUSTOMER_COOKIE)
+    if (!token) {
+      return null
     }
+
+    const decodedToken = decode(token) as { cid: string }
+    const customer = await fetch({
+      query: options.query,
+      method: options.method,
+      variables: [
+        `${API_URL}/customers/${decodedToken.cid}`,
+        'get',
+        null,
+        {},
+        token,
+      ],
+    })
+    return customer
   },
+  useHook:
+    ({ useData }) =>
+    (input) => {
+      return useData({
+        swrOptions: {
+          revalidateOnFocus: false,
+          ...input?.swrOptions,
+        },
+      })
+    },
 }
