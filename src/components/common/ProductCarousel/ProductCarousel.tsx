4da45bd17a6d8c9512f96a73c7c9c42d7040bed7
lytrankieio123 <lytran@kie.io>
2021-11-12 15:52:28 +0700

    :recycle: enhan: fix slidesToSlide in product carousel
    :%s

--- src/components/common/ProductCarousel/ProductCarousel.tsx
+++ src/components/common/ProductCarousel/ProductCarousel.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
+import React from 'react';
 import { ResponsiveType } from 'react-multi-carousel';
-import { useModalCommon } from 'src/components/hooks';
-import { ModalConfirm } from '..';
 import CarouselCommon, {
-  CarouselCommonProps,
-} from '../CarouselCommon/CarouselCommon'
-import ProductCard, { ProductCardProps } from '../ProductCard/ProductCard'
-import s from "./ProductCarousel.module.scss"
+  CarouselCommonProps
+} from '../CarouselCommon/CarouselCommon';
+import ProductCard, { ProductCardProps } from '../ProductCard/ProductCard';
+import s from "./ProductCarousel.module.scss";
 
 interface ProductCarouselProps
   extends Omit<CarouselCommonProps<ProductCardProps>, 'Component' | "option"> {
@@ -16,25 +14,27 @@ const RESPONSIVE:ResponsiveType = {
   lgScreen: {
     breakpoint: { max:9999, min: 1440 },
     items: 5.5,
-    slidesToSlide: 1 // optional, default to 1.
+    slidesToSlide: 2
   },
   desktop: {
     breakpoint: { max: 1440, min: 1280 },
     items: 4.5,
-    slidesToSlide: 1 // optional, default to 1.
+    slidesToSlide: 2
   },
   lap: {
     breakpoint: { max: 1280, min: 1008 },
     items: 3.5,
-    slidesToSlide: 1 // optional, default to 1.
+    slidesToSlide: 2,
   },
   tablet: {
     breakpoint: { max: 1008, min: 640 },
     items: 3,
+    slidesToSlide: 2,
   },
   mobile: {
     breakpoint: { max: 640, min: 0 },
     items: 2,
+    slidesToSlide: 2,
   }
 };
 const ProductCarousel = ({ data, responsive= RESPONSIVE, collection,  ...props }: ProductCarouselProps) => {
