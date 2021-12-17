480b5ef330ddfdffb38618952d77ecf821d45b61
mdupree <mdupree@acromedia.com>
2021-12-17 10:23:39 -0800

    Fixes issue with next/dynamin when running next version > 12.0.3

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -25,28 +25,33 @@ const Loading = () => (
   </div>
 )
 
-const dynamicProps = {
-  loading: Loading,
-}
-
 const SignUpView = dynamic(
   () => import('@components/auth/SignUpView'),
-  dynamicProps
+  {
+    loading: Loading
+  }
 )
 
 const ForgotPassword = dynamic(
   () => import('@components/auth/ForgotPassword'),
-  dynamicProps
+  {
+    loading: Loading
+  }
 )
 
 const FeatureBar = dynamic(
   () => import('@components/common/FeatureBar'),
-  dynamicProps
+  {
+    loading: Loading
+  }
 )
 
 const Modal = dynamic(
   () => import('@components/ui/Modal'),
-  Object.assign(dynamicProps, { ssr: false })
+  {
+    loading: Loading,
+    ssr: false
+  }
 )
 
 interface Props {
