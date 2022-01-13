69893d5d127481b932c0a1086db3bc075d74747c
Tobias Koppers <tobias.koppers@googlemail.com>
2022-01-13 09:12:57 +0100

    set higher image priority for primary images

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,6 +44,7 @@ export default function Home({
         {products.slice(0, 3).map((product: any, i: number) => (
           <ProductCard
             key={product.id}
+            priority
             product={product}
             imgProps={{
               width: i === 0 ? 1080 : 540,
