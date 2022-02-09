d96e6be9333d4086fa657a40f499edc5060904d4
LFades <luis@vercel.com>
2022-02-09 12:00:35 -0500

    Updated Next.js detection

--- next.config.js
+++ next.config.js
@@ -1,2 +1,4 @@
 // This file is here to let Vercel think this is a Next.js project.
 // The actual next.config.js is in ./site/next.config.js.
+// `next` is also added as a dependency in the root package.json
+// for this same reason.
