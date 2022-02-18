db170558d55ee34b4bf86635f78d1aecc9269fa7
Dom Sip <dominik.sipowicz@gmail.com>
2022-02-18 09:26:31 +0000

    feat: replace next-seo with custom solution (#660)
    * replace next-seo with custom solution
    
    * Updated check
    
    Co-authored-by: LFades <luis@vercel.com>

--- site/components/common/Head/Head.tsx
+++ site/components/common/Head/Head.tsx
@@ -1,17 +1,16 @@
-import { FC } from 'react'
-import NextHead from 'next/head'
-import { DefaultSeo } from 'next-seo'
-import config from '@config/seo.json'
+import type { VFC } from 'react'
+import { SEO } from '@components/common'
 
-const Head: FC = () => {
+const Head: VFC = () => {
   return (
-    <>
-      <DefaultSeo {...config} />
-      <NextHead>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
-      </NextHead>
-    </>
+    <SEO>
+      <meta
+        key="viewport"
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      />
+      <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
+    </SEO>
   )
 }
 
