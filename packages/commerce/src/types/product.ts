fee16e795c947a86ce6e03008537ec8c86e751c4
Stefan Horne <stefan.horne@gmail.com>
2022-03-23 15:29:13 +0000

    feat(core): add GBP currency code to product price type (#717)
    Co-authored-by: Stefan Horne <stefanhorne@gmail.com>

--- packages/commerce/src/types/product.ts
+++ packages/commerce/src/types/product.ts
@@ -5,7 +5,7 @@ export type ProductImage = {
 
 export type ProductPrice = {
   value: number
-  currencyCode?: 'USD' | 'EUR' | 'ARS' | string
+  currencyCode?: 'USD' | 'EUR' | 'ARS' | 'GBP' | string
   retailPrice?: number
   salePrice?: number
   listPrice?: number
