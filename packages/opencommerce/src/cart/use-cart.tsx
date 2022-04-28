7d1b542b6f3b580427d141ca3f3aacb56ccffedf
Chloe <pinkcloudvnn@gmail.com>
2022-04-28 11:22:40 +0700

    Complete use-cart hook
    Signed-off-by: Chloe <pinkcloudvnn@gmail.com>

--- packages/opencommerce/src/cart/use-cart.tsx
+++ packages/opencommerce/src/cart/use-cart.tsx
@@ -1,42 +1,32 @@
 import { useMemo } from 'react'
 import { SWRHook } from '@vercel/commerce/utils/types'
 import useCart, { UseCart } from '@vercel/commerce/cart/use-cart'
+import type { GetCartHook } from '@vercel/commerce/types/cart'
 
 export default useCart as UseCart<typeof handler>
 
-export const handler: SWRHook<any> = {
+export const handler: SWRHook<GetCartHook> = {
   fetchOptions: {
-    query: '',
-  },
-  async fetcher() {
-    return {
-      id: '',
-      createdAt: '',
-      currency: { code: '' },
-      taxesIncluded: '',
-      lineItems: [],
-      lineItemsSubtotalPrice: '',
-      subtotalPrice: 0,
-      totalPrice: 0,
-    }
+    url: '/api/cart',
+    method: 'GET',
   },
   useHook:
     ({ useData }) =>
     (input) => {
+      const response = useData({
+        swrOptions: { revalidateOnFocus: false, ...input?.swrOptions },
+      })
       return useMemo(
         () =>
-          Object.create(
-            {},
-            {
-              isEmpty: {
-                get() {
-                  return true
-                },
-                enumerable: true,
+          Object.create(response, {
+            isEmpty: {
+              get() {
+                return (response.data?.lineItems.length ?? 0) <= 0
               },
-            }
-          ),
-        []
+              enumerable: true,
+            },
+          }),
+        [response]
       )
     },
 }
