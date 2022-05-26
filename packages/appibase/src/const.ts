ef882d0c27a3caa35e5b3d95dff8e8ef57b7b926
rafik mrabet <mrabetr@hotmail.com>
2022-05-26 12:42:09 +0100

    using Template literals to get env variables which specifies String as the Type

--- packages/appibase/src/const.ts
+++ packages/appibase/src/const.ts
@@ -2,6 +2,6 @@ export const API_URL = `https://appibase.com`
 
 export const SCOPE = `storefront client`
 
-export const CLIENT_ID_STOREFRONT = process.env.NEXT_PUBLIC_APPIBASE_CLIENT_ID_STOREFRONT
+export const CLIENT_ID_STOREFRONT = `${process.env.NEXT_PUBLIC_APPIBASE_CLIENT_ID_STOREFRONT}`
 
-export const STORE_ID = process.env.NEXT_PUBLIC_APPIBASE_STORE_ID
+export const STORE_ID = `${process.env.NEXT_PUBLIC_APPIBASE_STORE_ID}`
