c067efba1a6f2988f601971177fc9af86c0de66e
Dinushi Herath <dinushi.herath@roamdigital.com>
2022-05-31 14:32:35 +1000

    test

--- site/cypress/integration/shopping-cart.spec.js
+++ site/cypress/integration/shopping-cart.spec.js
@@ -1,5 +1,5 @@
 describe("Shopping Cart", () => {
-    it("users can add products to the cart", () => {
+    it("userss can add products to the cart", () => {
       cy.visit("/")
       cy.getBySel("product-tag").eq(0).click()
       cy.get('[aria-label="Add to Cart"]').click()
