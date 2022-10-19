e1751eef3f6a43025f5181564af23e68bbbf7cec
Catalin Pinte <1243434+cond0r@users.noreply.github.com>
2022-10-19 09:36:12 +0300

    Make nodejs default runtime

--- site/pages/api/commerce/[[...commerce]].ts
+++ site/pages/api/commerce/[[...commerce]].ts
@@ -1,8 +1,8 @@
 import commerce from '@lib/api/commerce'
 import endpoints from '@framework/api/endpoints'
 
-export const config = {
-  runtime: 'experimental-edge',
-}
+// export const config = {
+//   runtime: 'experimental-edge',
+// }
 
 export default endpoints(commerce)
