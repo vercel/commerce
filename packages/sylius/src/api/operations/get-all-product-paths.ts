b4a81e7d9a916f3cb9c82a28d51bc242af3706ce
DuvCharles <charles.duvigneau@gmail.com>
2022-11-18 17:28:33 +0100

    get-all-product-path operation

--- packages/sylius/src/api/operations/get-all-product-paths.ts
+++ packages/sylius/src/api/operations/get-all-product-paths.ts
@@ -1,12 +1,36 @@
+import { OperationContext } from '@vercel/commerce/api/operations'
+import type { Product } from '@vercel/commerce/types/product'
+import { SyliusConfig } from 'api'
+import { SyliusProduct } from 'types/products'
+import { normalizeProduct } from '../../utils/normalize/normalize-product'
+
 export type GetAllProductPathsResult = {
   products: Array<{ path: string }>
 }
 
-export default function getAllProductPathsOperation() {
-  function getAllProductPaths(): Promise<GetAllProductPathsResult> {
-    return Promise.resolve({
-      products: [],
-    })
+export default function getAllProductPathsOperation({
+  commerce,
+}: OperationContext<any>) {
+  async function getAllProductPaths({
+    query,
+    variables,
+    config: cfg,
+  }: {
+    query?: string
+    variables?: any
+    config?: Partial<SyliusConfig>
+    preview?: boolean
+  } = {}): Promise<GetAllProductPathsResult> {
+    const config = commerce.getConfig(cfg)
+    const syliusProducts = await config.fetch('GET', '/products')
+    const products = syliusProducts.map((syliusProduct: SyliusProduct) =>
+      normalizeProduct(syliusProduct)
+    )
+    return {
+      products: products.map((product: Product) => ({
+        path: product.path,
+      })),
+    }
   }
 
   return getAllProductPaths
