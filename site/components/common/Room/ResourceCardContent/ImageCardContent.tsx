68230134947b42310dd15be5a36f3b99190eebbd
Daniele Pancottini <daniele.pancottini@studio.unibo.it>
2023-02-23 10:06:50 +0100

    Try to fix safari full screen image into panorama view

--- site/components/common/Room/ResourceCardContent/ImageCardContent.tsx
+++ site/components/common/Room/ResourceCardContent/ImageCardContent.tsx
@@ -9,6 +9,26 @@ export default function ImageCardContent(props: {
 
     return (
         <>
+          <style jsx>{`
+            *|*:fullscreen:not(:root) {
+              position: fixed !important;
+              top: 0 !important;
+              left: 0 !important;
+              right: 0 !important;
+              bottom: 0 !important;
+              width: 100% !important;
+              height: 100% !important;
+              margin: 0 !important;
+              min-width: 0 !important;
+              max-width: none !important;
+              min-height: 0 !important;
+              max-height: none !important;
+              box-sizing: border-box !important;
+              object-fit: contain;
+              transform: none !important;
+            }
+            `}
+          </style>
             <Box
               className={props.style.imageContainer}
               w={'full'}
