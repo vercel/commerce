a1ae2357db8d63f3086d89c3f6c1d369e0bafe62
Henrik Larsson <hankisch@gmail.com>
2023-05-03 10:20:25 +0200

    Iterated with translations

--- app/[locale]/[[...slug]]/page.tsx
+++ app/[locale]/[[...slug]]/page.tsx
@@ -1,15 +1,22 @@
 'use client';
  
-import LocaleSwitcher from 'components/ui/locale-switcher';
+import LocaleSwitcher from 'components/ui/locale-switcher/locale-switcher';
 import { useTranslations } from 'next-intl';
+
+interface PageProps {
+  params: {
+    locale: string
+  }
+}
  
-export default function Index() {
+export default function Index({params: {locale}} : PageProps) {
+
   const t = useTranslations('Index');
   
   return (
     <div>
       <h1>{t('title')}</h1>
-      <LocaleSwitcher />
+      <LocaleSwitcher currentLocale={locale} />
     </div>
   )
 }
