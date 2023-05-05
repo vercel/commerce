69cdf9d18594bbdb8ad22d8f3dfb1cec21823d23
Lee Robinson <lrobinson2011@gmail.com>
2023-05-05 09:42:51 -0700

    Remove experimental flag

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ module.exports = {
     // Disabling on production builds because we're running checks on PRs via GitHub Actions.
     ignoreDuringBuilds: true
   },
-  experimental: {
-    appDir: true
-  },
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
