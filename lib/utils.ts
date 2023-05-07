8fa533441498162503fab4043c0f7aea0d698feb
Lee Robinson <lrobinson2011@gmail.com>
2023-05-07 13:00:02 -0500

    Push it

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,6 @@
-export const createUrl = (pathname: string, params: URLSearchParams) => {
+import { ReadonlyURLSearchParams } from 'next/navigation';
+
+export const createUrl = (pathname: string, params: URLSearchParams | ReadonlyURLSearchParams) => {
   const paramsString = params.toString();
   const queryString = `${paramsString.length ? '?' : ''}${paramsString}`;
 
