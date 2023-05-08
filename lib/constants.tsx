804b76d3a2b89d0a4cad3e660b003799d20cb29a
Victor Gerbrands <victorgerbrands@gmail.com>
2023-05-08 14:47:41 +0200

    refactor: remove more shopify references

--- lib/constants.tsx
+++ lib/constants.tsx
@@ -22,4 +22,3 @@ export const sorting: SortFilterItem[] = [
 
 export const HIDDEN_PRODUCT_TAG = 'nextjs-frontend-hidden';
 export const DEFAULT_OPTION = 'Default Title';
-export const SHOPIFY_GRAPHQL_API_ENDPOINT = '/api/2023-01/graphql.json';
