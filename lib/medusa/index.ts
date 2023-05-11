1a0c2eb85cd482a74555af55f7c1be3acda265e4
Victor Gerbrands <victorgerbrands@gmail.com>
2023-05-11 13:46:23 +0200

    fix: use thumbnail as featured image

--- lib/medusa/index.ts
+++ lib/medusa/index.ts
@@ -180,8 +180,8 @@ const reshapeProduct = (product: MedusaProduct): Product => {
   const tags = product.tags?.map((tag) => tag.value) || [];
   const descriptionHtml = product.description ?? '';
   const featuredImage = {
-    url: product.images?.[0]?.url ?? '',
-    altText: product.images?.[0]?.id ?? ''
+    url: product.thumbnail ?? '',
+    altText: product.title ?? ''
   };
   const availableForSale = product.variants?.[0]?.purchasable || true;
   const variants = product.variants.map((variant) =>
