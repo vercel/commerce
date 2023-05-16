68272fbaf3cd5ed007c6fcd510cb439fe776c8bf
Henrik Larsson <hankisch@gmail.com>
2023-05-16 15:02:42 +0200

    Remove revalidate until further

--- app/api/revalidate/route.ts
+++
@@ -1,9 +0,0 @@
-import { revalidatePath } from 'next/cache';
-import { NextRequest, NextResponse } from 'next/server';
- 
-export async function GET(request: NextRequest) {
-  
-  console.log(request)
-  revalidatePath('/');
-  return NextResponse.json({ revalidated: true, now: Date.now() });
-}
