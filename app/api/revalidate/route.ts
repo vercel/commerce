72135cdb4251192ce1897b7e3a718cebec1aacf5
Henrik Larsson <hankisch@gmail.com>
2023-05-16 14:54:31 +0200

    Revalidate test

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,12 +2,8 @@ import { revalidatePath } from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
  
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const slug = searchParams.get('slug');
-  const locale = searchParams.get('locale');
-
-  console.log(request.nextUrl.searchParams)
-
-  revalidatePath(`/${locale}/${slug}`);
+  
+  console.log(request)
+  revalidatePath('/');
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
