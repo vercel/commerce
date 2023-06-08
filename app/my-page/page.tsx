c001bd6a528297695beb6ac4af776861801504bf
Michael Novotny <manovotny@gmail.com>
2023-06-07 21:09:43 -0500

    Proof of concept for static pages only.

---
+++ app/my-page/page.tsx
@@ -0,0 +1,10 @@
+export const runtime = 'edge';
+
+export default async function Page() {
+  return (
+    <>
+      <h1 className="mb-8 text-5xl font-bold">My Page</h1>
+      <p>Content</p>
+    </>
+  );
+}
