d9216415856d327b00d90bfbf6101596a5ddf5e3
andr-ew <andrewcshike@gmail.com>
2023-07-03 17:44:13 -0500

    add: fonts, css reset, basic global styles

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,42 @@
+import localFont from 'next/font/local';
+
+import '/styles/global.scss';
 import styles from './styles.module.scss';
+
+const CenturyNovaSB = localFont({
+    src: [
+        {
+            path: '../fonts/CenturyNovaSBRoman/font.woff',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../fonts/CenturyNovaSBItalic/font.woff',
+            weight: '400',
+            style: 'italic',
+        },
+    ],
+    variable: '--font-century-nova-sb',
+});
+const Dia = localFont({
+    src: [
+        {
+            path: '../fonts/DiaRegular/Dia Regular.woff',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../fonts/DiaLight/Dia Light.woff',
+            weight: '100',
+            style: 'normal',
+        },
+    ],
+    variable: '--font-dia',
+});
 
 export default function RootLayout({ children }) {
     return (
-        <html lang='en'>
+        <html lang='en' className={`${CenturyNovaSB.variable} ${Dia.variable}`}>
             <body className={styles.body}>{children}</body>
         </html>
     );
