37e2ef06fcd9cb5f271516b01e55af9846dc0939
Henrik Larsson <hankisch@gmail.com>
2023-07-03 14:05:31 +0200

    Implement client fetch

--- app/[locale]/[[...slug]]/page.tsx
+++ app/[locale]/[[...slug]]/page.tsx
@@ -1,9 +1,12 @@
+// Next
+import type { Metadata } from 'next';
+import { draftMode } from 'next/headers';
+// Sanity
 import PreviewSuspense from 'components/preview-suspense';
 import getQueryFromSlug from 'helpers/getQueryFromSlug';
 import { docQuery } from 'lib/sanity/queries';
-import { client } from 'lib/sanity/sanity.client';
-import type { Metadata } from 'next';
-import { draftMode } from 'next/headers';
+import { clientFetch } from 'lib/sanity/sanity.client';
+// Pages.
 import CategoryPage from './category-page';
 import CategoryPagePreview from './category-page-preview';
 import HomePage from './home-page';
@@ -16,40 +19,28 @@ import SinglePagePreview from './single-page-preview';
 /**
  * Render pages depending on type.
  */
-export default async function Page({
-  params,
-}: {
-  params: { slug: string[], locale: string };
-}) {
+export default async function Page({ params }: { params: { slug: string[]; locale: string } }) {
   const { isEnabled } = draftMode();
 
   const { slug, locale } = params;
-  
-  const { query = '', queryParams, docType } = getQueryFromSlug(slug, locale)
 
-  const pageData = await client.fetch(query, queryParams)
+  const { query = '', queryParams, docType } = getQueryFromSlug(slug, locale);
 
-  const data = filterDataToSingleItem(pageData, isEnabled)
+  const pageData = await clientFetch(query, queryParams);
+
+  const data = filterDataToSingleItem(pageData, isEnabled);
 
   if (isEnabled) {
     return (
       <PreviewSuspense fallback="Loading...">
-        {docType === 'home' && (
-          <HomePagePreview query={query} queryParams={queryParams} />
-        )}
-        {docType === 'page' && (
-          <SinglePagePreview query={query} queryParams={queryParams} />
-        )}
-        {docType === 'product' && (
-          <ProductPagePreview query={query} queryParams={queryParams} />
-        )}
-        {docType === 'category' && (
-          <CategoryPagePreview query={query} queryParams={queryParams} />
-        )}
+        {docType === 'home' && <HomePagePreview query={query} queryParams={queryParams} />}
+        {docType === 'page' && <SinglePagePreview query={query} queryParams={queryParams} />}
+        {docType === 'product' && <ProductPagePreview query={query} queryParams={queryParams} />}
+        {docType === 'category' && <CategoryPagePreview query={query} queryParams={queryParams} />}
       </PreviewSuspense>
-    )
+    );
   }
-  
+
   return (
     <>
       {docType === 'home' && <HomePage data={data} />}
@@ -57,7 +48,7 @@ export default async function Page({
       {docType === 'category' && <CategoryPage data={data} />}
       {docType === 'page' && <SinglePage data={data} />}
     </>
-  )
+  );
 }
 
 // Background revalidate once every day.
@@ -67,15 +58,12 @@ export const revalidate = 86400;
  * Get paths for each page.
  */
 export async function generateStaticParams() {
-  const paths = await client.fetch(docQuery)
+  const paths = await clientFetch(docQuery);
 
-  return paths.map((path: { 
-    slug: string,
-    locale: string
-  }) => ({
+  return paths.map((path: { slug: string; locale: string }) => ({
     slug: path.slug.split('/').filter((p) => p),
     locale: path.locale
-  }))
+  }));
 }
 
 /**
@@ -83,54 +71,51 @@ export async function generateStaticParams() {
  * If we're in "preview mode" and have multiple documents, return the draft
  */
 function filterDataToSingleItem(data: any, preview = false) {
-  
   if (!Array.isArray(data)) {
-    return data
+    return data;
   }
 
   if (data.length === 1) {
-    return data[0]
+    return data[0];
   }
 
   if (preview) {
-    return data.find((item) => item._id.startsWith(`drafts.`)) || data[0]
+    return data.find((item) => item._id.startsWith(`drafts.`)) || data[0];
   }
 
-  return data[0]
+  return data[0];
 }
 
 /**
  * Generate metadata for each page.
  */
-export async function generateMetadata({ params }: {params: { slug: string[], locale: string }}): Promise<Metadata> {
-  const { slug, locale } = params
+export async function generateMetadata({
+  params
+}: {
+  params: { slug: string[]; locale: string };
+}): Promise<Metadata> {
+  const { slug, locale } = params;
 
-  const { query = '', queryParams } = getQueryFromSlug(slug, locale)
+  const { query = '', queryParams } = getQueryFromSlug(slug, locale);
 
-  const pageData = await client.fetch(query, queryParams)
+  const pageData = await clientFetch(query, queryParams);
 
-  const data = filterDataToSingleItem(pageData, false)
+  const data = filterDataToSingleItem(pageData, false);
 
   const { seo } = data ?? {};
 
   return {
     title: seo?.title ? seo?.title : data?.title,
-    description: seo?.description
-      ? seo.description
-      : 'Webb och digitalbyrå från Göteborg',
+    description: seo?.description ? seo.description : 'Webb och digitalbyrå från Göteborg',
     openGraph: {
       images: [
         {
-          url: seo?.image?.asset?.url
-            ? seo.image.asset.url
-            : '/og-image.jpg',
+          url: seo?.image?.asset?.url ? seo.image.asset.url : '/og-image.jpg',
           width: 1200,
           height: 630,
-          alt: seo?.coverImage?.alt
-            ? seo.coverImage.alt
-            : 'Kodamera AB',
-        },
-      ],
-    },
-  }
+          alt: seo?.coverImage?.alt ? seo.coverImage.alt : 'Kodamera AB'
+        }
+      ]
+    }
+  };
 }
--- lib/sanity/sanity.client.ts
+++ lib/sanity/sanity.client.ts
@@ -1,4 +1,5 @@
 import { createClient } from "next-sanity";
+import { cache } from 'react';
 
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
@@ -10,3 +11,5 @@ export const client = createClient({
   apiVersion,
   useCdn: true,
 });
+
+export const clientFetch = cache(client.fetch.bind(client));
