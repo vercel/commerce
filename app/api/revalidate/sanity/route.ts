31ecadb97fbe8150b71a892ab533ed1a454b6f47
Henrik Larsson <hankisch@gmail.com>
2023-07-03 14:16:47 +0200

    Rewrite res to response for convenience and clarity

--- app/api/revalidate/sanity/route.ts
+++ app/api/revalidate/sanity/route.ts
@@ -7,14 +7,14 @@ const SANITY_WEBHOOK_SECRET = `${process.env.SANITY_WEBHOOK_SECRET}`;
  
 export async function POST(request: NextRequest) {
   // Await the response from our request.
-  const res = await request.json();
+  const response = await request.json();
   
   // Get headers.
   const headersList = headers();
 
   // Get Sanity webhook signature header name.
   const signature = `${headersList.get(SIGNATURE_HEADER_NAME)}`;
-  const isValid = isValidSignature(JSON.stringify(res), signature, SANITY_WEBHOOK_SECRET);
+  const isValid = isValidSignature(JSON.stringify(response), signature, SANITY_WEBHOOK_SECRET);
 
   // Log out validity of request. 
   console.log(`===== Is the webhook request valid? ${isValid}`);
@@ -25,9 +25,9 @@ export async function POST(request: NextRequest) {
     return;
   }
 
-  const slug = res.slug;
-  const locale = res.locale;
-  const type = res.type;
+  const slug = response.slug;
+  const locale = response.locale;
+  const type = response.type;
   let pathToRevalidate = "";
 
   if (type === "home") {
