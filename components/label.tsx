650c9a4e289e59e492510f0fb182c9ff147f63ea
Michael Novotny <manovotny@gmail.com>
2023-07-17 11:43:33 -0500

    Minor `clsx` adjustments (#1083)

--- components/label.tsx
+++ components/label.tsx
@@ -16,19 +16,17 @@ const Label = ({
 }) => {
   return (
     <div
-      className={clsx(
-        'absolute bottom-0 left-0 flex w-full',
-        position === 'center'
-          ? 'px-4 pb-4 md:px-8 md:pb-8 lg:px-20 lg:pb-[35%]'
-          : size === 'large'
-          ? 'px-4 pb-4 md:px-8 md:pb-8'
-          : 'px-4 pb-4'
-      )}
+      className={clsx('absolute bottom-0 left-0 flex w-full px-4 pb-4', {
+        'px-4 pb-4 md:px-8 md:pb-8 lg:px-20 lg:pb-[35%]': position === 'center',
+        'px-4 pb-4 md:px-8 md:pb-8': size === 'large'
+      })}
     >
       <div
         className={clsx(
-          'flex items-center rounded-full border bg-white/80 p-1 text-black backdrop-blur-md dark:border-gray-800 dark:bg-black/80 dark:text-white',
-          size === 'large' ? 'text-sm' : 'text-xs'
+          'flex items-center rounded-full border bg-white/80 p-1 text-xs text-black backdrop-blur-md dark:border-gray-800 dark:bg-black/80 dark:text-white',
+          {
+            'text-sm': size === 'large'
+          }
         )}
       >
         <h3
