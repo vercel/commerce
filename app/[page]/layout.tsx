b6d8eed77f3717adf06a30320ef3532bdb5a0682
Michael Novotny <manovotny@gmail.com>
2023-07-17 18:38:47 -0500

    Tweaks page styles (#1087)

--- app/[page]/layout.tsx
+++ app/[page]/layout.tsx
@@ -4,7 +4,7 @@ import { Suspense } from 'react';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Suspense>
-      <div className="w-full bg-white dark:bg-black">
+      <div className="w-full">
         <div className="mx-8 max-w-2xl py-20 sm:mx-auto">
           <Suspense>{children}</Suspense>
         </div>
