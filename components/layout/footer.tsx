521b8fcb04008887a15f5226f40291618ee9188a
Michael Novotny <manovotny@gmail.com>
2023-07-17 16:59:02 -0500

    Adds company name (#1085)

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -6,17 +6,18 @@ import LogoSquare from 'components/logo-square';
 import { getMenu } from 'lib/shopify';
 import { Suspense } from 'react';
 
-const { SITE_NAME } = process.env;
+const { COMPANY_NAME, SITE_NAME } = process.env;
 
 export default async function Footer() {
   const currentYear = new Date().getFullYear();
   const copyrightDate = 2023 + (currentYear > 2023 ? `-${currentYear}` : '');
   const skeleton = 'w-full h-6 animate-pulse rounded bg-gray-200 dark:bg-gray-700';
   const menu = await getMenu('next-js-frontend-footer-menu');
+  const copyrightName = COMPANY_NAME || SITE_NAME || '';
 
   return (
     <footer className="text-gray-400 dark:text-gray-500">
-      <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-gray-200 px-6 py-12 dark:border-gray-700 md:flex-row md:gap-12 md:px-0">
+      <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-gray-200 px-6 py-12 dark:border-gray-700 md:flex-row md:gap-12 md:px-4 xl:px-0">
         <div>
           <Link className="flex items-center gap-2 text-black dark:text-white" href="/">
             <LogoSquare size="sm" />
@@ -51,7 +52,8 @@ export default async function Footer() {
       <div className="border-t border-gray-200 py-6 text-sm dark:border-gray-700">
         <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 md:flex-row md:gap-0">
           <p>
-            &copy; {copyrightDate} {SITE_NAME}. All rights reserved.
+            &copy; {copyrightDate} {copyrightName}
+            {copyrightName.length && !copyrightName.endsWith('.') ? '.' : ''} All rights reserved.
           </p>
           <hr className="mx-4 hidden h-4 w-[1px] border-l border-gray-400 md:inline-block" />
           <p>Designed in California</p>
