39cb4202c4ee19b9f5c6fbe684f766307a93c6f3
Sol Irvine <sol@zenzen.io>
2023-07-29 16:42:21 +0900

    wip: Basic site working

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Suspense } from 'react';
 export const runtime = 'edge';
 
 export const metadata = {
-  description: 'High-performance ecommerce store built with Next.js, Vercel, and Shopify.',
+  description: 'narai by suginomori brewery.',
   openGraph: {
     type: 'website'
   }
