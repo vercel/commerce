3811fb3807714f9406b0d9d287d7202a3c02dd5f
Michael Novotny <manovotny@gmail.com>
2023-08-02 20:22:08 -0500

    Fix lints

--- app/product/[handle]/page.tsx
+++ app/product/[handle]/page.tsx
@@ -117,12 +117,11 @@ async function RelatedProducts({ id }: { id: string }) {
       <h2 className="mb-4 text-2xl font-bold">Related Products</h2>
       <ul className="flex w-full gap-4 overflow-x-auto pt-1">
         {relatedProducts.map((product) => (
-          <li className="aspect-square w-full flex-none min-[475px]:w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5">
-            <Link
-              key={product.handle}
-              className="relative h-full w-full"
-              href={`/product/${product.handle}`}
-            >
+          <li
+            key={product.handle}
+            className="aspect-square w-full flex-none min-[475px]:w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5"
+          >
+            <Link className="relative h-full w-full" href={`/product/${product.handle}`}>
               <GridTileImage
                 alt={product.title}
                 label={{
--- components/carousel.tsx
+++ components/carousel.tsx
@@ -12,12 +12,11 @@ export async function Carousel() {
     <div className=" w-full overflow-x-auto pb-6 pt-1">
       <ul className="flex animate-carousel gap-4">
         {[...products, ...products].map((product, i) => (
-          <li className="relative aspect-square h-[30vh] max-h-[275px] w-2/3 max-w-[475px] flex-none md:w-1/3">
-            <Link
-              key={`${product.handle}${i}`}
-              href={`/product/${product.handle}`}
-              className="relative h-full w-full"
-            >
+          <li
+            key={`${product.handle}${i}`}
+            className="relative aspect-square h-[30vh] max-h-[275px] w-2/3 max-w-[475px] flex-none md:w-1/3"
+          >
+            <Link href={`/product/${product.handle}`} className="relative h-full w-full">
               <GridTileImage
                 alt={product.title}
                 label={{
