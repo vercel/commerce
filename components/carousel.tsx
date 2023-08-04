ef92d578cd6e845a7725ac98c7d22dbc6834354b
Michael Novotny <manovotny@gmail.com>
2023-08-03 21:17:02 -0500

    Adds more home page carousel products for wide screens (#1142)

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -8,10 +8,13 @@ export async function Carousel() {
 
   if (!products?.length) return null;
 
+  // Purposefully duplicating products to make the carousel loop and not run out of products on wide screens.
+  const carouselProducts = [...products, ...products, ...products];
+
   return (
     <div className=" w-full overflow-x-auto pb-6 pt-1">
       <ul className="flex animate-carousel gap-4">
-        {[...products, ...products].map((product, i) => (
+        {carouselProducts.map((product, i) => (
           <li
             key={`${product.handle}${i}`}
             className="relative aspect-square h-[30vh] max-h-[275px] w-2/3 max-w-[475px] flex-none md:w-1/3"
