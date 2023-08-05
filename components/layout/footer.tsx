c3f393673283ea6a394bead43ed951eb3e0fc785
Michael Novotny <manovotny@gmail.com>
2023-08-04 20:19:52 -0500

    Changes footer source button to deploy button (#1151)

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 
-import GitHubIcon from 'components/icons/github';
 import FooterMenu from 'components/layout/footer-menu';
 import LogoSquare from 'components/logo-square';
 import { getMenu } from 'lib/shopify';
@@ -40,12 +39,13 @@ export default async function Footer() {
         </Suspense>
         <div className="md:ml-auto">
           <a
-            className="flex items-center gap-2 text-lg hover:text-black dark:hover:text-neutral-300 md:text-sm"
-            aria-label="Github Repository"
-            href="https://github.com/vercel/commerce"
+            className="flex h-8 flex-none items-center justify-center rounded-md border border-neutral-200 bg-white text-xs text-black dark:border-neutral-700 dark:bg-black dark:text-white"
+            aria-label="Deploy on Vercel"
+            href="https://vercel.com/templates/next.js/nextjs-commerce"
           >
-            <GitHubIcon className="h-6" />
-            <p>Source</p>
+            <span className="px-3">▲</span>
+            <hr className="h-full border-r border-neutral-200 dark:border-neutral-700" />
+            <span className="px-3">Deploy</span>
           </a>
         </div>
       </div>
