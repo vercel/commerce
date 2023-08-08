3f1a4f65aed78f0181f7e7ce1922038ce0c539a0
Michael Novotny <manovotny@gmail.com>
2023-08-08 10:05:38 -0500

    Fixes product detail spacing (#1158)

--- app/product/[handle]/page.tsx
+++ app/product/[handle]/page.tsx
@@ -82,7 +82,7 @@ export default async function ProductPage({ params }: { params: { handle: string
         }}
       />
       <div className="mx-auto max-w-screen-2xl px-4">
-        <div className="flex flex-col rounded-lg border border-neutral-200 bg-white p-8 dark:border-neutral-800 dark:bg-black md:p-12 lg:flex-row">
+        <div className="flex flex-col rounded-lg border border-neutral-200 bg-white p-8 dark:border-neutral-800 dark:bg-black md:p-12 lg:flex-row lg:gap-8">
           <div className="h-full w-full basis-full lg:basis-4/6">
             <Gallery
               images={product.images.map((image: Image) => ({
