14743f44d19ef6a10a9a1886a8a4f0463d20096c
Michael Novotny <manovotny@gmail.com>
2023-08-08 16:09:52 -0500

    Prettier

--- components/grid/tile.tsx
+++ components/grid/tile.tsx
@@ -20,7 +20,7 @@ export function GridTileImage({
   return (
     <div
       className={clsx(
-        'flex h-full w-full items-center justify-center overflow-hidden rounded-lg border group bg-white hover:border-blue-600 dark:bg-black',
+        'group flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-white hover:border-blue-600 dark:bg-black',
         {
           relative: label,
           'border-2 border-blue-600': active,
