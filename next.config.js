48c5d3b0f97f1bce5616a3336aa1725f6e6daff1
Ghita Lucian <ghita@adobe.com>
2023-08-10 19:45:34 +0200

    output: "standalone",

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  output: 'standalone',
   eslint: {
     // Disabling on production builds because we're running checks on PRs via GitHub Actions.
     ignoreDuringBuilds: true
