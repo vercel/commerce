92cb1b6174bf66595aede8b7a81875173c4f81e1
June <june.zhao@yahoo.com>
2023-08-11 18:46:33 +0200

    set SEK format without decimals

--- components/price.tsx
+++ components/price.tsx
@@ -1,9 +1,7 @@
-import clsx from 'clsx';
-
 const Price = ({
   amount,
   className,
-  currencyCode = 'USD',
+  currencyCode = 'SEK',
   currencyCodeClassName
 }: {
   amount: string;
@@ -12,12 +10,13 @@ const Price = ({
   currencyCodeClassName?: string;
 } & React.ComponentProps<'p'>) => (
   <p suppressHydrationWarning={true} className={className}>
-    {`${new Intl.NumberFormat(undefined, {
+    {`${new Intl.NumberFormat('sv-SE', {
       style: 'currency',
       currency: currencyCode,
-      currencyDisplay: 'narrowSymbol'
+      currencyDisplay: 'narrowSymbol',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0
     }).format(parseFloat(amount))}`}
-    <span className={clsx('ml-1 inline', currencyCodeClassName)}>{`${currencyCode}`}</span>
   </p>
 );
 
