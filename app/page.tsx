4c273a83c0f141ea3373ddea63c7c4a25958303b
Sol Irvine <sol@zenzen.io>
2023-08-13 15:23:08 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Carousel } from 'components/carousel';
 import { ThreeItemGrid } from 'components/grid/three-items';
 import Footer from 'components/layout/footer';
+import { LanguageControl } from 'components/layout/navbar/language-control';
 import Image from 'next/image';
 import Namemark from 'public/assets/images/namemark.png';
 import { Suspense } from 'react';
@@ -18,11 +19,7 @@ export default async function HomePage() {
   return (
     <>
       <div className="invisible absolute right-40 top-12 md:visible">
-        <div className="flex flex-row space-x-0">
-          <span className="px-2 py-4">JP</span>
-          <span className="py-4">/</span>
-          <span className="px-2 py-4">EN</span>
-        </div>
+        <LanguageControl />
       </div>
       <div className="px-6 pb-12 pt-6 md:py-12 md:pl-6">
         <Image
