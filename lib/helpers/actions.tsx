ff99d738bfc6b934e8d1dfaa506f0ad88e0a7511
Samantha Kellow <sammii.h@icloud.com>
2023-08-20 20:06:02 +0100

    Fix getKeyByValue

--- lib/helpers/actions.tsx
+++ lib/helpers/actions.tsx
@@ -2,10 +2,10 @@ export function copyText(text: string) {
   navigator.clipboard.writeText(text);
 }
 
-export function capitalizeFirstLetter(string) {
+export function capitalizeFirstLetter(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export function getKeyByValue(object, value) {
-  return Object.keys(object).find(key => object[key] === value);
+export function getKeyByValue<O extends object>(object: O, value: O[keyof O]) {
+  return Object.keys(object).find(key => object[key as keyof O] === value);
 }
