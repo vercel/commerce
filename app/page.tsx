1adda37f2ec27cff33e71b82e6705ccea9bc9e0c
Lee Robinson <lrobinson2011@gmail.com>
2023-09-02 11:11:20 -0500

    rip

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import { ThreeItemGrid } from 'components/grid/three-items';
 import Footer from 'components/layout/footer';
 import { Suspense } from 'react';
 
-export const runtime = 'edge';
-
 export const metadata = {
   description: 'High-performance ecommerce store built with Next.js, Vercel, and Shopify.',
   openGraph: {
