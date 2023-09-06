8c194a6ca1ec3f8d534ae4c2a1169acee03f6ed5
Sol Irvine <sol@zenzen.io>
2023-09-06 13:38:48 -0700

    fix: Mailchimp signups
    Also, inline age check

--- components/cart/modal.tsx
+++ components/cart/modal.tsx
@@ -49,7 +49,16 @@ export default function CartModal({
       // Always update the quantity reference
       quantityRef.current = cart?.totalQuantity;
     }
+
+    return () => {
+      setIsConfirming(false);
+    };
   }, [isOpen, cart?.totalQuantity, quantityRef]);
+
+  const checkoutWithAgeCheck = () => {
+    setIsOpen(true);
+    setIsConfirming(true);
+  };
 
   return (
     <>
@@ -87,136 +96,141 @@ export default function CartModal({
                 </button>
               </div>
 
-              {!cart || cart.lines.length === 0 ? (
-                <div className="mt-20 flex w-full flex-col items-center justify-center overflow-hidden">
-                  <ShoppingBagIcon className="h-16" strokeWidth={1} />
-                  <p className="mt-6 text-center font-serif text-xl">Your shopping bag is empty.</p>
-                </div>
+              {!!isConfirming && !!cart && cart?.checkoutUrl ? (
+                <AgeGateForm
+                  didCancel={() => setIsConfirming(false)}
+                  checkoutUrl={cart.checkoutUrl}
+                />
               ) : (
-                <div className="flex h-full flex-col justify-between overflow-hidden p-1">
-                  <ul className="grow overflow-auto py-4">
-                    {cart.lines.map((item, i) => {
-                      const merchandiseSearchParams = {} as MerchandiseSearchParams;
-
-                      item.merchandise.selectedOptions.forEach(({ name, value }) => {
-                        if (value !== DEFAULT_OPTION) {
-                          merchandiseSearchParams[name.toLowerCase()] = value;
-                        }
-                      });
-
-                      const merchandiseUrl = createUrl(
-                        `/product/${item.merchandise.product.handle}`,
-                        new URLSearchParams(merchandiseSearchParams)
-                      );
-
-                      return (
-                        <li key={i} className="flex w-full flex-col border-b border-white/20">
-                          <div className="relative flex w-full flex-row justify-between px-1 py-4">
-                            <div className="absolute z-40 -mt-2 ml-[55px]">
-                              <DeleteItemButton item={item} />
-                            </div>
-                            <Link
-                              href={merchandiseUrl}
-                              onClick={closeCart}
-                              className="z-30 flex flex-row space-x-4"
-                            >
-                              <div className="relative h-16 w-16 cursor-pointer overflow-hidden rounded-md border border-white/20 bg-white/40">
-                                <Image
-                                  className="h-full w-full object-cover"
-                                  width={64}
-                                  height={64}
-                                  alt={
-                                    item.merchandise.product.featuredImage.altText ||
-                                    item.merchandise.product.title
-                                  }
-                                  src={item.merchandise.product.featuredImage.url}
-                                />
-                              </div>
-
-                              <div className="flex flex-1 flex-col text-base">
-                                <span className="leading-tight">
-                                  {item.merchandise.product.title}
-                                </span>
-                                {item.merchandise.title !== DEFAULT_OPTION ? (
-                                  <p className="text-sm text-white">{item.merchandise.title}</p>
-                                ) : null}
-                              </div>
-                            </Link>
-                            <div className="flex h-16 flex-col justify-between">
-                              <Price
-                                className="flex justify-end space-y-2 text-right text-sm"
-                                amount={item.cost.totalAmount.amount}
-                                currencyCode={item.cost.totalAmount.currencyCode}
-                              />
-                              <div className="ml-auto flex h-9 flex-row items-center rounded-full border border-white/20">
-                                <EditItemQuantityButton item={item} type="minus" />
-                                <p className="w-6 text-center">
-                                  <span className="w-full text-sm">{item.quantity}</span>
-                                </p>
-                                <EditItemQuantityButton item={item} type="plus" />
-                              </div>
-                            </div>
-                          </div>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                  {!!promotedItem && (
-                    <PromotedCartItem
-                      product={promotedItem}
-                      availableForSale={promotedItem.availableForSale}
-                    />
-                  )}
-                  <div className="pb-4 pt-12 text-sm text-neutral-500 dark:text-neutral-400">
-                    <div className="mb-3 flex items-center justify-between border-b border-white/20 pb-1">
-                      <p>Taxes</p>
-                      <p className="text-right text-white/50">Calculated at checkout</p>
+                <>
+                  {!cart || cart.lines.length === 0 ? (
+                    <div className="mt-20 flex w-full flex-col items-center justify-center overflow-hidden">
+                      <ShoppingBagIcon className="h-16" strokeWidth={1} />
+                      <p className="mt-6 text-center font-serif text-xl">
+                        Your shopping bag is empty.
+                      </p>
                     </div>
-                    <div className="mb-3 flex items-center justify-between border-b border-white/20 py-1">
-                      <p>Shipping</p>
-                      <p className="text-right text-white/50">Calculated at checkout</p>
-                    </div>
-                    <div className="mb-3 flex items-center justify-between border-b border-white/20 py-1">
-                      <p>Total</p>
-                      <Price
-                        className="text-right text-base text-white"
-                        amount={cart.cost.totalAmount.amount}
-                        currencyCode={cart.cost.totalAmount.currencyCode}
-                      />
-                    </div>
-                  </div>
-                  {ageConfirmed ? (
-                    <>
-                      <Link
-                        href={cart.checkoutUrl}
-                        className="block w-full border border-white/20 bg-dark px-12 py-6 text-center font-sans font-medium uppercase tracking-wider text-white transition-colors duration-300 hover:bg-white hover:text-black"
-                      >
-                        {t('cart.proceed')}
-                      </Link>
-                    </>
                   ) : (
-                    <>
-                      <button
-                        type="button"
-                        onClick={() => setIsConfirming(true)}
-                        className="block w-full border border-white/20 bg-dark px-12 py-6 text-center font-sans font-medium uppercase tracking-wider text-white transition-colors duration-300 hover:bg-white hover:text-black"
-                      >
-                        {t('cart.proceed')}
-                      </button>
-                    </>
+                    <div className="flex h-full flex-col justify-between overflow-hidden p-1">
+                      <ul className="grow overflow-auto py-4">
+                        {cart.lines.map((item, i) => {
+                          const merchandiseSearchParams = {} as MerchandiseSearchParams;
+
+                          item.merchandise.selectedOptions.forEach(({ name, value }) => {
+                            if (value !== DEFAULT_OPTION) {
+                              merchandiseSearchParams[name.toLowerCase()] = value;
+                            }
+                          });
+
+                          const merchandiseUrl = createUrl(
+                            `/product/${item.merchandise.product.handle}`,
+                            new URLSearchParams(merchandiseSearchParams)
+                          );
+
+                          return (
+                            <li key={i} className="flex w-full flex-col border-b border-white/20">
+                              <div className="relative flex w-full flex-row justify-between px-1 py-4">
+                                <div className="absolute z-40 -mt-2 ml-[55px]">
+                                  <DeleteItemButton item={item} />
+                                </div>
+                                <Link
+                                  href={merchandiseUrl}
+                                  onClick={closeCart}
+                                  className="z-30 flex flex-row space-x-4"
+                                >
+                                  <div className="relative h-16 w-16 cursor-pointer overflow-hidden rounded-md border border-white/20 bg-white/40">
+                                    <Image
+                                      className="h-full w-full object-cover"
+                                      width={64}
+                                      height={64}
+                                      alt={
+                                        item.merchandise.product.featuredImage.altText ||
+                                        item.merchandise.product.title
+                                      }
+                                      src={item.merchandise.product.featuredImage.url}
+                                    />
+                                  </div>
+
+                                  <div className="flex flex-1 flex-col text-base">
+                                    <span className="leading-tight">
+                                      {item.merchandise.product.title}
+                                    </span>
+                                    {item.merchandise.title !== DEFAULT_OPTION ? (
+                                      <p className="text-sm text-white">{item.merchandise.title}</p>
+                                    ) : null}
+                                  </div>
+                                </Link>
+                                <div className="flex h-16 flex-col justify-between">
+                                  <Price
+                                    className="flex justify-end space-y-2 text-right text-sm"
+                                    amount={item.cost.totalAmount.amount}
+                                    currencyCode={item.cost.totalAmount.currencyCode}
+                                  />
+                                  <div className="ml-auto flex h-9 flex-row items-center rounded-full border border-white/20">
+                                    <EditItemQuantityButton item={item} type="minus" />
+                                    <p className="w-6 text-center">
+                                      <span className="w-full text-sm">{item.quantity}</span>
+                                    </p>
+                                    <EditItemQuantityButton item={item} type="plus" />
+                                  </div>
+                                </div>
+                              </div>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                      {!!promotedItem && (
+                        <PromotedCartItem
+                          product={promotedItem}
+                          availableForSale={promotedItem.availableForSale}
+                        />
+                      )}
+                      <div className="pb-4 pt-12 text-sm text-neutral-500 dark:text-neutral-400">
+                        <div className="mb-3 flex items-center justify-between border-b border-white/20 pb-1">
+                          <p>Taxes</p>
+                          <p className="text-right text-white/50">Calculated at checkout</p>
+                        </div>
+                        <div className="mb-3 flex items-center justify-between border-b border-white/20 py-1">
+                          <p>Shipping</p>
+                          <p className="text-right text-white/50">Calculated at checkout</p>
+                        </div>
+                        <div className="mb-3 flex items-center justify-between border-b border-white/20 py-1">
+                          <p>Total</p>
+                          <Price
+                            className="text-right text-base text-white"
+                            amount={cart.cost.totalAmount.amount}
+                            currencyCode={cart.cost.totalAmount.currencyCode}
+                          />
+                        </div>
+                      </div>
+                      {ageConfirmed ? (
+                        <>
+                          <Link
+                            href={cart.checkoutUrl}
+                            className="block w-full border border-white/20 bg-dark px-12 py-6 text-center font-sans font-medium uppercase tracking-wider text-white transition-colors duration-300 hover:bg-white hover:text-black"
+                          >
+                            {t('cart.proceed')}
+                          </Link>
+                        </>
+                      ) : (
+                        <>
+                          <button
+                            type="button"
+                            onClick={() => checkoutWithAgeCheck()}
+                            className="block w-full border border-white/20 bg-dark px-12 py-6 text-center font-sans font-medium uppercase tracking-wider text-white transition-colors duration-300 hover:bg-white hover:text-black"
+                          >
+                            {t('cart.proceed')}
+                          </button>
+                        </>
+                      )}
+                    </div>
                   )}
-                  {/* <AgeConfirmBeforeCheckout checkoutUrl={cart.checkoutUrl}>
-                    {t('cart.proceed')}
-                  </AgeConfirmBeforeCheckout> */}
-                </div>
+                </>
               )}
             </Dialog.Panel>
           </Transition.Child>
         </Dialog>
       </Transition>
-      {!!isConfirming && !!cart && cart?.checkoutUrl && (
-        <AgeGateForm didCancel={() => setIsConfirming(false)} checkoutUrl={cart.checkoutUrl} />
-      )}
     </>
   );
 }
