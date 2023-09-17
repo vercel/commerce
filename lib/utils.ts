74b7c91ad193968b95414d6d1b9569b960b526fd
Michael Novotny <manovotny@gmail.com>
2023-09-16 20:00:23 -0500

    Adds link

--- lib/utils.ts
+++ lib/utils.ts
@@ -22,7 +22,7 @@ export const validateEnvironmentVariables = () => {
 
   if (missingEnvironmentVariables.length) {
     throw new Error(
-      `The following environment variables are missing. Your site will not work without them.\n\n${missingEnvironmentVariables.join(
+      `The following environment variables are missing. Your site will not work without them. Read more: https://vercel.com/docs/integrations/shopify#configure-environment-variables\n\n${missingEnvironmentVariables.join(
         '\n'
       )}\n`
     );
