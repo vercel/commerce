154bda33fe604d88813b11c50a1486c9b03bda24
Sol Irvine <sol@zenzen.io>
2023-10-09 07:34:59 +0900

    more locale updates

--- messages/index.ts
+++ messages/index.ts
@@ -4,8 +4,8 @@ import 'server-only';
 // We enumerate all dictionaries here for better linting and typescript support
 // We also get the default import for cleaner types
 const dictionaries = {
-  en: () => import('./en.json').then((module) => module.default),
-  ja: () => import('./ja.json').then((module) => module.default)
+  ja: () => import('./ja.json').then((module) => module.default),
+  en: () => import('./en.json').then((module) => module.default)
 };
 
 export const getDictionary = async (locale: SupportedLocale) =>
