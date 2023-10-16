7e11ee3d470ea1d7698c0fea2473981ce9e3add9
Michele Riva <ciao@micheleriva.it>
2023-10-16 21:05:42 +0200

    removes unsupported filters

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,8 +14,6 @@ export const defaultSort: SortFilterItem = {
 
 export const sorting: SortFilterItem[] = [
   defaultSort,
-  { title: 'Trending', slug: 'trending-desc', sortKey: 'BEST_SELLING', reverse: false }, // asc
-  { title: 'Latest arrivals', slug: 'latest-desc', sortKey: 'CREATED_AT', reverse: true },
   { title: 'Price: Low to high', slug: 'price-asc', sortKey: 'PRICE', reverse: false }, // asc
   { title: 'Price: High to low', slug: 'price-desc', sortKey: 'PRICE', reverse: true }
 ];
