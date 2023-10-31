dc410e240410baa8631f186c072d6669f1c189bd
Sol Irvine <sol@zenzen.io>
2023-10-31 16:03:50 +0900

    Update layout.tsx

--- app/[locale]/shop-list/layout.tsx
+++ app/[locale]/shop-list/layout.tsx
@@ -1,5 +1,7 @@
 import { Suspense } from 'react';
 
+export const revalidate = 300; // 5 minutes in seconds
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <Suspense>{children}</Suspense>;
 }
