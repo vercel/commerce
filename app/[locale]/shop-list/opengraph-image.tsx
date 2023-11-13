0ce215fa7584ec9db2a1ac1a498ba7e30b48dd5d
Sol Irvine <sol@zenzen.io>
2023-11-13 13:40:33 +0900

    fix: Issue with shoplist

--- app/[locale]/shop-list/opengraph-image.tsx
+++ app/[locale]/shop-list/opengraph-image.tsx
@@ -1,8 +1,16 @@
+import { SupportedLocale } from 'components/layout/navbar/language-control';
 import OpengraphImage from 'components/opengraph-image';
 import { getPage } from 'lib/shopify';
 
-export default async function Image({ params }: { params: { page: string } }) {
-  const page = await getPage({ handle: params.page });
+export default async function Image({
+  params
+}: {
+  params: { page: string; locale?: SupportedLocale };
+}) {
+  const page = await getPage({
+    handle: params.page,
+    language: params?.locale?.toUpperCase() || 'JA'
+  });
   const title = page.seo?.title || page.title;
 
   return await OpengraphImage({ title });
