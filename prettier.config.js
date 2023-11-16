ca5787dbaac12120ade44d21dbed5499161d9ea4
leonmargaritis <leon.margaritis@kernpunkt.de>
2023-11-16 16:26:11 +0100

    revert: revert eslint config and prettier config

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  singleQuote: false,
+  arrowParens: 'always',
+  trailingComma: 'none',
+  printWidth: 100,
+  tabWidth: 2,
+  plugins: ['prettier-plugin-tailwindcss']
+};
