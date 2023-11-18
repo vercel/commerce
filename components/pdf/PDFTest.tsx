dc4c20be9b9bfcc32e77578cefaa8b67dbf06836
Benson Wally Tran <bewallyt@gmail.com>
2023-11-17 19:29:41 -0600

    proof of concept

--- components/pdf/PDFTest.tsx
+++ components/pdf/PDFTest.tsx
@@ -1,43 +1,225 @@
 'use client';
 
-import { Document, Page, StyleSheet, Text, View } from '@react-pdf/renderer';
+import { Button } from '@/components/ui/button';
+import { Document, Font, Image, Page, StyleSheet, Text, View } from '@react-pdf/renderer';
+import { LoremIpsum } from 'lorem-ipsum';
 import dynamic from 'next/dynamic';
+import { useMemo, useState } from 'react';
 
 export const runtime = 'edge';
+
+const imgURL =
+  'https://cdn.discordapp.com/attachments/989274756341706822/1175024578578366534/pinturillu_sian_couple_of_men_illustration_fantasy_Charlie_Bowa_1c51f19c-d5b9-4b53-b32f-e5468912bd1d.png?ex=6569b9ea&is=655744ea&hm=8dd0e4e5653bb9f7a7330f745983035f93e1891279351efe2dd6be7657987d88&';
 
 const PDFViewerNoSSR = dynamic(() => import('@react-pdf/renderer').then((mod) => mod.PDFViewer), {
   ssr: false
 });
 
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 6,
+    min: 1
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4
+  }
+});
+
 export default function PDFTest() {
+  const [paragraphs, setParagraphs] = useState([
+    lorem.generateParagraphs(1),
+    lorem.generateParagraphs(1),
+    lorem.generateParagraphs(1),
+    lorem.generateParagraphs(1)
+  ]);
   return (
-    <PDFViewerNoSSR>
-      <MyDocument />
-    </PDFViewerNoSSR>
+    <div
+      style={{
+        width: '100vw',
+        height: 'calc(100vh - 76px)',
+        position: 'relative'
+      }}
+    >
+      <Button
+        style={styles.button}
+        onClick={() => {
+          setParagraphs([
+            lorem.generateParagraphs(1),
+            lorem.generateParagraphs(1),
+            lorem.generateParagraphs(1),
+            lorem.generateParagraphs(1)
+          ]);
+        }}
+      >
+        Remix Text
+      </Button>
+      <PDFViewerNoSSR style={styles.pdfContainer}>
+        <MyDocument paragraphs={paragraphs} />
+      </PDFViewerNoSSR>
+    </div>
   );
 }
 
-const MyDocument = () => (
-  <Document>
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>Section #1</Text>
-      </View>
-      <View style={styles.section}>
-        <Text>Section #2</Text>
-      </View>
-    </Page>
-  </Document>
-);
+Font.register({
+  family: 'JosefinSans',
+  fonts: [
+    { src: '/fonts/Josefin_Sans/static/JosefinSans-Thin.ttf', fontWeight: 100 },
+    {
+      src: '/fonts/Josefin_Sans/static/JosefinSans-ExtraLight.ttf',
+      fontWeight: 200
+    },
+    {
+      src: '/fonts/Josefin_Sans/static/JosefinSans-Light.ttf',
+      fontWeight: 300
+    },
+    {
+      src: '/fonts/Josefin_Sans/static/JosefinSans-Regular.ttf',
+      fontWeight: 400
+    },
+    {
+      src: '/fonts/Josefin_Sans/static/JosefinSans-Medium.ttf',
+      fontWeight: 500
+    },
+    {
+      src: '/fonts/Josefin_Sans/static/JosefinSans-SemiBold.ttf',
+      fontWeight: 600
+    },
+    { src: '/fonts/Josefin_Sans/static/JosefinSans-Bold.ttf', fontWeight: 700 },
+    {
+      src: '/fonts/Josefin_Sans/static/JosefinSans-ExtraBold.ttf',
+      fontWeight: 800
+    },
+    {
+      src: '/fonts/Josefin_Sans/static/JosefinSans-Black.ttf',
+      fontWeight: 900
+    },
+    {
+      src: '/fonts/Josefin_Sans/static/JosefinSans-ThinItalic.ttf',
+      fontStyle: 'italic',
+      fontWeight: 100
+    },
+    {
+      src: '/fonts/Josefin_Sans/static/JosefinSans-ExtraLightItalic.ttf',
+      fontStyle: 'italic',
+      fontWeight: 200
+    },
+    {
+      src: '/fonts/Josefin_Sans/static/JosefinSans-LightItalic.ttf',
+      fontStyle: 'italic',
+      fontWeight: 300
+    },
+    {
+      src: '/fonts/Josefin_Sans/static/JosefinSans-Italic.ttf',
+      fontStyle: 'italic',
+      fontWeight: 400
+    },
+    {
+      src: '/fonts/Josefin_Sans/static/JosefinSans-MediumItalic.ttf',
+      fontStyle: 'italic',
+      fontWeight: 500
+    },
+    {
+      src: '/fonts/Josefin_Sans/static/JosefinSans-SemiBoldItalic.ttf',
+      fontStyle: 'italic',
+      fontWeight: 600
+    },
+    {
+      src: '/fonts/Josefin_Sans/static/JosefinSans-BoldItalic.ttf',
+      fontStyle: 'italic',
+      fontWeight: 700
+    },
+    {
+      src: '/fonts/Josefin_Sans/static/JosefinSans-ExtraBoldItalic.ttf',
+      fontStyle: 'italic',
+      fontWeight: 800
+    },
+    {
+      src: '/fonts/Josefin_Sans/static/JosefinSans-BlackItalic.ttf',
+      fontStyle: 'italic',
+      fontWeight: 900
+    }
+  ]
+});
+
+const MyDocument = ({ paragraphs }: { paragraphs: string[] }) => {
+  const leftSectionPositions = useMemo(() => {
+    // Generate first value between 5 and 25
+    const firstValue = 5;
+    // Generate second value between 50 and 75
+    const secondValue = Math.random() * 25 + 25;
+    return [firstValue, secondValue];
+  }, []);
+  const rightSectionPositions = useMemo(() => {
+    const firstValue = Math.random() * 25 + 5;
+    const secondValue = Math.random() * 15 + 50;
+    return [firstValue, secondValue];
+  }, []);
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.column}>
+          {leftSectionPositions.map((topPosition, index) => (
+            <View key={index} style={{ ...styles.section, top: `${topPosition}%` }}>
+              <Text>{paragraphs[index]}</Text>
+            </View>
+          ))}
+        </View>
+        <View style={styles.column}>
+          {rightSectionPositions.map((topPosition, index) => (
+            <View key={index} style={{ ...styles.section, top: `${topPosition}%` }}>
+              <Text>{paragraphs[index + 2]}</Text>
+            </View>
+          ))}
+        </View>
+        <Image src={imgURL} style={styles.image} />
+      </Page>
+    </Document>
+  );
+};
 
 const styles = StyleSheet.create({
+  pdfContainer: { width: '100%', height: '100%' },
+  image: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    zIndex: -1,
+    top: 0
+  },
   page: {
+    position: 'relative',
+    display: 'flex',
     flexDirection: 'row',
-    backgroundColor: '#E4E4E4'
+    justifyContent: 'space-between',
+    alignItems: 'flex-start'
+  },
+  column: {
+    height: '100%',
+    flexGrow: 1,
+    flexShrink: 0
   },
   section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1
+    top: 0,
+    position: 'absolute',
+    margin: 30,
+    fontFamily: 'JosefinSans',
+    /* Background properties */
+    backgroundColor: 'rgba(255, 255, 255, 0.3)' /* semi-transparent white background */,
+    /* Blur effect */
+    backdropFilter: 'blur(10px)',
+    /* Additional styling for aesthetics */
+    padding: 8,
+    borderRadius: 16,
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    fontSize: 12
+  },
+  button: {
+    position: 'absolute',
+    top: 10,
+    right: 100,
+    color: 'black',
+    background: 'white',
+    borderRadius: 4
   }
 });
---
+++ public/fonts/Josefin_Sans/README.txt
@@ -0,0 +1,77 @@
+Josefin Sans Variable Font
+==========================
+
+This download contains Josefin Sans as both variable fonts and static fonts.
+
+Josefin Sans is a variable font with this axis:
+  wght
+
+This means all the styles are contained in these files:
+  JosefinSans-VariableFont_wght.ttf
+  JosefinSans-Italic-VariableFont_wght.ttf
+
+If your app fully supports variable fonts, you can now pick intermediate styles
+that aren’t available as static fonts. Not all apps support variable fonts, and
+in those cases you can use the static font files for Josefin Sans:
+  static/JosefinSans-Thin.ttf
+  static/JosefinSans-ExtraLight.ttf
+  static/JosefinSans-Light.ttf
+  static/JosefinSans-Regular.ttf
+  static/JosefinSans-Medium.ttf
+  static/JosefinSans-SemiBold.ttf
+  static/JosefinSans-Bold.ttf
+  static/JosefinSans-ThinItalic.ttf
+  static/JosefinSans-ExtraLightItalic.ttf
+  static/JosefinSans-LightItalic.ttf
+  static/JosefinSans-Italic.ttf
+  static/JosefinSans-MediumItalic.ttf
+  static/JosefinSans-SemiBoldItalic.ttf
+  static/JosefinSans-BoldItalic.ttf
+
+Get started
+-----------
+
+1. Install the font files you want to use
+
+2. Use your app's font picker to view the font family and all the
+available styles
+
+Learn more about variable fonts
+-------------------------------
+
+  https://developers.google.com/web/fundamentals/design-and-ux/typography/variable-fonts
+  https://variablefonts.typenetwork.com
+  https://medium.com/variable-fonts
+
+In desktop apps
+
+  https://theblog.adobe.com/can-variable-fonts-illustrator-cc
+  https://helpx.adobe.com/nz/photoshop/using/fonts.html#variable_fonts
+
+Online
+
+  https://developers.google.com/fonts/docs/getting_started
+  https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Fonts/Variable_Fonts_Guide
+  https://developer.microsoft.com/en-us/microsoft-edge/testdrive/demos/variable-fonts
+
+Installing fonts
+
+  MacOS: https://support.apple.com/en-us/HT201749
+  Linux: https://www.google.com/search?q=how+to+install+a+font+on+gnu%2Blinux
+  Windows: https://support.microsoft.com/en-us/help/314960/how-to-install-or-remove-a-font-in-windows
+
+Android Apps
+
+  https://developers.google.com/fonts/docs/android
+  https://developer.android.com/guide/topics/ui/look-and-feel/downloadable-fonts
+
+License
+-------
+Please read the full license text (OFL.txt) to understand the permissions,
+restrictions and requirements for usage, redistribution, and modification.
+
+You can use them in your products & projects – print or digital,
+commercial or otherwise.
+
+This isn't legal advice, please consider consulting a lawyer and see the full
+license for all details.
