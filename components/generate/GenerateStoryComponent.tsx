35cf58a5109d14a0281529f84afe2bd1f839ff2f
Patricio Dieck <patricio.dieck@outlook.com>
2023-11-19 13:50:08 -0600

    the dalle3 cover renders now on ui lesgoooo

--- components/generate/GenerateStoryComponent.tsx
+++ components/generate/GenerateStoryComponent.tsx
@@ -1,16 +1,29 @@
 'use-client';
+import Image from 'next/image';
 import chatOperations from 'operations/chatOperations';
+import imageOperations from 'operations/imageOperations';
 import { useState } from 'react';
 import GenerateStoryContextProvider, { IGenerateStoryContext } from './GenerateStoryContext';
 
 export default function GenerateStoryComponent() {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState();
+  const [cover, setCover] = useState('');
 
   const getStory = async () => {
     setLoading(true);
     const data = await chatOperations.createStoryAsync();
     setData(data);
+    setLoading(false);
+  };
+
+  const getCover = async () => {
+    setLoading(true);
+    const data = await imageOperations.createImageAsync(
+      'make a cover image of the best book in the world for children'
+    );
+    const coverURL = data.text[0].url;
+    setCover(coverURL);
     setLoading(false);
   };
 
@@ -26,7 +39,19 @@ export default function GenerateStoryComponent() {
               Run A New Story
             </button>
 
+            <button
+              onClick={getCover}
+              className="mb-10 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            >
+              Get a Title Image
+            </button>
+
             {loading ? <div className="mb10">Loading...</div> : null}
+
+            {cover && <img src={cover} alt="" width={500} height={500} />}
+
+            {JSON.stringify(data)}
+
             {JSON.stringify(data)}
           </main>
         );
