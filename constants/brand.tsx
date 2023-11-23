1b387d80735ad4beff4a59a77a54a6aec0e12b2b
Samantha Kellow <sammii.h@icloud.com>
2023-11-23 11:31:03 +0000

    brand overview details consts

--- constants/brand.tsx
+++ constants/brand.tsx
@@ -1,1 +1,46 @@
 export const SCAPE = 'scape²'
+
+export const brandOverviewDetails = [
+  {
+    heading: "conciously created",
+    content: "created with care, always",
+  },
+  {
+    heading: "art for the wall & wardrobe",
+    content: "if it's good enough for the wall, it's good enough to wear",
+  },
+  {
+    heading: "altering angles",
+    content: "through the power of photography",
+  },
+];
+
+export const wallOverviewDetails = [
+  {
+    heading: "conciously created",
+    content: "FSC paper, carbon neutral shipping",
+  },
+  {
+    heading: "art for the wall",
+    content: "because I did take a picture of it",
+  },
+  {
+    heading: "altering angles",
+    content: "lets look at familiar things in unfamiliar ways",
+  },
+];
+
+export const wardrobeOverviewDetails = [
+  {
+    heading: "conciously created",
+    content: "organic cotton and 100% plastic free packaging",
+  },
+  {
+    heading: "art for the wardrobe",
+    content: "why leave art on the wall?",
+  },
+  {
+    heading: "altering angles",
+    content: "style shouldn't lack sustainability",
+  },
+];
