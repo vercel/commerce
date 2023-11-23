13132c883c485ea930f91d1896a21f83e19676db
Samantha Kellow <sammii.h@icloud.com>
2023-11-23 16:13:04 +0000

    updating homepage with new categories WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
+import { Button } from '@/components/ui/button';
 import { Carousel } from 'components/carousel';
-import Grid from 'components/grid';
 import Footer from 'components/layout/footer';
-import ProductGridItems from 'components/layout/product-grid-items';
-import { getLiveProducts } from 'lib/utils';
+import Link from 'next/link';
 import { Suspense } from 'react';
 
 export const runtime = 'edge';
@@ -11,21 +10,25 @@ export const metadata = {
   description: 'scape²: art for the wall & wardrobe.',
   openGraph: {
     type: 'website'
-  }
+  },
 };
 
 export default async function HomePage() {
-  const liveProducts  = await getLiveProducts({});
-  
   return (
     <>
       <Suspense>
         <Carousel />
-        {liveProducts.length > 0 ? (
-          <Grid className="grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 m-6">
-            <ProductGridItems products={liveProducts} />
-          </Grid>
-        ) : null}
+        <div className='container grid justify-around my-4 grid-cols-1 md:grid-cols-2 items-center justify-items-center'>
+          <Link className='h-full w-full md:w-10/12 flex items-center justify-center justify-items-center' href="/wall">
+            <Button className='py-3 w-full m-3' variant={'dark'}>
+              <h2>Art for the Wall</h2>
+              {/* <Image src="" alt='' /> */}
+            </Button>
+          </Link>
+          <Link className='h-full w-full md:w-10/12 flex items-center justify-center' href="/wardrobe">
+            <Button className='py-3 w-full m-3' variant={'dark'}>Art for the Wardrobe</Button>
+          </Link>
+        </div>
         <Suspense>
           <Footer />
         </Suspense>
