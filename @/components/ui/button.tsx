ad2f16fdbb8b35b47be0b843617c6e3bbb9c10ce
Samantha Kellow <sammii.h@icloud.com>
2023-11-23 16:11:59 +0000

    styling base button

--- @/components/ui/button.tsx
+++ @/components/ui/button.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
@@ -10,6 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        dark: "bg-black text-white hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
