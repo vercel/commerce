e3dee2c90438390abe5053e3275e785cd43308d0
Ron Dopaz <rondopaz10@gmail.com>
2023-12-02 00:56:34 +0200

    Updated graphql api version

--- lib/constants.ts
+++ lib/constants.ts
@@ -28,4 +28,4 @@ export const TAGS = {
 
 export const HIDDEN_PRODUCT_TAG = 'nextjs-frontend-hidden';
 export const DEFAULT_OPTION = 'Default Title';
-export const SHOPIFY_GRAPHQL_API_ENDPOINT = '/api/2023-01/graphql.json';
+export const SHOPIFY_GRAPHQL_API_ENDPOINT = '/api/2023-10/graphql.json';
