0c3283f591d2e6ae3bdb0d2ba2607430487e0b1b
Malte Ubl <malte.ubl@gmail.com>
2024-02-04 16:57:55 -0800

    Upgrade to canary and fix static build

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,9 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
   return (
     <html lang="en" className={GeistSans.variable}>
       <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
-        <Navbar />
+        <Suspense>
+          <Navbar />
+        </Suspense>
         <Suspense>
           <main>{children}</main>
         </Suspense>
