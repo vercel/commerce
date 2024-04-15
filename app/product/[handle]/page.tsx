746a0fcd0310f4bdc10021ab9bca557b17d3dcc4
Chloe <pinkcloudvnn@gmail.com>
2024-04-15 12:34:56 +0700

    fix: update Shopify API version

--- app/product/[handle]/page.tsx
+++ app/product/[handle]/page.tsx
@@ -61,7 +61,7 @@ export default async function ProductPage({ params }: { params: { handle: string
     '@type': 'Product',
     name: product.title,
     description: product.description,
-    image: product.featuredImage.url,
+    image: product.featuredImage?.url,
     offers: {
       '@type': 'AggregateOffer',
       availability: product.availableForSale
--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  logging: {
-    fetches: {
-      fullUrl: true
-    }
-  },
   eslint: {
     // Disabling on production builds because we're running checks on PRs via GitHub Actions.
     ignoreDuringBuilds: true
