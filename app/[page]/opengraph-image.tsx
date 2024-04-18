e2b20810583f1c9b1a238eb2ef46f29fca84f44e
Lee Robinson <lrobinson2011@gmail.com>
2024-04-17 21:38:19 -0500

    Fix?

--- app/[page]/opengraph-image.tsx
+++ app/[page]/opengraph-image.tsx
@@ -1,6 +1,8 @@
 import OpengraphImage from 'components/opengraph-image';
 import { getPage } from 'lib/shopify';
 
+export const runtime = 'edge';
+
 export default async function Image({ params }: { params: { page: string } }) {
   const page = await getPage(params.page);
   const title = page.seo?.title || page.title;
