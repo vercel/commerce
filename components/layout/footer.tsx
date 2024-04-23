9c9a5c035b245d8811a601ba2dd134c5c5f124d7
Samantha Kellow <sammii.h@icloud.com>
2024-04-23 21:25:05 +0100

    making borders muted

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -16,7 +16,7 @@ export default async function Footer() {
 
   return (
     <footer className="text-sm text-neutral-500 dark:text-neutral-400">
-      <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-200 px-6 py-12 text-sm dark:border-neutral-700 md:flex-row md:gap-12 md:px-4 xl:px-0">
+      <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-700 px-6 py-12 text-sm dark:border-neutral-700 md:flex-row md:gap-12 md:px-4 xl:px-0">
         <div>
           <Link className="flex items-center gap-2 text-black dark:text-white" href="/">
             <LogoSquare size="sm" />
@@ -48,7 +48,7 @@ export default async function Footer() {
           </a>
         </div> */}
       </div>
-      <div className="border-t border-neutral-200 py-6 text-sm dark:border-neutral-700">
+      <div className="border-t border-neutral-700 py-6 text-sm dark:border-neutral-700">
         <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 md:flex-row md:gap-0 pl-4">
           <p>
             &copy; {copyrightDate} {copyrightName}
