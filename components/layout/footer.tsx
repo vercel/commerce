0bf97ecdf69b4f7a4aa00c090e09e613fa1aba8a
Samantha Kellow <sammii.h@icloud.com>
2024-04-24 17:50:39 +0100

    styling and adjusting footer & navbar

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,7 +1,5 @@
-import Link from 'next/link';
 
 import FooterMenu from 'components/layout/footer-menu';
-import LogoSquare from 'components/ui/logo-square';
 import { getMenu } from 'lib/shopify';
 import { Suspense } from 'react';
 
@@ -17,12 +15,12 @@ export default async function Footer() {
   return (
     <footer className="text-sm text-neutral-500 dark:text-neutral-400">
       <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-700 px-6 py-12 text-sm dark:border-neutral-700 md:flex-row md:gap-12 md:px-4 xl:px-0">
-        <div>
+        {/* <div>
           <Link className="flex items-center gap-2 text-black dark:text-white" href="/">
             <LogoSquare size="sm" />
-            {/* <span className="uppercase">{SITE_NAME}</span> */}
+            <span className="uppercase">{SITE_NAME}</span>
           </Link>
-        </div>
+        </div> */}
         <Suspense
           fallback={
             <div className="flex h-[188px] w-[100px] flex-col gap-2 ">
