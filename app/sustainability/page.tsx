90494b116cd385c7795aa0e2fc8c64222770b8e9
Samantha Kellow <sammii.h@icloud.com>
2024-04-25 18:01:48 +0100

    creating sustainability page

---
+++ app/sustainability/page.tsx
@@ -0,0 +1,11 @@
+import { SustainabilityInfo } from "components/product/sustainability-info";
+
+export default function SustainabilityStatement() {
+  return (
+    <div className="container">
+      <div className="m-6 pt-6">
+        <SustainabilityInfo />
+      </div>
+    </div>
+  )
+}
