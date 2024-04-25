9f02e3f29e8ea07b3608630238c57ea8bbbe2562
Samantha Kellow <sammii.h@icloud.com>
2024-04-25 18:02:21 +0100

    updating sustainability info

--- components/product/sustainability-info.tsx
+++ components/product/sustainability-info.tsx
@@ -12,7 +12,9 @@ export function SustainabilityInfo() {
   return (
     <>
       <div>
-          <p className='font-bold mt-6'>Credentials:</p>
+          {/* <p className='font-bold mt-6'>Credentials:</p> */}
+          <p className='font-bold mt-6 mb-1'>Garments</p>
+          <p className="mb-3">Our base garments are Stanley & Stella, which boast a range of eco credentials:</p>
           <Accordion type="single" collapsible>
             {credentialsKeys.map(credential => (
               <AccordionItem id={credential} key={credential} value={credential}>
@@ -30,6 +32,14 @@ export function SustainabilityInfo() {
               </AccordionItem>
             ))}
           </Accordion>
+          <p className='font-bold mt-6 mb-1'>Packaging</p>
+          <p className="mb-3">
+            We pride ourselves on our products being 100% plastic free, including packaging. <br /><br />
+            Our 
+              <a href="#fsc"> FSC </a>
+            certified labels ae attached to our products by a metal safety pin. <br /><br />
+            Our parcels are also FSC certified paper, so you can feel completely guilt free about your purchase!
+          </p>
         </div>
     </>
   )
