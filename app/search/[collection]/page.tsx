723afd37a9e2cc8cfb927d2709636be4af1f8ce3
Chloe <pinkcloudvnn@gmail.com>
2024-05-07 15:41:30 +0700

    fix: revert changes
    Signed-off-by: Chloe <pinkcloudvnn@gmail.com>

--- app/search/[collection]/page.tsx
+++ app/search/[collection]/page.tsx
@@ -20,6 +20,8 @@ import {
 } from 'lib/constants';
 import { Suspense } from 'react';
 
+export const runtime = 'edge';
+
 export async function generateMetadata({
   params
 }: {
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,6 +3,8 @@ import ProductGridItems from 'components/layout/product-grid-items';
 import { defaultSort, sorting } from 'lib/constants';
 import { getProducts } from 'lib/shopify';
 
+export const runtime = 'edge';
+
 export const metadata = {
   title: 'Search',
   description: 'Search for products in the store.'
