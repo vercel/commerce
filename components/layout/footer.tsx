b1b157fb8865349efe47b0f66331ae5b01b53807
Mandeep Tatla <mandy.tatla@aligent.com.au>
2024-06-21 15:43:20 +0930

    Removed the footer changes

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import FooterMenu from 'components/layout/footer-menu';
 import LogoSquare from 'components/logo-square';
 import { getMenu } from 'lib/shopify';
-import { RemoveTheDomainFromArray } from 'lib/utils';
 import { Suspense } from 'react';
 
 const { COMPANY_NAME, SITE_NAME } = process.env;
@@ -13,7 +12,6 @@ export default async function Footer() {
   const copyrightDate = 2023 + (currentYear > 2023 ? `-${currentYear}` : '');
   const skeleton = 'w-full h-6 animate-pulse rounded bg-neutral-200 dark:bg-neutral-700';
   const menu = await getMenu('next-js-frontend-footer-menu');
-  const updatedMenuLinks = RemoveTheDomainFromArray(menu);
   const copyrightName = COMPANY_NAME || SITE_NAME || '';
 
   return (
@@ -37,7 +35,7 @@ export default async function Footer() {
             </div>
           }
         >
-          <FooterMenu menu={updatedMenuLinks} />
+          <FooterMenu menu={menu} />
         </Suspense>
         <div className="md:ml-auto">
           <a
