b17cc49e3b274dd754216a0052506deb7876f024
jrphilo <james.philo@me.com>
2024-07-12 21:29:26 +0200

    feat: add transmission warranty details
    Signed-off-by: jrphilo <james.philo@me.com>

--- components/product/warranty.tsx
+++ components/product/warranty.tsx
@@ -199,6 +199,165 @@ const Warranty = ({ product, siteName }: { product: Product; siteName: string | 
             </section>
 
             <section>
+              <h3>Remanufactured Transmissions</h3>
+            </section>
+
+            <section>
+              <p className="text-sm">
+                A remanufactured and improved torque converter is included with every transmission.
+                To eliminate the possibility of front seal leaks, vibration, and premature bushing
+                wear, all torque converters are tested for leaks, lock-up, concentricity, and
+                balance. Every fully remanufactured valve body, with complete system correction and
+                recalibration kit, is tested independently. All wear-prone valves are restored to
+                stringent specifcations and vacuum tested to confrm proper function. Each
+                remanufactured transmission is hot, cold, load, and simulated road tested using our
+                proprietary CARS (Computer-Aided Road Simulation) dynamometer program. All
+                remanufactured transmissions are backed by the industry's best, no-hassle nationwide
+                warranty. Warranty claim reimbursement cannot exceed the total cost of the part
+                purchased.
+              </p>
+            </section>
+
+            <section>
+              <p className="mb-1 mt-2 text-sm font-semibold">Year 2000 and Older</p>
+              <p className="text-sm">
+                Personal: 36 Months/Unlimited Miles Commercial: 18 Months/100,000 Miles CVT
+                Transmissions: 36 Months/Unlimited Miles Manual Transmissions: 36 Months/Unlimited
+                Miles
+              </p>
+            </section>
+
+            <section>
+              <p className="mb-1 mt-2 text-sm font-semibold">Year 2001 and Newer</p>
+              <p className="text-sm">
+                Personal: 60 Months/Unlimited Miles Commercial: Prior to 3/1/2020 18 Months/100,000
+                Miles EFFECTIVE 3/1/2020 Commercial Warranty 36 Months/Unlimited miles CVT
+                Transmissions: 36 Months/Unlimited Miles Manual Transmissions: 36 Months/Unlimited
+                Miles
+              </p>
+            </section>
+
+            <section>
+              <p className="mb-1 mt-2 text-sm font-semibold">WHAT DOES “NO HASSLE” MEAN?</p>
+              <p className="text-sm">
+                Transferrable – The warranty is attached to the VIN and fully transferrable with
+                vehicle ownership; no fees or paperwork involved. Nationwide Coverage – You are
+                covered anywhere (and everywhere) in the continental United States. Instant
+                Replacement – We send out a replacement transmission as soon as a warranty claim is
+                submitted so you are back on the road as quickly as possible. Paid Parts & Labor –
+                For warranty work done in a certifed shop, we pay parts and labor. Authorized
+                repairs on approved claims (after factory inspection of returned parts) will be
+                reimbursed at a rate not to exceed the Mitchell Repair Manual published applicable
+                fat rate schedule. Hourly rates for all authorized repairs will be paid in
+                accordance with rate but not to exceed $50.00 an hour. Do-it yourself repairs,
+                repairs performed by unlicensed repair facilities, or repairs made to units not
+                originally installed by licensed auto repair facility, labor will not be reimbursed.
+                RT is not responsible for any charges associated with improper transmission
+                installation and or labor costs. RT will not assume towing, shipping, transportation
+                charges or rental car costs Modifed vehicles will have no warranty coverage If
+                return or exchange was authorized by RT, the part needs to be received in the same
+                assembled condition as it was originally shipped to the customer. If the part is not
+                received in the same condition there will be absolutely NO REFUND under any
+                circumstances. If transmission was disassembled or altered in any way without RT
+                written authorization the warranty will be void. If core is not returned or paid
+                for, warranty will be voided.
+              </p>
+            </section>
+
+            <section>
+              <p className="mb-1 mt-2 text-sm font-semibold">WARRANTY WILL BE VOID IF:</p>
+              <p className="text-sm">
+                Transmission was not properly installed. Transmission was used for racing.
+                Transmission was used without proper lubrication or cooling regardless of the
+                reason. Complete warranty coverage can be found at
+                www.reman-transmission.com/warranty-info. Our inventory is located at various
+                supplier facilities, delivery times may vary, but it normally takes 7-14 BUSINESS
+                days (excluding weekends and holidays). Even though its uncommon but sometimes
+                delivery times may be affected due to unusual circumstances that may require some
+                additional time. We offer Flat Rate shipping (Commercial address) to address within
+                Continental USA. All returned parts or cancelled orders are subject to a 25%
+                handling and restocking fees, customer assumes all the shipping costs unless
+                replacement is authorized by RT during warranty period. If order was cancelled after
+                payment was processed, but part was not shipped out, customer is responsible for 10%
+                cancellation fee. Customer is responsible for fulflling the terms of payment. If
+                customer defaults on the payment terms he/she will be responsible for all of the
+                charges associated with collection. These charges will include all collection costs
+                and attorney fees. Wrong parts ordered or parts that were incorrectly diagnosed are
+                non-refundable. RT will not assume towing, shipping, lodging, transportation charges
+                or rental car costs. Falsifying any information will automatically void your
+                warranty. Please note that you are purchasing used part! We do our best to make the
+                part as presentable as possible but we make no guarantees towards the appearance of
+                it Please inspect all the parts before signing on delivery. The mileage stated is
+                approximate whenever is available to the best of our knowledge. RT warranty does not
+                apply to oil leaks/damage on engines due to seals, gaskets and flters that has not
+                been replaced. OEM auto parts are interchangeable within multiple years, makes and
+                models. Same part throughout multiple year range will result in exact ft for
+                multiple makes and models as determined by OEM standards. Deposits made to us are
+                non-refundable unless otherwise noted. If return or exchange was authorized by RT,
+                the part needs to be received in the same assembled condition as it was originally
+                shipped to the customer. If the part is not received in the same condition there
+                will be absolutely NO REFUND under any circumstances. Refunds are only authorized if
+                replacement is not available. If the engine was disassembled or altered in any way
+                without RT written authorization the warranty will be void. Transmission is not
+                installed within 30 days after delivery. Used Transmission warranty will cover only
+                a one-time claim per product purchased from us. Reman-transmission warranty claim
+                representative will work with you to ensure your experience is a complete success.
+                We reserve the right to charge any applicable fees in connection with processing of
+                your warranty claim. Used transmission warranty is for the original purchaser only
+                and is not transferrable.
+              </p>
+            </section>
+
+            <section>
+              <h3>Used Transmissions</h3>
+            </section>
+
+            <section>
+              <p className="text-sm">
+                {siteName}&apos;s used transmission warranty is limited to manufacturing defects of
+                the following parts: transmission housing, torque converter (included for automatic
+                transmissions) and all internal components, including gears and shafts. The warranty
+                does not cover any external wiring, sensors, or other external parts that can be
+                transferred from your current transmission. {siteName} warranty does not cover any
+                attached accessory parts, such as switches, sensors, cables, electronics etc.
+              </p>
+            </section>
+
+            <section>
+              <p className="mb-1 mt-2 text-sm font-semibold">Year 2000 and Older</p>
+              <p className="text-sm">
+                Personal: 6 Months/ 6,000 miles (parts only) Commercial: 6 Months/ 6,000 miles
+                (parts only) CVT Transmissions: 6 Months/ 6,000 miles (parts only) Manual
+                Transmissions: 6 Months/ 6,000 miles (parts only)
+              </p>
+            </section>
+
+            <section>
+              <p className="mb-1 mt-2 text-sm font-semibold">Years 2001 and Newer</p>
+              <p className="text-sm">
+                Personal: 1 Year/ 12,000 miles (parts only) Commercial: 6 Months/ 6,000 miles (parts
+                only) CVT Transmissions: 6 Months/ 6,000 miles (parts only) Manual Transmissions: 6
+                Months/ 6,000 miles (parts only)
+              </p>
+            </section>
+
+            <section>
+              <p className="text-sm">
+                Reman-transmission is not responsible for any charges associated with improper
+                transmission installation and or labor costs. Warranty claim reimbursement cannot
+                exceed the total cost of the part purchased. Vehicle needs be be at the repair
+                facility at the time the warranty is filed. Reman-transmission warranty does not
+                apply for an initial installation. Reman-transmission warranty is limited to
+                one-time replacement. Warranty reimbursement cannot exceed the purchase price.
+                Reman-transmission warranty claim must be submitted and authorized by
+                Reman-transmission warranty technician prior to any work being performed. Purchaser
+                needs to submit documentation, as required within 5 business days, otherwise your
+                claim will be denied, no exceptions. Vehicle use shall be immediately discontinued
+                once the issue or problem has been identified, otherwise warranty will be void.
+              </p>
+            </section>
+
+            <section>
               <p className="mb-1 mt-2 text-sm font-semibold">Limited Liability</p>
               <p className="text-sm">
                 The {siteName} liability is solely and exclusively limited to supplying a
@@ -207,19 +366,41 @@ const Warranty = ({ product, siteName }: { product: Product; siteName: string | 
                 replacement of engine oil, transmission fuid and antifreeze, or damage to other
                 engine or transmission parts or components towing charges, telephone calls, freight,
                 lost profts, lost time, substitute transportation or replacement vehicle or any
-                other consequential damages. RE assumes no responsibility for any failure resulting
-                from improper installation, modifcation of the product faulty or incompatible parts
-                and accessories and/or abnormal use of operation. State and federal laws regulate
-                odometer mileage for most used automobiles, but odometer mileage determinations for
-                used engines, transmissions and their parts are not practically possible.
-                Determination of mileage use for used engines cannot be done with certainty since
-                the engines are often sold and/or transferred apart from the original vehicle in
-                which they were originally installed. Therefore all representations of mileage are
-                estimates.
+                other consequential damages. {siteName} assumes no responsibility for any failure
+                resulting from improper installation, modifcation of the product faulty or
+                incompatible parts and accessories and/or abnormal use of operation. State and
+                federal laws regulate odometer mileage for most used automobiles, but odometer
+                mileage determinations for used engines, transmissions and their parts are not
+                practically possible. Determination of mileage use for used engines cannot be done
+                with certainty since the engines are often sold and/or transferred apart from the
+                original vehicle in which they were originally installed. Therefore all
+                representations of mileage are estimates.
                 {siteName} offers an extensive engine and transmission warranty regardless of the
                 mileage, based upon our expertise and evaluation of our products. Extended
                 warranties are also available at the additional cost. Contact our customer service
                 department for details.
+              </p>
+            </section>
+
+            <section>
+              <p className="mb-1 mt-2 text-sm font-semibold">Limited Liability</p>
+              <p className="text-sm">
+                The Reman-transmission liability is solely and exclusively limited to supplying a
+                replacement transmission whenever one is available or refunding customer with
+                purchase price. Reman-transmission does not assume any liability associated with
+                labor costs, replacement of transmission fluid, or damage to other transmission
+                parts or components towing charges, telephone calls, freight, lost profits, lost
+                time, substitute transportation or replacement vehicle or any other consequential
+                damages. Reman-transmission assumes no responsibility for any failure resulting from
+                improper installation, modification of the product faulty or incompatible parts and
+                accessories and/or abnormal use of operation. State and federal laws regulate
+                odometer mileage for most used automobiles, but odometer mileage determinations for
+                used transmissions and their parts are not practically possible. Determination of
+                mileage use for used engines cannot be done with certainty since the engines are
+                often sold and/or transferred apart from the original vehicle in which they were
+                originally installed. Therefore all representations of mileage are estimates.
+                Reman-transmission offers an extensive transmission warranty regardless of the
+                mileage, based upon our expertise and evaluation of our products.
               </p>
             </section>
 
@@ -229,14 +410,14 @@ const Warranty = ({ product, siteName }: { product: Product; siteName: string | 
                 returned before any warranty claims will be paid out.
               </p>
               <p className="text-sm">
-                Please visit our
+                Please visit our{' '}
                 <Link
                   href="/warranty-info"
                   target="_blank"
                   className="text-blue-800 hover:underline"
                 >
                   warranty page
-                </Link>
+                </Link>{' '}
                 for our full warranty disclosure.
               </p>
             </section>
