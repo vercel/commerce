7756b912e8acc69357ef7487f218827c211da715
Lee Robinson <lrobinson2011@gmail.com>
2024-07-24 14:02:26 -0500

    Update deps

--- components/layout/navbar/mobile-menu.tsx
+++ components/layout/navbar/mobile-menu.tsx
@@ -83,7 +83,7 @@ export default function MobileMenu({ menu }: { menu: Menu[] }) {
                         className="py-2 text-xl text-black transition-colors hover:text-neutral-500 dark:text-white"
                         key={item.title}
                       >
-                        <Link href={item.path} onClick={closeMobileMenu}>
+                        <Link href={item.path} prefetch={true} onClick={closeMobileMenu}>
                           {item.title}
                         </Link>
                       </li>
