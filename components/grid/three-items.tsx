11190da60a8bc895b44b1dd764c7d2b0df200dd4
Vincent Voyer <vincent@codeagain.com>
2024-07-25 11:29:49 +0200

    feat(design): Show carousel above the fold on desktop
    Before this commit, we would not see the carousel without scrolling. The top
    images are so big that take most of the space. This made the website looked a
    bit weird, thus I am proposing this change.

--- components/grid/three-items.tsx
+++ components/grid/three-items.tsx
@@ -52,7 +52,7 @@ export async function ThreeItemGrid() {
   const [firstProduct, secondProduct, thirdProduct] = homepageItems;
 
   return (
-    <section className="mx-auto grid max-w-screen-2xl gap-4 px-4 pb-4 md:grid-cols-6 md:grid-rows-2">
+    <section className="mx-auto grid max-w-screen-2xl gap-4 px-4 pb-4 md:grid-cols-6 md:grid-rows-2 lg:max-h-[calc(100vh-200px)]">
       <ThreeItemGridItem size="full" item={firstProduct} priority={true} />
       <ThreeItemGridItem size="half" item={secondProduct} priority={true} />
       <ThreeItemGridItem size="half" item={thirdProduct} />
