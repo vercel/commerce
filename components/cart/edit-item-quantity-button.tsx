e3437f0e5c4c2cc216232b6c80e9db1d813a53cb
Lee Robinson <lrobinson2011@gmail.com>
2024-07-28 11:42:56 -0500

    Increase tap area for button

--- components/cart/edit-item-quantity-button.tsx
+++ components/cart/edit-item-quantity-button.tsx
@@ -12,7 +12,7 @@ function SubmitButton({ type }: { type: 'plus' | 'minus' }) {
       type="submit"
       aria-label={type === 'plus' ? 'Increase item quantity' : 'Reduce item quantity'}
       className={clsx(
-        'ease flex h-full min-w-[36px] max-w-[36px] flex-none items-center justify-center rounded-full px-2 transition-all duration-200 hover:border-neutral-800 hover:opacity-80',
+        'ease flex h-full min-w-[36px] max-w-[36px] flex-none items-center justify-center rounded-full p-2 transition-all duration-200 hover:border-neutral-800 hover:opacity-80',
         {
           'ml-auto': type === 'minus'
         }
