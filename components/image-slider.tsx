60afae9ff4e84fb53645e2e050d449eb1ced3151
Josh Furie <joshua.furie@aspireiq.com>
2024-08-06 15:18:25 -0700

    fix: scroll bug fix

--- components/image-slider.tsx
+++ components/image-slider.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { ImgHTMLAttributes, useEffect } from 'react';
+import { ImgHTMLAttributes, useEffect, useRef } from 'react';
 import { isMobile } from 'react-device-detect';
 export const ImageSlider = ({
   images,
@@ -10,21 +10,26 @@ export const ImageSlider = ({
   images: ImgHTMLAttributes<HTMLImageElement>[];
   selectedImageIndex: string;
 }) => {
+  const previousIndex = useRef(-1);
   useEffect(() => {
-    const element = document.getElementById(`slider-image-${selectedImageIndex}`);
-    if (element) {
-      if (isMobile) {
-        element.scrollIntoView(true);
-      } else {
-        element.scrollIntoView({ behavior: 'smooth', inline: 'center' });
+    // skip the first render
+    if (previousIndex.current >= 0 && selectedImageIndex !== previousIndex.current.toString()) {
+      const element = document.getElementById(`slider-image-${selectedImageIndex}`);
+      if (element) {
+        if (isMobile) {
+          element.scrollIntoView(true);
+        } else {
+          element.scrollIntoView({ behavior: 'smooth', inline: 'center' });
+        }
       }
     }
+    previousIndex.current = parseInt(selectedImageIndex, 10);
   }, [selectedImageIndex]);
 
   return (
     <ul className="scrollbar-none grid snap-x snap-mandatory auto-cols-[100%] grid-flow-col overflow-x-auto">
       {images.map((image, index) => (
-        <li className="snap-center" key={index}>
+        <li className="snap-center" key={image.id}>
           <Image
             className="aspect-square h-auto w-full object-contain"
             {...image}
