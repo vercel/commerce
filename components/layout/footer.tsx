38e9056509c5df501c0f4fd74d59061b3aa5dd4a
Jieren Chen <jieren.chen@gmail.com>
2024-09-23 16:54:19 -0400

    footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -7,7 +7,6 @@ const { COMPANY_NAME, SITE_NAME } = process.env;
 export default async function Footer() {
   const currentYear = new Date().getFullYear();
   const copyrightDate = 2023 + (currentYear > 2023 ? `-${currentYear}` : '');
-  const skeleton = 'w-full h-6 animate-pulse rounded bg-neutral-200 dark:bg-neutral-700';
   const copyrightName = COMPANY_NAME || SITE_NAME || '';
 
   return (
