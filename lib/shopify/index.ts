8d4cc9a9a77c0cdd7ae478c190e7905d059a6637
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-10-27 17:21:33 -0300

    fixing response status code for no secret or wrong secret (#1397)

--- lib/shopify/index.ts
+++ lib/shopify/index.ts
@@ -435,7 +435,7 @@ export async function revalidate(req: NextRequest): Promise<NextResponse> {
 
   if (!secret || secret !== process.env.SHOPIFY_REVALIDATION_SECRET) {
     console.error('Invalid revalidation secret.');
-    return NextResponse.json({ status: 200 });
+    return NextResponse.json({ status: 401 });
   }
 
   if (!isCollectionUpdate && !isProductUpdate) {
