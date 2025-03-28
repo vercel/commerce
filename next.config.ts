9d245b5cd8ee15948e75e1f788e8381fe8a47aad
karl <karl@linconson.com>
2025-03-28 12:04:02 -0400

    add image

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,23 @@ export default {
   experimental: {
     ppr: true,
     inlineCss: true,
-    useCache: true
+    useCache: true,
   },
   images: {
     formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'cdn.shopify.com',
-        pathname: '/s/files/**'
-      }
-    ]
-  }
-};
+        pathname: '/s/files/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'linconson-a.netlify.app',
+        port: '80',
+        pathname: '**',
+      },
+    ],
+  },
+}
