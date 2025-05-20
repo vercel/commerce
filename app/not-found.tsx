41562e065e45c38baad2dacabd6ccdb93ee39382
basstian-ai <stian.prestegaard@gmail.com>
2025-05-20 17:24:09 +0200

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ padding: 40, textAlign: 'center' }}>
+      <h1 style={{ fontSize: 48, marginBottom: 16 }}>404</h1>
+      <p style={{ fontSize: 18 }}>This page could not be found.</p>
+    </div>
+  );
+}
