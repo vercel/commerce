a7c44a85adbd4de255b5c96765a790df176cd7bb
mark-villanueva <villanuevamark455@gmai.com>
2025-09-22 16:50:20 +0800

    added cypress tests and GitHub action config

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -7,18 +7,18 @@ describe("Home Page", () => {
         cy.getBySel("product-name").should("contain", "Code Shirt")
         cy.getBySel("product-price").should("contain", "₱2,000.00")
       })
-    cy.getBySel("product-tag")
-      .eq(1)
-      .within(() => {
-        cy.getBySel("product-name").should("contain", "Funny it works on my machine programmer")
-        cy.getBySel("product-price").should("contain", "₱1,000.00")
-      })  
     // cy.getBySel("product-tag")
-    //   .eq(2)
+    //   .eq(1)
     //   .within(() => {
-    //     cy.getBySel("product-name").should("contain", "I'm not a robot Classic T-Shirt")
+    //     cy.getBySel("product-name").should("contain", "Funny it works on my machine programmer")
     //     cy.getBySel("product-price").should("contain", "₱1,000.00")
-    //   })    
+    //   })  
+    // // cy.getBySel("product-tag")
+    // //   .eq(2)
+    // //   .within(() => {
+    // //     cy.getBySel("product-name").should("contain", "I'm not a robot Classic T-Shirt")
+    // //     cy.getBySel("product-price").should("contain", "₱1,000.00")
+    // //   })    
 
   })
 
