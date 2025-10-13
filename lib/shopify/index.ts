cd3c834e96750c768cd5434bab39cc469e37ada9
Jude Gao <jude.gao@vercel.com>
2025-10-13 17:27:17 -0400

    update to nextjs 16

--- lib/shopify/index.ts
+++ lib/shopify/index.ts
@@ -490,11 +490,11 @@ export async function revalidate(req: NextRequest): Promise<NextResponse> {
   }
 
   if (isCollectionUpdate) {
-    revalidateTag(TAGS.collections);
+    revalidateTag(TAGS.collections, 'max');
   }
 
   if (isProductUpdate) {
-    revalidateTag(TAGS.products);
+    revalidateTag(TAGS.products, 'max');
   }
 
   return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
